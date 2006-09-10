4adcf49d55dd6b2a5ff54f85ce01fc61f1d8b763
Olli Salli <olli.salli@collabora.co.uk>
2006-09-10 16:51:55 +0000

    Add JEP-0115 feature bundle string constants header bundles.h
    20060910165155-9db4d-cabd12adc9d7d2c86b27f5d9807cbfd3e0fbfdfa.gz

---
+++ src/bundles.h
@@ -0,0 +1,28 @@
+/*
+ * bundles.h - JEP-0015 feature bundle constants
+ * Copyright (C) 2005 Collabora Ltd.
+ * Copyright (C) 2005 Nokia Corporation
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+#ifndef __GABBLE_BUNDLES__H__
+#define __GABBLE_BUNDLES__H__
+
+#define BUNDLE_VOICE_V1         "voice-v1"
+#define BUNDLE_JINGLE_AUDIO     "jingle-audio"
+#define BUNDLE_JINGLE_VIDEO     "jingle-video"
+
+#endif  /* __GABBLE_BUNDLES__H__ */

ddfc8d290ce3971d5f7f2058a206d5a1f977be60
Simon McVittie <simon.mcvittie@collabora.co.uk>
2006-10-18 14:44:19 +0000

    gabble-connection.c: Encode SHA-1 in hex again, not in base64.
    It appears (from the behaviour of at least Gajim) that it's meant to be in
    hex after all, despite the schema in XEP-0153 claiming base64Binary. Thanks,
    XEP-0153.
    
    20061018144419-53eee-5073eb13d4bbd6436fef2a8df3b970c5439066fc.gz

--- src/gabble-connection.c
+++ src/gabble-connection.c
@@ -4957,21 +4957,25 @@ _set_avatar_cb2 (GabbleVCardManager *manager,
     {
       SHA1Context sc;
       uint8_t hash[SHA1_HASH_SIZE];
-      gchar *b64_hash;
+      gchar hex_hash[SHA1_HASH_SIZE*2 + 1];
+      int i;
       GabblePresence *presence;
       GError *error;
 
       SHA1Init (&sc);
       SHA1Update (&sc, ctx->avatar->str, ctx->avatar->len);
       SHA1Final (&sc, hash);
-      b64_hash = base64_encode ((gchar *)hash, SHA1_HASH_SIZE);
+      for (i = 0; i < SHA1_HASH_SIZE; i++)
+        {
+          sprintf(hex_hash + 2*i, "%02x", (unsigned int)hash[i]);
+        }
       presence = gabble_presence_cache_get (ctx->conn->presence_cache,
                                             ctx->conn->self_handle);
       g_free (presence->avatar_sha1);
-      presence->avatar_sha1 = b64_hash;
+      presence->avatar_sha1 = hex_hash;
       if (signal_own_presence (ctx->conn, &error))
         {
-          dbus_g_method_return (ctx->invocation, b64_hash);
+          dbus_g_method_return (ctx->invocation, hex_hash);
         }
       else
         {

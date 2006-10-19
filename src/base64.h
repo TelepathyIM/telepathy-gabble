c4cd66c89890a26960ed63f0a603187e7de5ddeb
Simon McVittie <simon.mcvittie@collabora.co.uk>
2006-10-19 12:39:55 +0000

    base64.[ch]: add copyright blurb
    20061019123955-53eee-002cc75304d1b09dcd84b4cf6db92cb14d608107.gz

--- src/base64.h
+++ src/base64.h
@@ -1,3 +1,21 @@
+/*
+ * base64.h - Base 64 encoding/decoding implementation
+ * Copyright (C) 2006 Collabora Ltd.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
 
 #ifndef __BASE64_H__
 #define __BASE64_H__
@@ -8,4 +26,3 @@ gchar *base64_encode (const GString *str);
 GString *base64_decode (const gchar *str);
 
 #endif /* __BASE64_H__ */
-

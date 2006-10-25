c01dcaf20a0e684a972d6d0b5864cd1b049bde56
Robert McQueen <robert.mcqueen@collabora.co.uk>
2006-10-25 12:11:22 +0000

    GabbleMediaSession: set incoming streams to acknowledged immediately
    20061025121122-418b8-aae77a5304d872c36f8fec5fc579761461be88d1.gz

--- src/gabble-media-session.c
+++ src/gabble-media-session.c
@@ -230,6 +230,12 @@ create_media_stream (GabbleMediaSession *session,
         "combined-direction", TP_MEDIA_STREAM_DIRECTION_NONE,
         NULL);
 
+  /* for incoming streams, set the signalling state to ACKNOWLEDGED */
+  if (initiator == INITIATOR_REMOTE)
+    g_object_set (stream,
+        "signalling-state", STREAM_SIG_STATE_ACKNOWLEDGED,
+        NULL);
+
   g_signal_connect (stream, "close",
                     (GCallback) stream_close_cb,
                     session);

51e5f3b0d7c0a4a7974348d37a8a6d89f003feaa
Simon McVittie <simon.mcvittie@collabora.co.uk>
2007-01-18 16:15:39 +0000

    Add infrastructure for generating lots of GInterfaces from the Telepathy specification
    20070118161539-53eee-84f40b2a547726ce2f1e913d779e2db6d0947031.gz

---
+++ lib/update-from-spec.mk
@@ -0,0 +1,83 @@
+#! /usr/bin/make -f
+
+# To be run from inside _spec.
+
+TELEPATHY_SPEC = $(CURDIR)/../../../telepathy-spec
+
+# Exclude ChannelInterfaceDTMF for now, it causes odd behaviour in the
+# camel-case-to-lower-case algorithms and we don't need it yet.
+
+INTERFACES = \
+	     ChannelInterfaceGroup \
+	     ChannelInterfaceHold \
+	     ChannelInterfaceMediaSignalling \
+	     ChannelInterfacePassword \
+	     ChannelInterfaceTransfer \
+	     ChannelTypeContactList \
+	     ChannelTypeContactSearch \
+	     ChannelTypeRoomList \
+	     ChannelTypeStreamedMedia \
+	     ChannelTypeText \
+	     Channel \
+	     ConnectionInterfaceAliasing \
+	     ConnectionInterfaceAvatars \
+	     ConnectionInterfaceCapabilities \
+	     ConnectionInterfaceContactInfo \
+	     ConnectionInterfaceForwarding \
+	     ConnectionInterfacePresence \
+	     ConnectionInterfacePrivacy \
+	     ConnectionInterfaceRenaming \
+	     ConnectionManager \
+	     Connection \
+	     MediaSessionHandler \
+	     MediaStreamHandler \
+	     Properties
+
+INTERFACES_LC = $(shell cat $(INTERFACES:%=$(TELEPATHY_SPEC)/tmp/%.name.lower))
+INTERFACES_DASH = $(subst _,-,$(INTERFACES_LC))
+INTERFACES_UC = $(shell cat $(INTERFACES:%=$(TELEPATHY_SPEC)/tmp/%.name.upper))
+
+GLUE = $(INTERFACES_DASH:%=%-service-iface-glue.h)
+SIGNALS_MARSHAL_LISTS = $(INTERFACES_DASH:%=%-service-iface-signals-marshal.list)
+IMPLS = $(INTERFACES_DASH:%=%-service-iface.c)
+HEADERS = $(INTERFACES_DASH:%=telepathy-glib/%-service-iface.h)
+
+SIMPLE_HEADERS = telepathy-glib/_spec/telepathy-enums.h \
+	         telepathy-glib/_spec/telepathy-errors.h \
+	         telepathy-glib/_spec/telepathy-interfaces.h
+
+GENERATED_FILES = $(SIMPLE_HEADERS) $(GLUE) $(IMPLS) $(HEADERS) $(SIGNALS_MARSHAL_LISTS) Makefile.am.gen
+
+update: prepare $(GENERATED_FILES) check
+
+check:
+	@cat ../signals-marshal.list $(SIGNALS_MARSHAL_LISTS) \
+		| sort | uniq | diff ../signals-marshal.list - \
+		|| echo "*** Some signals are missing from signals-marshal.list"
+
+Makefile.am.gen: ../update-from-spec.mk
+	@echo "Making _spec/Makefile.am.gen"
+	@(\
+	echo "# Generated from Makefile.update-from-spec";\
+	echo "noinst_LTLIBRARIES = libtelepathy-glib-_spec.la";\
+	echo "tpginclude_HEADERS = $(HEADERS) $(SIMPLE_HEADERS)";\
+	echo "libtelepathy_glib__spec_la_SOURCES = $(GLUE) $(IMPLS)";\
+	echo "SIGNALS_MARSHAL_LISTS = $(SIGNALS_MARSHAL_LISTS)";\
+	) > $@
+
+prepare:
+	@{ test -n $(TELEPATHY_SPEC) && test -d $(TELEPATHY_SPEC) \
+		&& test -f $(TELEPATHY_SPEC)/spec/all.xml; } \
+		|| { echo "TELEPATHY_SPEC must be specified"; exit 1; }
+	$(MAKE) -C $(TELEPATHY_SPEC)
+
+$(GLUE) $(IMPLS) $(SIGNALS_MARSHAL_LISTS): %: $(TELEPATHY_SPEC)/c/ginterfaces/%
+	cp $< $@
+
+$(HEADERS): telepathy-glib/%: $(TELEPATHY_SPEC)/c/ginterfaces/%
+	@install -d telepathy-glib
+	cp $< $@
+
+$(SIMPLE_HEADERS): telepathy-glib/_spec/%: $(TELEPATHY_SPEC)/c/%
+	@install -d telepathy-glib/_spec
+	cp $< $@

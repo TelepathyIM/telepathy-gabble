a11cc43c4983fb5866ee8a87032b4fb36608523b
Dafydd Harries <dafydd.harries@collabora.co.uk>
2007-02-25 14:36:03 +0000

    define OLPC namespaces
    20070225143603-c9803-fd80be304aa470c42cb6e51c7c01c6cbc728199c.gz

--- src/namespaces.h
+++ src/namespaces.h
@@ -45,6 +45,8 @@
 #define NS_MUC_OWNER            "http://jabber.org/protocol/muc#owner"
 #define NS_NICK                 "http://jabber.org/protocol/nick"
 #define NS_NOKIA_IV             "http://videovoip.tableteer.nokia.com/xmpp/privilege"
+#define NS_OLPC_BUDDY_PROPS     "http://laptop.org/xmpp/buddy-properties"
+#define NS_OLPC_ACTIVITIES      "http://laptop.org/xmpp/buddy-activities"
 #define NS_PUBSUB               "http://jabber.org/protocol/pubsub"
 #define NS_PRESENCE_INVISIBLE   "presence-invisible"
 #define NS_PRIVACY              "jabber:iq:privacy"

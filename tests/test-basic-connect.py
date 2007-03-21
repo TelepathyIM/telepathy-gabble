6672c0b1a5ab419ac6090b5c379d62f53fda4e82
Dafydd Harries <dafydd.harries@collabora.co.uk>
2007-03-21 18:56:39 +0000

    test-basic-connect.py: factor out constants
    20070321185639-c9803-fa3c74913060c5daf82a055fcb9c4695f6c9df23.gz

--- tests/test-basic-connect.py
+++ tests/test-basic-connect.py
@@ -1,16 +1,23 @@
 #!/usr/bin/python
 from dbustesting import *
 
+service = "org.freedesktop.Telepathy.ConnectionManager.gabble"
+path = "/org/freedesktop/Telepathy/ConnectionManager/gabble"
+iface_conn_mgr = "org.freedesktop.Telepathy.ConnectionManager"
+iface_conn = "org.freedesktop.Telepathy.Connection"
+iface_chan = "org.freedesktop.Telepathy.Channel"
+chan_type_text = "org.freedesktop.Telepathy.Channel.Type.Text"
+
 class BasicConnectTest(ClientTestCase):
 
     def start_manager(self, nick):
-        self[nick] = ("org.freedesktop.Telepathy.ConnectionManager.gabble","/org/freedesktop/Telepathy/ConnectionManager/gabble","org.freedesktop.Telepathy.ConnectionManager")
+        self[nick] = (service, path, iface_conn_mgr)
 
     def connect(self, nick, proto, params):
         # Connect to the manager
         conn, obj = self[nick]["RequestConnection"].call(proto, params)
         # register connection as self.nick_conn
-        self[nick+"_conn"] = (conn, obj, "org.freedesktop.Telepathy.Connection")
+        self[nick+"_conn"] = (conn, obj, iface_conn)
         self[nick+"_conn"]["Connect"].call(timeout=30000)
 
         # Retrieve connection status, and wait until connected
@@ -42,11 +49,10 @@ class BasicConnectTest(ClientTestCase):
             # The connection service
             conn2,
             # A new text channel to the client 1
-            self["client2_conn"]["RequestChannel"].call(
-                "org.freedesktop.Telepathy.Channel.Type.Text", 1,
+            self["client2_conn"]["RequestChannel"].call(chan_type_text, 1,
                 self["client2_conn"]["RequestHandles"].call (1, ["telepathytest1@jabber.org"])[0], False), 
             # The interfaces for this text channel
-            {"text": "org.freedesktop.Telepathy.Channel.Type.Text", "chan": "org.freedesktop.Telepathy.Channel"})
+            {"text": chan_type_text, "chan": iface_chan})
 
         # Send a test message to client 1 from client 2
         self["client2_chan"]["text"]["Send"].call (0, "Test")
@@ -54,7 +60,7 @@ class BasicConnectTest(ClientTestCase):
         # We got a newchannel from client 1
         obj, channel_type, handle_type, handle, suppress_handler = self["client1_conn"]["NewChannel"].wait()
 
-        assert channel_type == "org.freedesktop.Telepathy.Channel.Type.Text"
+        assert channel_type == chan_type_text
         assert handle_type == 1
         assert suppress_handler == False
 
@@ -62,7 +68,7 @@ class BasicConnectTest(ClientTestCase):
         self["client1_chan"] = (
             conn1,
             obj, 
-            {"text": "org.freedesktop.Telepathy.Channel.Type.Text", "chan": "org.freedesktop.Telepathy.Channel"})
+            {"text": chan_type_text, "chan": iface_chan})
 
         # Retreive pending messages
         messages = self["client1_chan"]["text"]["ListPendingMessages"].call(True)

959444ac621f0217e202c263e04d2de8c719efba
Simon McVittie <simon.mcvittie@collabora.co.uk>
2007-03-26 19:28:34 +0000

    Add documentation for headers that are auto-generated (not putting it in the headers themselves to avoid spurious recompiles)
    20070326192834-53eee-473365ea9385cfda75dadf8c6de2bd2887e18e5a.gz

---
+++ lib/telepathy-glib/extra-gtkdoc.h
@@ -0,0 +1,51 @@
+/* This file contains no code - it's just here for gtkdoc to pick up
+ * documentation for otherwise undocumented generated files.
+ */
+
+/**
+ * SECTION:svc-properties-interface
+ * @title: Service-side Properties interface
+ * @short_description: GInterface for D-Bus objects exporting Telepathy
+ *  properties
+ * @see_also: #TpPropertiesMixin
+ *
+ * The Telepathy Properties interface associates a number of named properties
+ * with a channel, connection or other D-Bus object. Signals are emitted
+ * when the properties or their flags (readable/writable) change.
+ */
+
+/**
+ * SECTION:svc-channel
+ * @title: Service-side Channel types and interfaces
+ * @short-description: GInterfaces for Telepathy Channel objects
+ * @see_also: #TpChannelIface
+ *
+ * These interfaces (auto-generated from the Telepathy spec) make it easier
+ * to export objects implementing the Telepathy Channel and its types and
+ * optional interfaces, with the correct method and signal signatures,
+ * and emit signals from those objects in a type-safe way.
+ */
+
+/**
+ * SECTION:svc-connection
+ * @title: Service-side Connection interfaces
+ * @short-description: GInterfaces for Telepathy Connection objects
+ * @see_also: #TpBaseConnection
+ *
+ * These interfaces (auto-generated from the Telepathy spec) make it easier
+ * to export objects implementing the Telepathy Connection and its
+ * optional interfaces, with the correct method and signal signatures,
+ * and emit signals from those objects in a type-safe way.
+ */
+
+/**
+ * SECTION:svc-connection-manager
+ * @title: Service-side Connection Manager interface
+ * @short-description: GInterface for Telepathy ConnectionManager objects
+ * @see_also: #TpBaseConnection
+ *
+ * The #TpSvcConnectionManager interface (auto-generated from the Telepathy
+ * spec) makes it easier to export an object implementing the Telepathy
+ * ConnectionManager interface, with the correct method and signal signatures,
+ * and emit signals from that object in a type-safe way.
+ */

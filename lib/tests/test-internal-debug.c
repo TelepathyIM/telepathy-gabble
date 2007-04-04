7e5ee52625b3bee3c1377740c05c2ebdeb162d5e
Simon McVittie <simon.mcvittie@collabora.co.uk>
2007-04-04 15:56:18 +0000

    Add a trivial test for the debug infrastructure
    20070404155618-53eee-f8df7ed5fe842fb52873c7396355f08b48e2d810.gz

---
+++ lib/tests/test-internal-debug.c
@@ -0,0 +1,78 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <glib.h>
+
+#include <telepathy-glib/debug.h>
+
+#undef DEBUG_FLAG
+#define DEBUG_FLAG TP_DEBUG_IM
+#include "telepathy-glib/internal-debug.h"
+
+static void
+test_debugging (void)
+{
+#ifndef DEBUG
+#error internal-debug.h should always define DEBUG
+#endif
+
+#ifndef DEBUGGING
+#error internal-debug.h should always define DEBUGGING
+#endif
+
+#ifdef ENABLE_DEBUG
+  g_assert (DEBUGGING == 1);
+#else
+  g_assert (DEBUGGING == 0);
+#endif
+}
+
+#undef DEBUG_FLAG
+#define DEBUG_FLAG TP_DEBUG_CONNECTION
+#include "telepathy-glib/internal-debug.h"
+
+static void
+test_not_debugging (void)
+{
+#ifndef DEBUG
+#error internal-debug.h should always define DEBUG
+#endif
+
+#ifndef DEBUGGING
+#error internal-debug.h should always define DEBUGGING
+#endif
+
+  g_assert (DEBUGGING == 0);
+}
+
+#undef DEBUG_FLAG
+#define DEBUG_FLAG TP_DEBUG_IM
+#include "telepathy-glib/internal-debug.h"
+
+static void
+test_debugging_again (void)
+{
+#ifndef DEBUG
+#error internal-debug.h should always define DEBUG
+#endif
+
+#ifndef DEBUGGING
+#error internal-debug.h should always define DEBUGGING
+#endif
+
+#ifdef ENABLE_DEBUG
+  g_assert (DEBUGGING == 1);
+#else
+  g_assert (DEBUGGING == 0);
+#endif
+}
+
+int
+main (int argc, char **argv)
+{
+  /* We enable debugging for IM, but not for the connection. */
+  tp_debug_set_flags_from_string ("im");
+  test_debugging();
+  test_not_debugging();
+  test_debugging_again();
+}

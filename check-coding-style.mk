31f9c4e348c6cddfcd472059f664a6ad2de18fa8
Simon McVittie <simon.mcvittie@collabora.co.uk>
2007-04-06 17:01:10 +0000

    Make coding style violations not fail "make check" unless CHECK_CODING_STYLE is non-empty (changing everything to correct coding style is likely to cause hideous conflicts right now)
    20070406170110-53eee-54453768bd05de3baa014cd117bf87baedb37a42.gz

--- check-coding-style.mk
+++ check-coding-style.mk
@@ -1,9 +1,15 @@
 check-local::
+	@fail=0; \
 	if test -n "$(check_misc_sources)"; then \
 		top_srcdir=$(top_srcdir) sh $(top_srcdir)/check-whitespace.sh \
-			$(check_misc_sources); \
-	fi
+			$(check_misc_sources) || fail=1; \
+	fi; \
 	if test -n "$(check_c_sources)"; then \
 		top_srcdir=$(top_srcdir) sh $(top_srcdir)/check-c-style.sh \
-			$(check_c_sources); \
+			$(check_c_sources) || fail=1; \
+	fi;\
+	if test -n "$(CHECK_CODING_STYLE)"; then \
+		exit "$$fail";\
+	else \
+		exit 0;\
 	fi

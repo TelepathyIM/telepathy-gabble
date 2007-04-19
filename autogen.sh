80779b7ba64997265afd016366c8651150e32059
Simon McVittie <simon.mcvittie@collabora.co.uk>
2007-04-19 16:02:25 +0000

    Use ${MAKE} -C instead of make -C
    20070419160225-53eee-36fc85408153652eb80bbd001e2a65c7f3aa8db3.gz

--- autogen.sh
+++ autogen.sh
@@ -1,10 +1,15 @@
 #!/bin/sh
 set -e
 
+if test -z "$MAKE"
+then
+	MAKE=make
+fi
+
 gtkdocize
 
 ( cd lib/spec && sh ../tools/update-spec-gen-am.sh spec-gen.am )
-make -C lib/telepathy-glib -f stable-interfaces.mk _gen/stable-interfaces.txt
+$MAKE -C lib/telepathy-glib -f stable-interfaces.mk _gen/stable-interfaces.txt
 ( cd lib/telepathy-glib && sh ../tools/update-spec-gen-am.sh _gen/spec-gen.am _gen _gen/stable-interfaces.txt )
 
 autoreconf -i

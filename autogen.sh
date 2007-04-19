b460017efd9356676831ece5057ad41bd1dd57f7
Simon McVittie <simon.mcvittie@collabora.co.uk>
2007-04-19 15:57:47 +0000

    Generate telepathy-glib's spec-gen.am inside _gen. Remove the old spec-gen.am
    20070419155747-53eee-669bc2eb18018f610ddfbd0815261ac4a08b9403.gz

--- autogen.sh
+++ autogen.sh
@@ -5,7 +5,7 @@ gtkdocize
 
 ( cd lib/spec && sh ../tools/update-spec-gen-am.sh spec-gen.am )
 make -C lib/telepathy-glib -f stable-interfaces.mk _gen/stable-interfaces.txt
-( cd lib/telepathy-glib && sh ../tools/update-spec-gen-am.sh spec-gen.am _gen _gen/stable-interfaces.txt )
+( cd lib/telepathy-glib && sh ../tools/update-spec-gen-am.sh _gen/spec-gen.am _gen _gen/stable-interfaces.txt )
 
 autoreconf -i
 

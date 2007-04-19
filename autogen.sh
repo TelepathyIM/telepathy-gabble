3f6acd3553948e05d8f8c2d2a22cdc7c2011599d
Simon McVittie <simon.mcvittie@collabora.co.uk>
2007-04-19 18:09:38 +0000

    Remove telepathy-glib from build system, use an external copy instead
    20070419180938-53eee-0617e942744c8271e047bbd10e1e97503b36b80d.gz

--- autogen.sh
+++ autogen.sh
@@ -1,17 +1,6 @@
 #!/bin/sh
 set -e
 
-if test -z "$MAKE"
-then
-	MAKE=make
-fi
-
-gtkdocize
-
-( cd lib/spec && sh ../tools/update-spec-gen-am.sh spec-gen.am )
-$MAKE -C lib/telepathy-glib -f stable-interfaces.mk _gen/stable-interfaces.txt
-( cd lib/telepathy-glib && sh ../tools/update-spec-gen-am.sh _gen/spec-gen.am _gen _gen/stable-interfaces.txt )
-
 autoreconf -i
 
 ./configure "$@"

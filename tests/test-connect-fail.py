4f8cedb0da737f92fe06d0ce18e1c83a29f9ff61
Dafydd Harries <dafydd.harries@collabora.co.uk>
2007-05-15 17:38:36 +0000

    add test for connection failure
    20070515173836-c9803-a88da13657955fa4ee7445fb9b268aa6358944b0.gz

---
+++ tests/test-connect-fail.py
@@ -0,0 +1,38 @@
+
+"""
+Test network error handling.
+"""
+
+import dbus
+
+from twisted.internet import glib2reactor
+glib2reactor.install()
+
+from gabbletest import go
+
+def expect_connecting(event, data):
+    if event[0] != 'dbus-signal':
+        return False
+
+    if event[2] != 'StatusChanged':
+        return False
+
+    if event[3] != [1, 1]:
+        return False
+
+    return True
+
+def expect_disconnected(event, data):
+    if event[0] != 'dbus-signal':
+        return False
+
+    if event[2] != 'StatusChanged':
+        return False
+
+    # status: disconnected / reason: network error
+    assert event[3] == [2, 2]
+    return True
+
+if __name__ == '__main__':
+    go({'port': dbus.UInt32(4243)})
+

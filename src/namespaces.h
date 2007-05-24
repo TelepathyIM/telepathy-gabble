e0981028fcfee6efc9e7060fc9f149193a9dddd2
Guillaume Desmottes <guillaume.desmottes@collabora.co.uk>
2007-05-24 09:23:41 +0000

    fix NS_SI alignement
    20070524092341-7fe3f-631d31becf2cd4b44efe493b3a39e0ce8eeeeb02.gz

--- src/namespaces.h
+++ src/namespaces.h
@@ -63,7 +63,7 @@
 #define NS_PRIVACY              "jabber:iq:privacy"
 #define NS_REGISTER             "jabber:iq:register"
 #define NS_ROSTER               "jabber:iq:roster"
-#define NS_SI                  "http://jabber.org/protocol/si"
+#define NS_SI                   "http://jabber.org/protocol/si"
 #define NS_SI_TUBES             "http://jabber.org/protocol/si/profile/tubes"
 #define NS_TUBES                "http://telepathy.freedesktop.org/xmpp/tubes"
 #define NS_VCARD_TEMP           "vcard-temp"

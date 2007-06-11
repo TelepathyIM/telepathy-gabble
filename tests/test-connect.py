0599c62cf939cfd0e657a035e4f99cf90408c8a4
Dafydd Harries <dafydd.harries@collabora.co.uk>
2007-06-11 16:39:31 +0000

    test-connect.py: use @match
    20070611163931-c9803-04ac0d14cfbc27f55f85010c4ef02a423575fb3f.gz

--- tests/test-connect.py
+++ tests/test-connect.py
@@ -3,60 +3,30 @@
 Test connecting to a server.
 """
 
+from servicetest import match
 from gabbletest import go
 
+@match('dbus-signal', signal='StatusChanged', args=[1, 1])
 def expect_connecting(event, data):
-    if event.type != 'dbus-signal':
-        return False
-
-    if event.signal != 'StatusChanged':
-        return False
-
-    if event.args != [1, 1]:
-        return False
-
     return True
 
+@match('stream-authenticated')
 def expect_authenticated(event, data):
-    if event.type != 'stream-authenticated':
-        return False
-
     return True
 
+@match('dbus-signal', signal='PresenceUpdate')
 def expect_presence_update(event, data):
-    if event.type != 'dbus-signal':
-        return False
-
-    if event.signal != 'PresenceUpdate':
-        return False
-
     # expecting presence update for self handle
     assert event.args == [{1L: (0L, {u'available': {}})}]
     return True
 
+@match('dbus-signal', signal='StatusChanged', args=[0, 1])
 def expect_connected(event, data):
-    if event.type != 'dbus-signal':
-        return False
-
-    if event.signal != 'StatusChanged':
-        return False
-
-    if event.args != [0, 1]:
-        return False
-
     data['conn_iface'].Disconnect()
     return True
 
+@match('dbus-signal', signal='StatusChanged', args=[2, 1])
 def expect_disconnected(event, data):
-    if event.type != 'dbus-signal':
-        return False
-
-    if event.signal != 'StatusChanged':
-        return False
-
-    if event.args != [2, 1]:
-        return False
-
     return True
 
 if __name__ == '__main__':

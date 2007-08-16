002017a5aedd406d5c7b126b0b7dd5ab4dff86ba
Simon McVittie <simon.mcvittie@collabora.co.uk>
2007-08-16 15:15:59 +0000

    gabble_tubes_channel_tube_offered: assert that the message is an IQ with type="set"
    20070816151559-53eee-37eb3b6535c48273f54ca98141e38e5b40f987e0.gz

--- src/tubes-channel.c
+++ src/tubes-channel.c
@@ -1178,6 +1178,10 @@ gabble_tubes_channel_tube_offered (GabbleTubesChannel *self,
   /* Caller is expected to have checked that we have a SI node with
    * a stream ID and the TUBES (or TUBES_OLD) profile
    */
+  g_return_val_if_fail (lm_message_get_type (msg) == LM_MESSAGE_TYPE_IQ,
+      FALSE);
+  g_return_val_if_fail (lm_message_get_type (msg) ==
+      LM_MESSAGE_TYPE_SUB_TYPE_SET, FALSE);
   si_node = lm_message_node_get_child_with_namespace (msg->node, "si",
       NS_SI);
   g_return_val_if_fail (si_node != NULL, FALSE);

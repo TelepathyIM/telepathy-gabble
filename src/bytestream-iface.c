7d154d80a7f14c80bb6b69313004d1f5b52c00e4
Guillaume Desmottes <guillaume.desmottes@collabora.co.uk>
2007-08-30 12:31:49 +0000

    add bytestream-iface.[ch]
    20070830123149-7fe3f-0b3a19db2397271a7a1a69c77d69bd3dd7ff0697.gz

---
+++ src/bytestream-iface.c
@@ -0,0 +1,104 @@
+/*
+ * bytestream-iface.c - Source for GabbleBytestream interface
+ * Copyright (C) 2007 Ltd.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+#include "bytestream-iface.h"
+
+#include <glib.h>
+
+gboolean
+gabble_bytestream_iface_initiation (GabbleBytestreamIface *self)
+{
+  gboolean (*virtual_method)(GabbleBytestreamIface *) =
+    GABBLE_BYTESTREAM_IFACE_GET_CLASS (self)->initiation;
+  g_assert (virtual_method != NULL);
+  return virtual_method (self);
+}
+
+gboolean
+gabble_bytestream_iface_send (GabbleBytestreamIface *self,
+                              guint len,
+                              gchar *data)
+{
+  gboolean (*virtual_method)(GabbleBytestreamIface *, guint, gchar *) =
+    GABBLE_BYTESTREAM_IFACE_GET_CLASS (self)->send;
+  g_assert (virtual_method != NULL);
+  return virtual_method (self, len, data);
+}
+
+void
+gabble_bytestream_iface_close (GabbleBytestreamIface *self)
+{
+  void (*virtual_method)(GabbleBytestreamIface *) =
+    GABBLE_BYTESTREAM_IFACE_GET_CLASS (self)->close;
+  g_assert (virtual_method != NULL);
+  virtual_method (self);
+}
+
+void
+gabble_bytestream_iface_accept (GabbleBytestreamIface *self,
+                                LmMessage *msg)
+{
+  void (*virtual_method)(GabbleBytestreamIface *, LmMessage *) =
+    GABBLE_BYTESTREAM_IFACE_GET_CLASS (self)->accept;
+  g_assert (virtual_method != NULL);
+  virtual_method (self, msg);
+}
+
+const gchar *
+gabble_bytestream_iface_get_protocol (GabbleBytestreamIface *self)
+{
+  const gchar * (*virtual_method)(GabbleBytestreamIface *) =
+    GABBLE_BYTESTREAM_IFACE_GET_CLASS (self)->get_protocol;
+  g_assert (virtual_method != NULL);
+  return virtual_method (self);
+}
+
+LmMessage *
+gabble_bytestream_iface_make_accept_iq (GabbleBytestreamIface *self)
+{
+  LmMessage * (*virtual_method)(GabbleBytestreamIface *) =
+    GABBLE_BYTESTREAM_IFACE_GET_CLASS (self)->make_accept_iq;
+  g_assert (virtual_method != NULL);
+  return virtual_method (self);
+}
+
+GType
+gabble_bytestream_iface_get_type (void)
+{
+  static GType type = 0;
+
+  if (type == 0) {
+    static const GTypeInfo info = {
+      sizeof (GabbleBytestreamIfaceClass),
+      NULL,   /* base_init */
+      NULL,   /* base_finalize */
+      NULL,   /* class_init */
+      NULL,   /* class_finalize */
+      NULL,   /* class_data */
+      0,
+      0,      /* n_preallocs */
+      NULL    /* instance_init */
+    };
+
+    type = g_type_register_static (G_TYPE_INTERFACE, "GabbleBytestreamIface",
+        &info, 0);
+  }
+
+  return type;
+}
---
+++ src/bytestream-iface.h
@@ -0,0 +1,94 @@
+/*
+ * bytestream-iface.h - Header for GabbleBytestream interface
+ * Copyright (C) 2007 Collabora Ltd.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+#ifndef __GABBLE_BYTESTREAM_IFACE_H__
+#define __GABBLE_BYTESTREAM_IFACE_H__
+
+#include <glib-object.h>
+#include <loudmouth/loudmouth.h>
+
+G_BEGIN_DECLS
+
+typedef enum
+{
+  /* Received a SI request, response not yet sent */
+  GABBLE_BYTESTREAM_STATE_LOCAL_PENDING = 0,
+  /* We accepted SI request.
+   * bytestream specific init steps not yet performed */
+  GABBLE_BYTESTREAM_STATE_ACCEPTED,
+  /* Remote contact accepted the SI request.
+   * bytestream specific initiation started */
+  GABBLE_BYTESTREAM_STATE_INITIATING,
+  /* Bytestream open */
+  GABBLE_BYTESTREAM_STATE_OPEN,
+  GABBLE_BYTESTREAM_STATE_CLOSED,
+  LAST_GABBLE_BYTESTREAM_STATE,
+} GabbleBytestreamState;
+
+typedef struct _GabbleBytestreamIface GabbleBytestreamIface;
+typedef struct _GabbleBytestreamIfaceClass GabbleBytestreamIfaceClass;
+
+struct _GabbleBytestreamIfaceClass {
+  GTypeInterface parent;
+
+  gboolean (*initiation) (GabbleBytestreamIface *bytestream);
+  gboolean (*send) (GabbleBytestreamIface *bytestream, guint len, gchar *data);
+  void (*close) (GabbleBytestreamIface *bytestream);
+  void (*accept) (GabbleBytestreamIface *bytestream, LmMessage *msg);
+  const gchar * (*get_protocol) (GabbleBytestreamIface *bytestream);
+  LmMessage * (*make_accept_iq) (GabbleBytestreamIface *bytestream);
+};
+
+GType gabble_bytestream_iface_get_type (void);
+
+/* TYPE MACROS */
+#define GABBLE_TYPE_BYTESTREAM_IFACE \
+  (gabble_bytestream_iface_get_type ())
+#define GABBLE_BYTESTREAM_IFACE(obj) \
+  (G_TYPE_CHECK_INSTANCE_CAST((obj), GABBLE_TYPE_BYTESTREAM_IFACE, \
+                              GabbleBytestreamIface))
+#define GABBLE_IS_BYTESTREAM_IFACE(obj) \
+  (G_TYPE_CHECK_INSTANCE_TYPE((obj), GABBLE_TYPE_BYTESTREAM_IFACE))
+#define GABBLE_BYTESTREAM_IFACE_GET_CLASS(obj) \
+  (G_TYPE_INSTANCE_GET_INTERFACE ((obj), GABBLE_TYPE_BYTESTREAM_IFACE,\
+                              GabbleBytestreamIfaceClass))
+
+gboolean
+gabble_bytestream_iface_initiation (GabbleBytestreamIface *bytestream);
+
+gboolean
+gabble_bytestream_iface_send (GabbleBytestreamIface *bytestream, guint len,
+   gchar *data);
+
+void
+gabble_bytestream_iface_close (GabbleBytestreamIface *bytestream);
+
+void
+gabble_bytestream_iface_accept (GabbleBytestreamIface *bytestream,
+    LmMessage *msg);
+
+const gchar *
+gabble_bytestream_iface_get_protocol (GabbleBytestreamIface *bytestream);
+
+LmMessage *
+gabble_bytestream_iface_make_accept_iq (GabbleBytestreamIface *bytestream);
+
+G_END_DECLS
+
+#endif /* #ifndef __GABBLE_BYTESTREAM_IFACE_H__ */

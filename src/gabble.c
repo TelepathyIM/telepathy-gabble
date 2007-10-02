467873d2fdc6afb0b15d76c65d95d6127472127c
Dafydd Harries <dafydd.harries@collabora.co.uk>
2007-10-02 17:31:59 +0000

    add timestamps to debug messages if GABBLE_TIMING env variable is set
    20071002173159-c9803-c2a694a7fd08dde10532c21b0b971f141e370edc.gz

--- src/gabble.c
+++ src/gabble.c
@@ -18,6 +18,9 @@
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  */
 
+#include <time.h>
+#include <unistd.h>
+
 #include <telepathy-glib/debug.h>
 #include <telepathy-glib/run.h>
 #include "debug.h"
@@ -30,6 +33,27 @@ construct_cm (void)
       GABBLE_TYPE_CONNECTION_MANAGER, NULL);
 }
 
+#ifdef ENABLE_DEBUG
+static void
+stamp_log (const gchar *log_domain,
+           GLogLevelFlags log_level,
+           const gchar *message,
+           gpointer user_data)
+{
+  GTimeVal now;
+  gchar now_str[32];
+  gchar *tmp;
+  struct tm tm;
+
+  g_get_current_time (&now);
+  localtime_r (&(now.tv_sec), &tm);
+  strftime (now_str, 32, "%Y-%m-%d %H:%M:%S", &tm);
+  tmp = g_strdup_printf ("%s.%06ld: %s", now_str, now.tv_usec, message);
+  g_log_default_handler (log_domain, log_level, tmp, NULL);
+  g_free (tmp);
+}
+#endif
+
 int
 main (int argc,
       char **argv)
@@ -38,6 +62,9 @@ main (int argc,
 
 #ifdef ENABLE_DEBUG
   gabble_debug_set_flags_from_env ();
+
+  if (g_getenv ("GABBLE_TIMING"))
+    g_log_set_handler (NULL, G_LOG_LEVEL_DEBUG, stamp_log, NULL);
 
   /* backwards compatibility */
   if (g_getenv ("GABBLE_PERSIST"))

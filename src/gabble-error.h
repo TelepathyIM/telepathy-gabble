2c86ddb242c5a384b11f99b430f2b399df640544
Guillaume Desmottes <guillaume.desmottes@collabora.co.uk>
2007-10-18 12:10:24 +0000

    gabble-error: update copyright and drop @author
    20071018121024-7fe3f-fcfb8b9ab51be8633fb015f0e62cbe1eec9faf26.gz

--- src/gabble-error.h
+++ src/gabble-error.h
@@ -1,8 +1,7 @@
 /*
  * gabble-error.h - Header for Gabble's error handling API
- * Copyright (C) 2006 Collabora Ltd.
+ * Copyright (C) 2006-2007 Collabora Ltd.
  * Copyright (C) 2006 Nokia Corporation
- *   @author Ole Andre Vadla Ravnaas <ole.andre.ravnaas@collabora.co.uk>
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public

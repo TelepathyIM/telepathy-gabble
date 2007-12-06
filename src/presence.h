cb10a17dedc8886ce482c8f57c3e80736bb79340
Guillaume Desmottes <guillaume.desmottes@collabora.co.uk>
2007-12-06 10:27:54 +0000

    move GabblePresenceCapabilities from presence.h to gabble-types.h as we'll need it in gabble-connection.h
    20071206102754-7fe3f-191144a8d11f9f25a7ad12d9dc436774c5966d5a.gz

--- src/presence.h
+++ src/presence.h
@@ -51,22 +51,6 @@ G_BEGIN_DECLS
     (G_TYPE_INSTANCE_GET_CLASS ((obj), \
                                   GABBLE_TYPE_PRESENCE, GabblePresenceClass))
 
-typedef enum {
-    PRESENCE_CAP_NONE = 0,
-    PRESENCE_CAP_GOOGLE_TRANSPORT_P2P = 1 << 0,
-    PRESENCE_CAP_GOOGLE_VOICE = 1 << 1,
-    PRESENCE_CAP_JINGLE = 1 << 2,
-    PRESENCE_CAP_JINGLE_DESCRIPTION_AUDIO = 1 << 3,
-    PRESENCE_CAP_JINGLE_DESCRIPTION_VIDEO = 1 << 4,
-    PRESENCE_CAP_JINGLE_H263_N800_HACK = 1 << 5,
-    PRESENCE_CAP_CHAT_STATES = 1 << 6,
-    PRESENCE_CAP_SI = 1 << 7,
-    PRESENCE_CAP_BYTESTREAMS = 1 << 8,
-    PRESENCE_CAP_IBB = 1 << 9,
-    PRESENCE_CAP_SI_TUBES = 1 << 10,
-    PRESENCE_CAP_FILE_TRANSFER = 1 << 11,
-} GabblePresenceCapabilities;
-
 struct _GabblePresence {
     GObject parent;
     GabblePresenceCapabilities caps;

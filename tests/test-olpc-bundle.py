270a845644cbae9df17f4cb2e302b920f89e15e9
Guillaume Desmottes <guillaume.desmottes@collabora.co.uk>
2007-12-12 15:23:55 +0000

    test-olpc-bundle.py: updated to use new test API
    20071212152355-7fe3f-8544c98cba3f685f25f30c9557f9a4a06fdd06d2.gz

--- tests/test-olpc-bundle.py
+++ tests/test-olpc-bundle.py
@@ -6,14 +6,10 @@ import os
 
 import dbus
 
-# must come before the twisted imports due to side-effects
-from gabbletest import go, make_result_iq
-from servicetest import call_async, lazy, match, tp_name_prefix, unwrap, Event
+from servicetest import call_async, EventPattern
+from gabbletest import exec_test, make_result_iq, acknowledge_iq
 
-from twisted.internet.protocol import Factory, Protocol
-from twisted.words.protocols.jabber.client import IQ
 from twisted.words.xish import domish, xpath
-from twisted.internet import reactor
 
 NS_OLPC_BUDDY_PROPS_NOTIFY = "http://laptop.org/xmpp/buddy-properties+notify"
 NS_OLPC_ACTIVITIES_NOTIFY = "http://laptop.org/xmpp/activities+notify"
@@ -23,8 +19,15 @@ NS_OLPC_ACTIVITY_PROPS_NOTIFY = "http://laptop.org/xmpp/activity-properties+noti
 olpc_features = set([NS_OLPC_BUDDY_PROPS_NOTIFY, NS_OLPC_ACTIVITIES_NOTIFY,
         NS_OLPC_CURRENT_ACTIVITY_NOTIFY, NS_OLPC_ACTIVITY_PROPS_NOTIFY])
 
-@match('dbus-signal', signal='StatusChanged', args=[0, 1])
-def expect_connected(event, data):
+def test(q, bus, conn, stream):
+    conn.Connect()
+
+    _, iq_event = q.expect_many(
+        EventPattern('dbus-signal', signal='StatusChanged', args=[0, 1]),
+        EventPattern('stream-iq', to=None, query_ns='vcard-temp',
+            query_name='vCard'))
+
+    acknowledge_iq(stream, iq_event.stanza)
 
     # send diso request
     m = domish.Element(('', 'iq'))
@@ -32,16 +35,12 @@ def expect_connected(event, data):
     m['id'] = '1'
     query = m.addElement('query')
     query['xmlns'] = 'http://jabber.org/protocol/disco#info'
-    data['stream'].send(m)
+    stream.send(m)
 
-    return True
-
-@match('stream-iq', iq_type='result',
-    query_ns='http://jabber.org/protocol/disco#info',
-    to='alice@jabber.laptop.org')
-def expect_first_disco_reply(event, data):
-    if event.stanza['id'] != '1':
-        return False
+    # wait for disco response
+    event = q.expect('stream-iq', iq_type='result',
+            query_ns='http://jabber.org/protocol/disco#info',
+            to='alice@jabber.laptop.org')
 
     features = set([str(f['var']) for f in xpath.queryForNodes('/iq/query/feature',
         event.stanza)])
@@ -50,16 +49,12 @@ def expect_first_disco_reply(event, data):
     assert len(olpc_features.intersection(features)) == 0
 
     # Use OLPC interface
-    data['olpc_iface'] = dbus.Interface(data['conn'],
-            'org.laptop.Telepathy.BuddyInfo')
-    call_async(data['test'], data['olpc_iface'], 'SetProperties',
+    buddy_info_iface = dbus.Interface(conn, 'org.laptop.Telepathy.BuddyInfo')
+    call_async(q, buddy_info_iface, 'SetProperties',
             {'color': '#ff0000,#0000ff'})
 
-    return True
-
-@match('stream-presence')
-def expect_bundle_presence(event, data):
-
+    # wait for <presence> stanza
+    event = q.expect('stream-presence')
     c_nodes = xpath.queryForNodes('/presence/c', event.stanza)
     assert c_nodes is not None
     assert len(c_nodes) == 1
@@ -73,16 +68,13 @@ def expect_bundle_presence(event, data):
     m['id'] = '2'
     query = m.addElement('query')
     query['xmlns'] = 'http://jabber.org/protocol/disco#info'
-    data['stream'].send(m)
+    stream.send(m)
 
-    return True
-
-@match('stream-iq', iq_type='result',
-    query_ns='http://jabber.org/protocol/disco#info',
-    to='alice@jabber.laptop.org')
-def expect_snd_disco_reply(event, data):
-    if event.stanza['id'] != '2':
-        return False
+    # wait for disco response
+    event = q.expect('stream-iq', iq_type='result',
+        query_ns='http://jabber.org/protocol/disco#info',
+        to='alice@jabber.laptop.org')
+    assert event.stanza['id'] == '2'
 
     # OLPC NS are now announced
     features = set([str(f['var']) for f in xpath.queryForNodes('/iq/query/feature',
@@ -90,7 +82,5 @@ def expect_snd_disco_reply(event, data):
 
     assert olpc_features.issubset(features)
 
-    return True
-
 if __name__ == '__main__':
-    go()
+    exec_test(test)

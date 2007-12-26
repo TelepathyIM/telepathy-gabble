b04c022ed4ef83acb7614c1cfd3c52727f3a430b
Dafydd Harries <dafydd.harries@collabora.co.uk>
2007-12-26 16:57:37 +0000

    port test-avatar.py to new test API
    20071226165737-c9803-25ec39eddc772fbf9b27ffe6bc92bfc853f9e5c9.gz

--- tests/test-avatar.py
+++ tests/test-avatar.py
@@ -5,68 +5,35 @@ Test avatar support.
 
 import base64
 
-import dbus
-from servicetest import tp_name_prefix, call_async, match, lazy
-from gabbletest import go
+from servicetest import call_async, EventPattern
+from gabbletest import exec_test, acknowledge_iq, make_result_iq
 
-def avatars_iface(proxy):
-    return dbus.Interface(proxy, tp_name_prefix +
-        '.Connection.Interface.Avatars')
+def test(q, bus, conn, stream):
+    conn.Connect()
+    _, event = q.expect_many(
+        EventPattern('dbus-signal', signal='StatusChanged', args=[0, 1]),
+        EventPattern('stream-iq', to=None, query_ns='vcard-temp',
+            query_name='vCard'))
 
-@lazy
-@match('dbus-signal', signal='StatusChanged', args=[0, 1])
-def expect_connected(event, data):
-    handle = data['conn_iface'].RequestHandles(1, ['bob@foo.com'])[0]
-    call_async(data['test'], avatars_iface(data['conn']), 'RequestAvatar',
-        handle, byte_arrays=True)
-    return True
+    acknowledge_iq(stream, event.stanza)
 
-@match('stream-iq')
-def expect_my_vcard_iq(event, data):
-    iq = event.stanza
-    if iq.getAttribute('to') is not None:
-        return False
+    handle = conn.RequestHandles(1, ['bob@foo.com'])[0]
+    call_async(q, conn.Avatars, 'RequestAvatar', handle, byte_arrays=True)
 
-    vcard = list(iq.elements())[0]
-
-    if vcard.name != 'vCard':
-        return False
-
-    iq['type'] = 'result'
-    data['stream'].send(iq)
-    return True
-
-@match('stream-iq')
-def expect_vcard_iq(event, data):
-    iq = event.stanza
-
-    if iq.getAttribute('to') != 'bob@foo.com':
-        return False
-
-    vcard = list(iq.elements())[0]
-
-    if vcard.name != 'vCard':
-        return False
-
-    photo = vcard.addElement('PHOTO')
+    event = q.expect('stream-iq', iq_type='get', to='bob@foo.com',
+        query_ns='vcard-temp', query_name='vCard')
+    result = make_result_iq(stream, event.stanza)
+    photo = result.firstChildElement().addElement('PHOTO')
     photo.addElement('TYPE', content='image/png')
     photo.addElement('BINVAL', content=base64.b64encode('hello'))
+    stream.send(result)
 
-    iq['type'] = 'result'
-    data['stream'].send(iq)
-    return True
+    q.expect('dbus-return', method='RequestAvatar',
+        value=('hello', 'image/png'))
 
-@match('dbus-return', method='RequestAvatar')
-def expect_RequestAvatar_return(event, data):
-    assert event.value[0] == 'hello'
-    assert event.value[1] == 'image/png'
-    data['conn_iface'].Disconnect()
-    return True
-
-@match('dbus-signal', signal='StatusChanged', args=[2, 1])
-def expect_disconnected(event, data):
-    return True
+    conn.Disconnect()
+    q.expect('dbus-signal', signal='StatusChanged', args=[2, 1])
 
 if __name__ == '__main__':
-    go()
+    exec_test(test)
 

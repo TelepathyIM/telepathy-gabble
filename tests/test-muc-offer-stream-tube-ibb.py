32cb2d539e642663700b2393630a98f344912e47
Guillaume Desmottes <guillaume.desmottes@collabora.co.uk>
2007-12-26 14:51:55 +0000

    test-muc-offer-stream-tube-ibb.py: remove D-Bus tube test
    20071226145155-7fe3f-fc82edd1d69dc6c59da94ef6b74da23f5732ee82.gz

--- tests/test-muc-offer-stream-tube-ibb.py
+++ tests/test-muc-offer-stream-tube-ibb.py
@@ -1,12 +1,10 @@
-"""Test IBB tube support in the context of a MUC."""
+"""Test IBB stream tube support in the context of a MUC."""
 
 import base64
 import errno
 import os
 
 import dbus
-from dbus.connection import Connection
-from dbus.lowlevel import SignalMessage
 
 from servicetest import call_async, EventPattern, tp_name_prefix
 from gabbletest import exec_test, make_result_iq, acknowledge_iq
@@ -69,6 +67,7 @@ def test(q, bus, conn, stream):
     event = q.expect('dbus-return', method='RequestHandles')
     handles = event.value[0]
 
+    # request tubes channel
     call_async(q, conn, 'RequestChannel',
         tp_name_prefix + '.Channel.Type.Tubes', 2, handles[0], True)
 
@@ -149,7 +148,7 @@ def test(q, bus, conn, stream):
         assert tube['service'] == 'echo'
         assert not tube.hasAttribute('stream-id')
         assert not tube.hasAttribute('dbus-name')
-        #assert tube['id'] == str(stream_tube_id)
+        assert tube['id'] == str(stream_tube_id)
 
     params = {}
     parameter_nodes = xpath.queryForNodes('/tube/parameters/parameter', tube)
@@ -200,7 +199,7 @@ def test(q, bus, conn, stream):
     iq_event, _ = q.expect_many(
         EventPattern('stream-iq', iq_type='result'),
         EventPattern('dbus-signal', signal='StreamTubeNewConnection',
-            args=[stream_tube_id,bob_handle]))
+            args=[stream_tube_id, bob_handle]))
 
     # handle iq_event
     iq = iq_event.stanza
@@ -234,6 +233,7 @@ def test(q, bus, conn, stream):
     data_node.addContent(base64.b64encode('hello, world'))
     stream.send(message)
 
+    # the server socket echoed our message
     event = q.expect('stream-message', to='chat@conf.localhost/bob')
     message = event.stanza
 
@@ -245,117 +245,6 @@ def test(q, bus, conn, stream):
     assert ibb_data['sid'] == 'alpha'
     binary = base64.b64decode(str(ibb_data))
     assert binary == 'hello, world'
-
-    # OK, enough streams. Now try D-Bus
-    call_async(q, tubes_iface, 'OfferDBusTube',
-        'com.example.TestCase', sample_parameters)
-
-    new_tube_event, presence_event, offer_return_event, dbus_changed_event = \
-        q.expect_many(
-        EventPattern('dbus-signal', signal='NewTube'),
-        EventPattern('stream-presence', to='chat@conf.localhost/test'),
-        EventPattern('dbus-return', method='OfferDBusTube'),
-        EventPattern('dbus-signal', signal='DBusNamesChanged'))
-
-    # handle new_tube_event
-    dbus_tube_id = new_tube_event.args[0]
-    assert new_tube_event.args[1] == tubes_self_handle
-    assert new_tube_event.args[2] == 0       # DBUS
-    assert new_tube_event.args[3] == 'com.example.TestCase'
-    assert new_tube_event.args[4] == sample_parameters
-    assert new_tube_event.args[5] == 2       # OPEN
-
-    # handle offer_return_event
-    assert offer_return_event.value[0] == dbus_tube_id
-
-    # handle presence_event
-    presence = presence_event.stanza
-    x_nodes = xpath.queryForNodes('/presence/x[@xmlns="http://jabber.org/'
-            'protocol/muc"]', presence)
-    assert x_nodes is not None
-    assert len(x_nodes) == 1
-
-    tubes_nodes = xpath.queryForNodes('/presence/tubes[@xmlns="%s"]'
-        % NS_TUBES, presence)
-    assert tubes_nodes is not None
-    assert len(tubes_nodes) == 1
-
-    tube_nodes = xpath.queryForNodes('/tubes/tube', tubes_nodes[0])
-    assert tube_nodes is not None
-    assert len(tube_nodes) == 2
-    for tube in tube_nodes:
-        assert tube['type'] in ('dbus', 'stream')
-
-        if tube['type'] == 'dbus':
-            assert tube['initiator'] == 'chat@conf.localhost/test'
-            assert tube['service'] == 'com.example.TestCase'
-            dbus_stream_id = tube['stream-id']
-            #assert tube['dbus-name'] == my_bus_name
-            #assert tube['id'] == str(dbus_tube_id)
-
-    params = {}
-    parameter_nodes = xpath.queryForNodes('/tube/parameters/parameter', tube)
-    for node in parameter_nodes:
-        assert node['name'] not in params
-        params[node['name']] = (node['type'], str(node))
-    assert params == {'ay': ('bytes', 'aGVsbG8='),
-                      's': ('str', 'hello'),
-                      'i': ('int', '-123'),
-                      'u': ('uint', '123'),
-                     }
-
-    # handle dbus_changed_event
-    assert dbus_changed_event.args[0] == dbus_tube_id
-    assert dbus_changed_event.args[1][0][0] == tubes_self_handle
-    my_bus_name = dbus_changed_event.args[1][0][1]
-
-    # handle offer_return_event
-    assert dbus_tube_id == offer_return_event.value[0]
-
-    tubes = tubes_iface.ListTubes(byte_arrays=True)
-    assert sorted(tubes) == sorted([(
-        dbus_tube_id,
-        tubes_self_handle,
-        0,      # DBUS
-        'com.example.TestCase',
-        sample_parameters,
-        2,      # OPEN
-        ),(
-        stream_tube_id,
-        tubes_self_handle,
-        1,      # stream
-        'echo',
-        sample_parameters,
-        2,      # OPEN
-        )])
-
-    dbus_tube_adr = tubes_iface.GetDBusTubeAddress(dbus_tube_id)
-    tube = Connection(dbus_tube_adr)
-    signal = SignalMessage('/', 'foo.bar', 'baz')
-    signal.append(42, signature='u')
-    tube.send_message(signal)
-
-    event = q.expect('stream-message', to='chat@conf.localhost',
-        message_type='groupchat')
-    message = event.stanza
-
-    data_nodes = xpath.queryForNodes('/message/data[@xmlns="%s"]' % NS_MUC_BYTESTREAM,
-        message)
-    assert data_nodes is not None
-    assert len(data_nodes) == 1
-    ibb_data = data_nodes[0]
-    assert ibb_data['sid'] == dbus_stream_id
-    binary = base64.b64decode(str(ibb_data))
-    # little and big endian versions of: SIGNAL, NO_REPLY, protocol v1,
-    # 4-byte payload
-    assert binary.startswith('l\x04\x01\x01' '\x04\x00\x00\x00') or \
-           binary.startswith('B\x04\x01\x01' '\x00\x00\x00\x04')
-    # little and big endian versions of the 4-byte payload, UInt32(42)
-    assert (binary[0] == 'l' and binary.endswith('\x2a\x00\x00\x00')) or \
-           (binary[0] == 'B' and binary.endswith('\x00\x00\x00\x2a'))
-    # XXX: verify that it's actually in the "sender" slot, rather than just
-    # being in the message somewhere
-    assert my_bus_name in binary
 
     # OK, we're done
     conn.Disconnect()

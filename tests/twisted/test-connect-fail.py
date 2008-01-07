6c511c3017e7556c342e2c20ce3434cd6d931df2
Dafydd Harries <dafydd.harries@collabora.co.uk>
2008-01-07 13:19:56 +0000

    port test-connect-fail.py to new test API
    20080107131956-c9803-ff738e5f14baadd489015e27b4a36e7bd4bc19b8.gz

--- tests/twisted/test-connect-fail.py
+++ tests/twisted/test-connect-fail.py
@@ -5,31 +5,13 @@ Test network error handling.
 
 import dbus
 
-from gabbletest import go
+from gabbletest import exec_test
 
-def expect_connecting(event, data):
-    if event.type != 'dbus-signal':
-        return False
-
-    if event.signal != 'StatusChanged':
-        return False
-
-    if event.args != [1, 1]:
-        return False
-
-    return True
-
-def expect_disconnected(event, data):
-    if event.type != 'dbus-signal':
-        return False
-
-    if event.signal != 'StatusChanged':
-        return False
-
-    # status: disconnected / reason: network error
-    assert event.args == [2, 2]
-    return True
+def test(q, bus, conn, stream):
+    conn.Connect()
+    q.expect('dbus-signal', signal='StatusChanged', args=[1, 1])
+    q.expect('dbus-signal', signal='StatusChanged', args=[2, 2])
 
 if __name__ == '__main__':
-    go({'port': dbus.UInt32(4243)})
+    exec_test(test, {'port': dbus.UInt32(4243)})
 

d55886dcf9cb6100d265a2cc645ecba331f2978b
Dafydd Harries <dafydd.harries@collabora.co.uk>
2008-01-07 13:16:38 +0000

    port test-avatar-async.py to new test API
    20080107131638-c9803-c90de3d641b8e825cd279344033546005ac0b561.gz

--- tests/test-avatar-async.py
+++ tests/test-avatar-async.py
@@ -6,107 +6,49 @@ Test support for retrieving avatars asynchronously using RequestAvatars.
 import base64
 import sha
 
-import dbus
+from servicetest import EventPattern
+from gabbletest import exec_test, acknowledge_iq
 
-from servicetest import tp_name_prefix, match, lazy
-from gabbletest import go
+def test(q, bus, conn, stream):
+    conn.Connect()
+    _, iq_event = q.expect_many(
+        EventPattern('dbus-signal', signal='StatusChanged', args=[0, 1]),
+        EventPattern('stream-iq', to=None, query_ns='vcard-temp',
+            query_name='vCard'))
 
-def avatars_iface(proxy):
-    return dbus.Interface(proxy, tp_name_prefix +
-        '.Connection.Interface.Avatars')
+    acknowledge_iq(stream, iq_event.stanza)
 
-@lazy
-def expect_connected(event, data):
-    if event.type != 'dbus-signal':
-        return False
+    handle = conn.RequestHandles(1, ['bob@foo.com'])[0]
+    conn.Avatars.RequestAvatars([handle])
 
-    if event.signal != 'StatusChanged':
-        return False
-
-    if event.args != [0, 1]:
-        return False
-
-    handle = data['conn_iface'].RequestHandles(1, ['bob@foo.com'])[0]
-    data['handle'] = handle
-    avatars_iface(data['conn']).RequestAvatars([handle])
-    return True
-
-@match('stream-iq')
-def expect_my_vcard_iq(event, data):
-    iq = event.stanza
-    if iq.getAttribute('to') is not None:
-        return False
-
-    vcard = list(iq.elements())[0]
-
-    if vcard.name != 'vCard':
-        return False
-
-    iq['type'] = 'result'
-    data['stream'].send(iq)
-    return True
-
-@match('stream-iq')
-def expect_vcard_iq(event, data):
-    iq = event.stanza
-
-    if iq.getAttribute('to') != 'bob@foo.com':
-        return False
-
-    vcard = list(iq.elements())[0]
-
-    if vcard.name != 'vCard':
-        return False
-
+    iq_event = q.expect('stream-iq', to='bob@foo.com', query_ns='vcard-temp',
+        query_name='vCard')
+    iq = iq_event.stanza
+    vcard = iq_event.query
     photo = vcard.addElement('PHOTO')
     photo.addElement('TYPE', content='image/png')
     photo.addElement('BINVAL', content=base64.b64encode('hello'))
-
     iq['type'] = 'result'
-    data['stream'].send(iq)
-    return True
+    stream.send(iq)
 
-def expect_AvatarRetrieved(event, data):
-    if event.type != 'dbus-signal':
-        return False
-
-    if event.signal != 'AvatarRetrieved':
-        return False
-
-    assert event.args[0] == data['handle']
+    event = q.expect('dbus-signal', signal='AvatarRetrieved')
+    assert event.args[0] == handle
     assert event.args[1] == sha.sha('hello').hexdigest()
     assert event.args[2] == 'hello'
     assert event.args[3] == 'image/png'
 
     # Request again; this request should be satisfied from the avatar cache.
-    avatars_iface(data['conn']).RequestAvatars([data['handle']])
-    return True
+    conn.Avatars.RequestAvatars([handle])
 
-def expect_AvatarRetrieved_again(event, data):
-    # This event *must* be the AvatarRetrieved signal.
-    assert event.type == 'dbus-signal'
-    assert event.signal == 'AvatarRetrieved'
-
-    assert event.args[0] == data['handle']
+    event = q.demand('dbus-signal', signal='AvatarRetrieved')
+    assert event.args[0] == handle
     assert event.args[1] == sha.sha('hello').hexdigest()
     assert event.args[2] == 'hello'
     assert event.args[3] == 'image/png'
 
-    data['conn_iface'].Disconnect()
-    return True
-
-def expect_disconnected(event, data):
-    if event.type != 'dbus-signal':
-        return False
-
-    if event.signal != 'StatusChanged':
-        return False
-
-    if event.args != [2, 1]:
-        return False
-
-    return True
+    conn.Disconnect()
+    q.expect('dbus-signal', signal='StatusChanged', args=[2, 1])
 
 if __name__ == '__main__':
-    go()
+    exec_test(test)
 

1477e6ae45e12e94d2ebae889e5f4b654edd0e66
Dafydd Harries <dafydd.harries@collabora.co.uk>
2008-01-07 13:15:15 +0000

    port test-disco.py to new test API
    20080107131515-c9803-55ceb4b710f899c4855fa5cdbc021f6152ceecd7.gz

--- tests/test-disco.py
+++ tests/test-disco.py
@@ -5,52 +5,26 @@ Test that Gabble responds to disco#info queries.
 
 from twisted.words.xish import domish
 
-from gabbletest import go
+from gabbletest import exec_test
 
-def expect_connected(event, data):
-    if event.type != 'dbus-signal':
-        return False
-
-    if event.signal != 'StatusChanged':
-        return False
-
-    if event.args != [0, 1]:
-        return False
+def test(q, bus, conn, stream):
+    conn.Connect()
+    q.expect('dbus-signal', signal='StatusChanged', args=[0, 1])
 
     m = domish.Element(('', 'iq'))
     m['from'] = 'foo@bar.com'
     m['id'] = '1'
-    query = m.addElement('query')
-    query['xmlns'] = 'http://jabber.org/protocol/disco#info'
-    data['stream'].send(m)
+    m.addElement(('http://jabber.org/protocol/disco#info', 'query'))
+    stream.send(m)
     return True
 
-def expect_disco_response(event, data):
-    if event.type != 'stream-iq':
-        return False
-
+    event = q.expect('stream-iq', iq_type='result', to='foo@bar.com')
     elem = event.stanza
+    assert elem['id'] == '1'
 
-    if elem['id'] != '1':
-        return False
-
-    assert elem['type'] == 'result'
-    assert elem['to'] == 'foo@bar.com'
-    data['conn_iface'].Disconnect()
-    return True
-
-def expect_disconnected(event, data):
-    if event.type != 'dbus-signal':
-        return False
-
-    if event.signal != 'StatusChanged':
-        return False
-
-    if event.args != [2, 1]:
-        return False
-
-    return True
+    conn.Disconnect()
+    q.expect('dbus-signal', signal='StatusChanged', args=[2, 1])
 
 if __name__ == '__main__':
-    go()
+    exec_test(test)
 

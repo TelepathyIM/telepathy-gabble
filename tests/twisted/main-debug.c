92679182836897b9002ee3e6e9146a65293a0fc4
Guillaume Desmottes <guillaume.desmottes@collabora.co.uk>
2008-04-21 13:28:49 +0000

    run a tweaked version of Gabble with tests
    20080421132849-7fe3f-811a0d4aecb3eade992caa36d03a1cc299f20ed3.gz

---
+++ tests/twisted/main-debug.c
@@ -0,0 +1,31 @@
+/*
+ * main.c - entry point for telepathy-gabble-debug used by tests
+ * Copyright (C) 2008 Collabora Ltd.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+#include "gabble.h"
+#include "gabble-connection.h"
+
+int
+main (int argc,
+      char **argv)
+{
+  /* needed for test-disco-no-reply.py */
+  gabble_connection_set_disco_reply_timeout (3000);
+
+  return gabble_main (argc, argv);
+}

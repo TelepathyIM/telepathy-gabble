c158c82e696f8448b18c9ed6fd322660427c3665
Simon McVittie <simon.mcvittie@collabora.co.uk>
2008-04-23 11:36:47 +0000

    Add a regression test for roster groups
    20080423113647-53eee-88f64055934cb5c54613267847ca237a2798143a.gz

---
+++ tests/twisted/roster/groups.py
@@ -0,0 +1,75 @@
+
+"""
+Test basic roster functionality.
+"""
+
+import dbus
+
+from gabbletest import exec_test
+
+
+HT_CONTACT_LIST = 3
+HT_GROUP = 4
+
+
+def _expect_contact_list_channel(q, bus, conn, name, contacts):
+    event = q.expect('dbus-signal', signal='NewChannel')
+    path, type, handle_type, handle, suppress_handler = event.args
+    assert type == u'org.freedesktop.Telepathy.Channel.Type.ContactList'
+    assert handle_type == HT_CONTACT_LIST
+    assert conn.InspectHandles(handle_type, [handle])[0] == name
+    chan = bus.get_object(conn._named_service, path)
+    group_iface = dbus.Interface(chan,
+        u'org.freedesktop.Telepathy.Channel.Interface.Group')
+    assert conn.InspectHandles(1, group_iface.GetMembers()) == contacts
+
+def _expect_group_channel(q, bus, conn, name, contacts):
+    event = q.expect('dbus-signal', signal='NewChannel')
+    path, type, handle_type, handle, suppress_handler = event.args
+    assert type == u'org.freedesktop.Telepathy.Channel.Type.ContactList'
+    assert handle_type == HT_GROUP
+    assert conn.InspectHandles(handle_type, [handle])[0] == name
+    chan = bus.get_object(conn._named_service, path)
+    group_iface = dbus.Interface(chan,
+        u'org.freedesktop.Telepathy.Channel.Interface.Group')
+    assert conn.InspectHandles(1, group_iface.GetMembers()) == contacts
+
+def test(q, bus, conn, stream):
+    conn.Connect()
+    q.expect('dbus-signal', signal='StatusChanged', args=[0, 1])
+
+    event = q.expect('stream-iq', query_ns='jabber:iq:roster')
+    event.stanza['type'] = 'result'
+
+    item = event.query.addElement('item')
+    item['jid'] = 'amy@foo.com'
+    item['subscription'] = 'both'
+    group = item.addElement('group', content='women')
+
+    item = event.query.addElement('item')
+    item['jid'] = 'bob@foo.com'
+    item['subscription'] = 'from'
+    group = item.addElement('group', content='men')
+
+    item = event.query.addElement('item')
+    item['jid'] = 'che@foo.com'
+    item['subscription'] = 'to'
+    group = item.addElement('group', content='men')
+
+    stream.send(event.stanza)
+
+    _expect_contact_list_channel(q, bus, conn, 'publish',
+        ['amy@foo.com', 'bob@foo.com'])
+    _expect_contact_list_channel(q, bus, conn, 'subscribe',
+        ['amy@foo.com', 'che@foo.com'])
+    _expect_contact_list_channel(q, bus, conn, 'known',
+        ['amy@foo.com', 'bob@foo.com', 'che@foo.com'])
+    _expect_group_channel(q, bus, conn, 'women', ['amy@foo.com'])
+    _expect_group_channel(q, bus, conn, 'men', ['bob@foo.com', 'che@foo.com'])
+
+    conn.Disconnect()
+    q.expect('dbus-signal', signal='StatusChanged', args=[2, 1])
+
+if __name__ == '__main__':
+    exec_test(test)
+

49f97fda338131ad0d58817d088ff944e88ffbe7
Simon McVittie <simon.mcvittie@collabora.co.uk>
2008-04-30 17:32:37 +0000

    Amend muc/test-muc.py to match Alban's fix for fd.o #14467
    20080430173237-53eee-1833bf9b961ebd6f1d9aabd73ee84f90263caefd.gz

--- tests/twisted/muc/test-muc.py
+++ tests/twisted/muc/test-muc.py
@@ -51,13 +51,6 @@ def expect_presence(event, data):
     item['affiliation'] = 'owner'
     item['role'] = 'moderator'
     data['stream'].send(presence)
-    return True
-
-@match('dbus-signal', signal='MembersChanged',
-    args=[u'', [3], [], [], [], 0, 0])
-def expect_members_changed2(event, data):
-    assert data['conn_iface'].InspectHandles(1, [3]) == [
-        'chat@conf.localhost/bob']
 
     # Send presence for own membership of room.
     presence = domish.Element((None, 'presence'))
@@ -67,6 +60,16 @@ def expect_members_changed2(event, data):
     item['affiliation'] = 'none'
     item['role'] = 'participant'
     data['stream'].send(presence)
+    return True
+
+@match('dbus-signal', signal='MembersChanged',
+    args=[u'', [2, 3], [], [], [], 0, 0])
+def expect_members_changed2(event, data):
+    assert data['conn_iface'].InspectHandles(1, [2]) == [
+        'chat@conf.localhost/test']
+    assert data['conn_iface'].InspectHandles(1, [3]) == [
+        'chat@conf.localhost/bob']
+
     return True
 
 @match('dbus-return', method='RequestChannel')

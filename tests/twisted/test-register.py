e95d6cf63949610177169e106b48ee9e95816937
Dafydd Harries <dafydd.harries@collabora.co.uk>
2008-05-03 00:11:23 +0000

    port test-register.py to new test API
    20080503001123-c9803-7bd054a19b4327ca70de447356a5cd7a23f689e5.gz

--- tests/twisted/test-register.py
+++ tests/twisted/test-register.py
@@ -3,47 +3,31 @@
 Test registration.
 """
 
-from gabbletest import go
-from servicetest import match
+from gabbletest import exec_test, make_result_iq, acknowledge_iq
 
 from twisted.words.xish import xpath
-from twisted.words.protocols.jabber.client import IQ
 
-@match('dbus-signal', signal='StatusChanged', args=[1, 1])
-def expect_connecting(event, data):
-    return True
+def test(q, bus, conn, stream):
+    conn.Connect()
+    q.expect('dbus-signal', signal='StatusChanged', args=[1, 1])
 
-@match('stream-iq', query_ns='jabber:iq:register')
-def expect_register_iq1(event, data):
-    iq = event.stanza
-    result = IQ(data['stream'], "result")
-    result["id"] = iq["id"]
-    query = result.addElement('query')
-    query["xmlns"] = "jabber:iq:register"
+    event = q.expect('stream-iq', query_ns='jabber:iq:register')
+    result = make_result_iq(stream, event.stanza)
+    query = result.firstChildElement()
     query.addElement('username')
     query.addElement('password')
-    data['stream'].send(result)
-    return True
+    stream.send(result)
 
-@match('stream-iq')
-def expect_register_iq2(event, data):
+    event = q.expect('stream-iq')
     iq = event.stanza
     assert xpath.queryForString('/iq/query/username', iq) == 'test'
     assert xpath.queryForString('/iq/query/password', iq) == 'pass'
+    acknowledge_iq(stream, iq)
 
-    iq["type"] = "result"
-    data['stream'].send(iq)
-    return True
-
-@match('dbus-signal', signal='StatusChanged', args=[0, 1])
-def expect_connected(event, data):
-    data['conn_iface'].Disconnect()
-    return True
-
-@match('dbus-signal', signal='StatusChanged', args=[2, 1])
-def expect_disconnected(event, data):
-    return True
+    q.expect('dbus-signal', signal='StatusChanged', args=[0, 1])
+    conn.Disconnect()
+    q.expect('dbus-signal', signal='StatusChanged', args=[2, 1])
 
 if __name__ == '__main__':
-    go({'register': True})
+    exec_test(test, {'register': True})
 

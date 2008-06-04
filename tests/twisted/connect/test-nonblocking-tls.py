0a5ab42263c508cb46e59dfeab00b6b3c2591120
Alban Crequy <alban.crequy@collabora.co.uk>
2008-06-04 10:25:01 +0000

    test-nonblocking-tls.py: Fix message
    20080604102501-a41c0-7247e3117e165b2be38f9888857ef551ee6bbed7.gz

--- tests/twisted/connect/test-nonblocking-tls.py
+++ tests/twisted/connect/test-nonblocking-tls.py
@@ -21,7 +21,7 @@ NS_XMPP_TLS = 'urn:ietf:params:xml:ns:xmpp-tls'
 NS_XMPP_SASL = 'urn:ietf:params:xml:ns:xmpp-sasl'
 
 
-print "FIXME: test-connect-twice.py disabled due to a bug in Loudmouth:"
+print "FIXME: test-nonblocking-tls.py disabled due to a bug in Loudmouth:"
 print "       http://developer.imendio.com/issues/browse/LM-44"
 print "       https://bugs.freedesktop.org/show_bug.cgi?id=14341"
 # exiting 77 causes automake to consider the test to have been skipped

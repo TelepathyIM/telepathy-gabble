980c1d39e35230c69c7b84fc3f20151946700d47
Simon McVittie <simon.mcvittie@collabora.co.uk>
2008-06-09 13:01:36 +0000

    extensions/: generate fully up-to-date extensions, including GTypes and interface quarks
    20080609130136-53eee-c51eb58a63073d91c1cc1917a5375a97028d1bc4.gz

--- extensions/extensions.h
+++ extensions/extensions.h
@@ -1,10 +1,15 @@
-
 #ifndef _GABBLE_EXTENSIONS_H
 #define _GABBLE_EXTENSIONS_H
 
-#include "extensions/_gen/svc.h"
 #include "extensions/_gen/enums.h"
+#include "extensions/_gen/svc.h"
+
+G_BEGIN_DECLS
+
+#include "extensions/_gen/gtypes.h"
 #include "extensions/_gen/interfaces.h"
+
+G_END_DECLS
 
 #endif /* _GABBLE_EXTENSIONS_H */
 

7c3aa05a43e39f8c037af1e781504a0e024dd57b
Sjoerd Simons <sjoerd@luon.net>
2008-07-21 13:18:52 +0000

    Remove resources and capabilities that gone into either unknown, offline or error
    20080721131852-93b9a-d8f36f77de01e2480466587931a63777754c749d.gz

--- src/presence.c
+++ src/presence.c
@@ -283,8 +283,7 @@ gabble_presence_update (GabblePresence *presence,
   res = _find_resource (presence, resource);
 
   /* remove, create or update a Resource as appropriate */
-  if (GABBLE_PRESENCE_OFFLINE == status &&
-      NULL == status_message)
+  if (GABBLE_PRESENCE_OFFLINE >= status  && status <= GABBLE_PRESENCE_ERROR)
     {
       if (NULL != res)
         {

2d3029f8e43af0b5513d231c91b16838cd7963f5
Simon McVittie <simon.mcvittie@collabora.co.uk>
2008-07-22 16:32:37 +0000

    muc/roomlist.py: exercise the Requested property
    20080722163237-53eee-5cf61f90f8e370640e6713742942bf9ef12c4e41.gz

--- tests/twisted/muc/roomlist.py
+++ tests/twisted/muc/roomlist.py
@@ -63,6 +63,7 @@ def test(q, bus, conn, stream):
     future_props = chan.GetAll(
             'org.freedesktop.Telepathy.Channel.FUTURE',
             dbus_interface='org.freedesktop.DBus.Properties')
+    assert future_props['Requested'] == True
     assert future_props['TargetID'] == ''
     assert future_props['InitiatorID'] == 'test@localhost'
     assert future_props['InitiatorHandle'] == conn.GetSelfHandle()

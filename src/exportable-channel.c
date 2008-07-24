88917a86d25d2e9984642c658b55dfa95c019be6
Simon McVittie <simon.mcvittie@collabora.co.uk>
2008-07-24 17:27:00 +0000

    Add a straw-man API for requestotron-capable channels
    20080724172700-53eee-04e4eed48957dd258db10b6b571f766e90fd7290.gz

---
+++ src/exportable-channel.c
@@ -0,0 +1,132 @@
+/*
+ * exportable-channel.c - A channel usable with the Channel Manager
+ *
+ * Copyright (C) 2008 Collabora Ltd.
+ * Copyright (C) 2008 Nokia Corporation
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+#include "config.h"
+#include "exportable-channel.h"
+
+#include <telepathy-glib/gtypes.h>
+
+enum {
+    CLOSED,
+    N_SIGNALS
+};
+
+static guint signals[N_SIGNALS] = { 0 };
+
+
+static void
+exportable_channel_base_init (gpointer klass)
+{
+  static gboolean initialized = FALSE;
+
+  if (!initialized)
+    {
+      GParamSpec *param_spec;
+
+      initialized = TRUE;
+
+      /**
+       * GabbleExportableChannel:object-path:
+       *
+       * The D-Bus object path used for this object on the bus. Read-only
+       * except during construction.
+       */
+      param_spec = g_param_spec_string ("object-path", "D-Bus object path",
+          "The D-Bus object path used for this object on the bus.", NULL,
+          G_PARAM_CONSTRUCT_ONLY | G_PARAM_READWRITE |
+          G_PARAM_STATIC_NAME | G_PARAM_STATIC_BLURB | G_PARAM_STATIC_NICK);
+      g_object_interface_install_property (klass, param_spec);
+
+      /**
+       * GabbleExportableChannel:channel-properties:
+       *
+       * The D-Bus properties to be announced in the NewChannels signal
+       * and in the Channels property, as a map from
+       * inter.face.name.propertyname to GValue.
+       *
+       * This can only change when the closed signal is emitted.
+       */
+      param_spec = g_param_spec_boxed ("channel-properties",
+          "Channel properties",
+          "The channel properties",
+          TP_HASH_TYPE_QUALIFIED_PROPERTY_VALUE_MAP,
+          G_PARAM_CONSTRUCT_ONLY | G_PARAM_READWRITE |
+          G_PARAM_STATIC_NAME | G_PARAM_STATIC_BLURB | G_PARAM_STATIC_NICK);
+      g_object_interface_install_property (klass, param_spec);
+
+      /**
+       * GabbleExportableChannel:channel-destroyed:
+       *
+       * If true, the closed signal indicates that the channel can go away.
+       *
+       * If false, the closed signal indicates that the channel should
+       * appear to go away and be re-created.
+       */
+      param_spec = g_param_spec_boolean ("channel-destroyed",
+          "Destroyed?",
+          "If true, the channel has *really* closed, rather than just "
+          "appearing to do so",
+          FALSE,
+          G_PARAM_READABLE |
+          G_PARAM_STATIC_NAME | G_PARAM_STATIC_BLURB | G_PARAM_STATIC_NICK);
+      g_object_interface_install_property (klass, param_spec);
+
+      /**
+       * GabbleExportableChannel::closed:
+       *
+       * The channel has "closed" for the purposes of the D-Bus API. However,
+       * if channel-destroyed is false, the Connection should immediately
+       * signal that it has been re-created.
+       */
+      signals[CLOSED] = g_signal_new ("closed",
+          G_OBJECT_CLASS_TYPE (klass),
+          G_SIGNAL_RUN_LAST | G_SIGNAL_DETAILED,
+          0, NULL, NULL,
+          g_cclosure_marshal_VOID__VOID,
+          G_TYPE_NONE, 0);
+    }
+}
+
+GType
+gabble_exportable_channel_get_type (void)
+{
+  static GType type = 0;
+
+  if (G_UNLIKELY (type == 0))
+    {
+      static const GTypeInfo info = {
+        sizeof (GabbleExportableChannelIface),
+        exportable_channel_base_init,   /* base_init */
+        NULL,   /* base_finalize */
+        NULL,   /* class_init */
+        NULL,   /* class_finalize */
+        NULL,   /* class_data */
+        0,
+        0,      /* n_preallocs */
+        NULL    /* instance_init */
+      };
+
+      type = g_type_register_static (G_TYPE_INTERFACE,
+          "GabbleExportableChannel", &info, 0);
+    }
+
+  return type;
+}

7cf6f6f82ac6f9cee82a914c7863cddc325721d7
Simon McVittie <simon.mcvittie@collabora.co.uk>
2008-08-01 17:48:55 +0000

    GabbleConnection: use GabbleMediaFactory as a GabbleChannelManager
    20080801174855-53eee-4751bd12eff623b35b3b372fc08bf1a2ede556df.gz

--- src/connection.c
+++ src/connection.c
@@ -190,11 +190,6 @@ _gabble_connection_create_channel_factories (TpBaseConnection *conn)
 
   GPtrArray *channel_factories = g_ptr_array_sized_new (4);
 
-  g_ptr_array_add (channel_factories,
-                   g_object_new (GABBLE_TYPE_MEDIA_FACTORY,
-                                 "connection", self,
-                                 NULL));
-
   /* Temporary hack for requestotron support - divert the channel factories
    * and channel managers to somewhere under our control */
   self->channel_factories = channel_factories;
@@ -224,6 +219,11 @@ _gabble_connection_create_channel_factories (TpBaseConnection *conn)
 
   self->private_tubes_factory = gabble_private_tubes_factory_new (self);
   g_ptr_array_add (self->channel_managers, self->private_tubes_factory);
+
+  g_ptr_array_add (self->channel_managers,
+      g_object_new (GABBLE_TYPE_MEDIA_FACTORY,
+        "connection", self,
+        NULL));
 
   return channel_factories;
 }

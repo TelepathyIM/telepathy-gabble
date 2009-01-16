92e3d31c4add10b7bd1c5611be3cb7cec76b8455
Guillaume Desmottes <guillaume.desmottes@collabora.co.uk>
2009-01-16 11:02:44 +0000

    add request-invalid-dbus-tube.py

---
+++ tests/twisted/tubes/request-invalid-dbus-tube.py
@@ -0,0 +1,35 @@
+import dbus
+
+from gabbletest import exec_test
+from constants import *
+
+invalid_service_names = [ 'invalidServiceName'
+                        , 'one ten hundred thousand million'
+                        , 'me.is.it.you?.hello.you.sexy.sons.o.@#$%.heh'
+                        , ':1.1'
+                        , ''
+                        ]
+
+def test(q, bus, conn, stream):
+    conn.Connect()
+    q.expect('dbus-signal', signal='StatusChanged', args=[0L, 1L])
+
+    requestotron = dbus.Interface(conn, CONN_IFACE_REQUESTS)
+
+    for invalid_service_name in invalid_service_names:
+        try:
+            requestotron.CreateChannel(
+                    {CHANNEL_TYPE: CHANNEL_TYPE_DBUS_TUBE,
+                     TARGET_HANDLE_TYPE: HT_CONTACT,
+                     TARGET_ID: 'alice@localhost',
+                     DBUS_TUBE_SERVICE_NAME: invalid_service_name
+                });
+        except dbus.DBusException, e:
+            print e
+            assert e.get_dbus_name() == INVALID_ARGUMENT, \
+                (e.get_dbus_name(), invalid_service_name)
+        else:
+            assert False
+
+if __name__ == '__main__':
+    exec_test(test)

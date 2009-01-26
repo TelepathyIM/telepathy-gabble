8067526fc3ec1592a53e826fa639531c665ecf69
Guillaume Desmottes <guillaume.desmottes@collabora.co.uk>
2009-01-26 15:28:15 +0000

    tube-dbus: emit TubeChannelStateChanged when tube is open

--- src/tube-dbus.c
+++ src/tube-dbus.c
@@ -498,6 +498,10 @@ bytestream_state_changed_cb (GabbleBytestreamIface *bytestream,
   else if (state == GABBLE_BYTESTREAM_STATE_OPEN)
     {
       tube_dbus_open (self);
+
+      gabble_svc_channel_interface_tube_emit_tube_channel_state_changed (self,
+          GABBLE_TUBE_CHANNEL_STATE_OPEN);
+
       g_signal_emit (G_OBJECT (self), signals[OPENED], 0);
     }
 }

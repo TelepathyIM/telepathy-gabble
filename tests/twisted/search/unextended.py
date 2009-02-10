32d66a40935b405eb30f117156187f07df2d44e1
Will Thompson <will.thompson@collabora.co.uk>
2009-02-10 15:31:45 +0000

    Test asking the server for available search keys

--- tests/twisted/search/unextended.py
+++ tests/twisted/search/unextended.py
@@ -5,10 +5,15 @@ extensibility via Data Forms
 
 import dbus
 
+from twisted.words.protocols.jabber.client import IQ
+
 from gabbletest import exec_test
-from servicetest import call_async, unwrap
+from servicetest import call_async, unwrap, make_channel_proxy, EventPattern
+
+from pprint import pformat
 
 import constants as cs
+import ns
 
 server = 'jud.localhost'
 
@@ -24,12 +29,37 @@ def test(q, bus, conn, stream):
         }, signature='sv')
     call_async(q, requests, 'CreateChannel', request)
 
-    # Gabble should disco the server here.
+    iq_event = q.expect('stream-iq', to=server, query_ns=ns.SEARCH)
+    iq = iq_event.stanza
+
+    result = IQ(stream, "result")
+    result["id"] = iq["id"]
+    query = result.addElement((ns.SEARCH, 'query'))
+    query.addElement("instructions", content="cybar?")
+    for f in ["first", "last", "nick", "email"]:
+        query.addElement(f)
+    stream.send(result)
 
     ret = q.expect('dbus-return', method='CreateChannel')
     sig = q.expect('dbus-signal', signal='NewChannels')
 
     path, props = ret.value
+    props = unwrap(props)
+
+    expected_search_keys = ['email', 'nickname', 'x-n-family', 'x-n-given']
+
+    assert props[cs.CONTACT_SEARCH_SERVER] == server, pformat(props)
+    assert sorted(props[cs.CONTACT_SEARCH_ASK]) == expected_search_keys, \
+        pformat(props)
+    assert cs.CONTACT_SEARCH_STATE not in props, pformat(props)
+
+    c = make_channel_proxy(conn, path, 'Channel')
+    c.Close()
+
+    q.expect_many(
+        EventPattern('dbus-signal', signal='Closed'),
+        EventPattern('dbus-signal', signal='ChannelClosed'),
+        )
 
     conn.Disconnect()
     q.expect('dbus-signal', signal='StatusChanged', args=[2, 1])

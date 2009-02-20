b51db8caf6e1260fb42869ebdcd5a694274e3c3e
Guillaume Desmottes <guillaume.desmottes@collabora.co.uk>
2009-02-20 16:32:40 +0000

    test-receive-file.py: test using ReceiveFileTestS5B as well

--- tests/twisted/file-transfer/test-receive-file.py
+++ tests/twisted/file-transfer/test-receive-file.py
@@ -1,6 +1,8 @@
 from gabbletest import exec_test
-from file_transfer_helper import ReceiveFileTestIBB
+from file_transfer_helper import ReceiveFileTestIBB, ReceiveFileTestS5B
 
 if __name__ == '__main__':
     test = ReceiveFileTestIBB()
+    exec_test(test.test)
+    test = ReceiveFileTestS5B()
     exec_test(test.test)

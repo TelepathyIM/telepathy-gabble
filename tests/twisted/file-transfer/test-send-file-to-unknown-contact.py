93123f44be78af99bbcad637e134207562a17daa
Guillaume Desmottes <guillaume.desmottes@collabora.co.uk>
2009-02-23 15:55:55 +0000

    add test-send-file-to-unknown-contact.py

---
+++ tests/twisted/file-transfer/test-send-file-to-unknown-contact.py
@@ -0,0 +1,23 @@
+import dbus
+
+from file_transfer_helper import SendFileTest, HT_CONTACT, exec_file_transfer_test
+
+class SendFileTransferToUnknownContactTest(SendFileTest):
+    def __init__(self, bytestream_cls):
+        SendFileTest.__init__(self, bytestream_cls)
+
+        self._actions = [self.connect, self.check_ft_available, self.my_request_ft_channel]
+
+    def my_request_ft_channel(self):
+        self.contact_name = 'jean@localhost'
+        self.handle = self.conn.RequestHandles(HT_CONTACT, [self.contact_name])[0]
+
+        try:
+            self.request_ft_channel()
+        except dbus.DBusException, e:
+            assert e.get_dbus_name() == 'org.freedesktop.Telepathy.Errors.NotAvailable'
+        else:
+            assert False
+
+if __name__ == '__main__':
+    exec_file_transfer_test(SendFileTransferToUnknownContactTest)

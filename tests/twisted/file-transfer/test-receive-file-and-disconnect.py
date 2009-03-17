bb9fd71dc06ac39b461b4109e341fe7cd4172c76
Guillaume Desmottes <guillaume.desmottes@collabora.co.uk>
2009-03-17 11:13:05 +0000

    test-receive-file-and-disconnect.py: use self.create_socket()

--- tests/twisted/file-transfer/test-receive-file-and-disconnect.py
+++ tests/twisted/file-transfer/test-receive-file-and-disconnect.py
@@ -4,7 +4,7 @@ from file_transfer_helper import exec_file_transfer_test, ReceiveFileTest
 
 class ReceiveFileAndDisconnectTest(ReceiveFileTest):
     def receive_file(self):
-        s = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+        s = self.create_socket()
         s.connect(self.address)
 
         # disconnect

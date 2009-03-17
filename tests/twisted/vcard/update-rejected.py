ffa1fd5d4b89a2d7c2aa440dd3e19d9cce42ad99
Will Thompson <will.thompson@collabora.co.uk>
2009-03-17 18:59:32 +0000

    Test SetAvatar's error when server rejects avatar

---
+++ tests/twisted/vcard/update-rejected.py
@@ -0,0 +1,42 @@
+"""
+Regression test for fd.o #20442, where the XMPP error returned by a server that
+doesn't like the avatar you tried to set was not mapped to a TP error before
+being sent over the bus.
+"""
+
+from twisted.words.xish import domish
+
+from servicetest import call_async
+from gabbletest import exec_test, expect_and_handle_get_vcard, send_error_reply
+
+import ns
+import constants as cs
+
+def test(q, bus, conn, stream):
+    conn.Connect()
+
+    expect_and_handle_get_vcard(q, stream)
+
+    call_async(q, conn.Avatars, 'SetAvatar', 'william shatner',
+        'image/x-actor-name')
+
+    set_vcard_event = q.expect('stream-iq', query_ns=ns.VCARD_TEMP,
+        query_name='vCard', iq_type='set')
+    iq = set_vcard_event.stanza
+
+    error = domish.Element((None, 'error'))
+    error['code'] = '400'
+    error['type'] = 'modify'
+    error.addElement((ns.STANZA, 'bad-request'))
+
+    send_error_reply(stream, iq, error)
+
+    event = q.expect('dbus-error', method='SetAvatar')
+
+    assert event.error.get_dbus_name() == cs.INVALID_ARGUMENT, \
+        event.error.get_dbus_name()
+
+    conn.Disconnect()
+
+if __name__ == '__main__':
+    exec_test(test)

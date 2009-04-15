7ba550963e656dd426d679aecaf20dcfa2abe647
Guillaume Desmottes <guillaume.desmottes@collabora.co.uk>
2009-04-15 17:26:57 +0100

    don't skip D-Bus tubes in gabble_tubes_channel_foreach
    they are now ported to the new API so they should be included

--- src/tubes-channel.c
+++ src/tubes-channel.c
@@ -735,8 +735,7 @@ foreach_slave (gpointer key,
   TpTubeType type;
 
   g_object_get (tube, "type", &type, NULL);
-  if (type == TP_TUBE_TYPE_STREAM)
-    data->foreach (TP_EXPORTABLE_CHANNEL (tube), data->user_data);
+  data->foreach (TP_EXPORTABLE_CHANNEL (tube), data->user_data);
 }
 
 void gabble_tubes_channel_foreach (GabbleTubesChannel *self,

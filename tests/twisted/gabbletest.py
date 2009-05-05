a00c160bfbab15c9d2e2bcc0309b201354777513
Guillaume Desmottes <guillaume.desmottes@collabora.co.uk>
2009-05-05 18:19:16 +0100

    gabbletest: use hashlib instead of the deprecated sha module
    This is fine as twisted tests already depend on python >= 2.5

--- tests/twisted/gabbletest.py
+++ tests/twisted/gabbletest.py
@@ -5,7 +5,7 @@ Infrastructure code for testing Gabble by pretending to be a Jabber server.
 
 import base64
 import os
-import sha
+import hashlib
 import sys
 import random
 
@@ -130,7 +130,7 @@ class JabberAuthenticator(xmlstream.Authenticator):
         assert map(str, username) == [self.username]
 
         digest = xpath.queryForNodes('/iq/query/digest', iq)
-        expect = sha.sha(self.xmlstream.sid + self.password).hexdigest()
+        expect = hashlib.sha1(self.xmlstream.sid + self.password).hexdigest()
         assert map(str, digest) == [expect]
 
         resource = xpath.queryForNodes('/iq/query/resource', iq)

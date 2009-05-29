9e1fee7380523722a822c064348c153ca5edebaf
Will Thompson <will.thompson@collabora.co.uk>
2009-05-29 12:55:40 +0100

    Remove spurious debug message.

--- src/connection.c
+++ src/connection.c
@@ -1687,8 +1687,6 @@ connection_iq_disco_cb (LmMessageHandler *handler,
   features = capabilities_get_features (self->self_presence->caps,
       self->self_presence->per_channel_manager_caps);
 
-  DEBUG ("caps now %u", self->self_presence->caps);
-
   /* If node is not NULL, it can be either a caps bundle as defined in the
    * legacy XEP-0115 version 1.3 or an hash as defined in XEP-0115 version
    * 1.5. */

8013fb4ba160459c7160ce00265aba1fc6cb6dae
Guillaume Desmottes <guillaume.desmottes@collabora.co.uk>
2009-06-29 15:41:29 +0100

    add lm-types.h

---
+++ lib/loudmouth/lm-types.h
@@ -0,0 +1,13 @@
+#ifndef __LM_TYPES_H__
+#define __LM_TYPES_H__
+
+#include <wocky/wocky-porter.h>
+
+G_BEGIN_DECLS
+
+typedef struct _LmMessageHandler LmMessageHandler;
+typedef WockyPorter LmConnection;
+
+G_END_DECLS
+
+#endif /* #ifndef __LM_TYPES_H__ */

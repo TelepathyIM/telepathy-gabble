ce0782058bf7a7b4c5785e064b6bfff6bbbe5e9a
Guillaume Desmottes <guillaume.desmottes@collabora.co.uk>
2009-07-14 10:13:21 +0100

    test-receive-file-and-disconnect.py: explain why we return True in this part of the test

--- tests/twisted/file-transfer/test-receive-file-and-disconnect.py
+++ tests/twisted/file-transfer/test-receive-file-and-disconnect.py
@@ -6,7 +6,8 @@ class ReceiveFileAndDisconnectTest(ReceiveFileTest):
         s = self.create_socket()
         s.connect(self.address)
 
-        # disconnect
+        # return True so the test will be ended and the connection
+        # disconnected
         return True
 
 if __name__ == '__main__':

6d87ea0988e77bd72a49227b8bfa7e99089fb377
Guillaume Desmottes <guillaume.desmottes@collabora.co.uk>
2009-07-21 11:22:22 +0100

    add test connect/stream-closed.py
    Test if Gabble properly disconnect the connection when server closes its
    XMPP connection.

---
+++ tests/twisted/connect/stream-closed.py
@@ -0,0 +1,27 @@
+
+"""
+Connection is disconnected because server closes its XMPP stream.
+"""
+
+from gabbletest import exec_test
+from servicetest import EventPattern
+import constants as cs
+
+def test(q, bus, conn, stream):
+    conn.Connect()
+    q.expect('dbus-signal', signal='StatusChanged',
+        args=[cs.CONN_STATUS_CONNECTED, cs.CSR_REQUESTED])
+
+    # server closes its stream
+    stream.sendFooter()
+
+    # Gabble disconnect and close its connection
+    q.expect_many(
+        EventPattern('dbus-signal', signal='StatusChanged',
+            args=[cs.CONN_STATUS_DISCONNECTED, cs.CSR_NETWORK_ERROR]),
+        EventPattern('stream-closed'))
+
+
+if __name__ == '__main__':
+    exec_test(test)
+

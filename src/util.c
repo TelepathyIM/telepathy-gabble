938915a897739bb4d4cd58dd08c90d34c5cd5fd6
Guillaume Desmottes <guillaume.desmottes@collabora.co.uk>
2009-07-23 17:33:23 +0100

    util.c: lm_message_node_add_build_va: use the right wocky api when setting xml:lang

--- src/util.c
+++ src/util.c
@@ -241,6 +241,8 @@ lm_message_node_add_build_va (LmMessageNode *node, guint spec, va_list ap)
             g_return_if_fail (value != NULL);
             if (!tp_strdiff (key, "xmlns"))
               wocky_xmpp_node_set_ns (stack->data, value);
+            else if (!tp_strdiff (key, "xml:lang"))
+              wocky_xmpp_node_set_language (stack->data, value);
             else
               lm_message_node_set_attribute (stack->data, key, value);
           }

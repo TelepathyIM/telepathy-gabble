1ef44ecb4beafc397feb048e50908ae2bdb8b625
Dafydd Harries <dafydd.harries@collabora.co.uk>
2009-08-12 16:39:00 +0100

    improve log strip regex for pointers

--- tools/log-strip.py
+++ tools/log-strip.py
@@ -12,7 +12,7 @@ def sanitise(line):
         re.sub('^(\*\* )?\(telepathy-gabble:\d+\)', '',
         re.sub('\x1b\[0m', '',
         re.sub('^RECV \[\d+\]', 'RECV [???]',
-        re.sub('0x.......', '0x???????',
+        re.sub('0x[0-9A-Fa-f]{5,8}', '0x???????',
         re.sub("('?<[^ ]+ [^>]*id=)[\"'][^\"']+[\"']",
             lambda m: m.group(1) + '"?????"', line))))))
 

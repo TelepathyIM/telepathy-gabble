8fb4b3c496e1b0c73d96d3e6aea115630819cb83
Dafydd Harries <dafydd.harries@collabora.co.uk>
2009-08-25 18:35:37 +0100

    test-jid-decode: factor out common code

--- tests/test-jid-decode.c
+++ tests/test-jid-decode.c
@@ -6,105 +6,47 @@
 #include "src/util.h"
 
 static void
-test1 (void)
+test_pass (
+    const gchar *jid,
+    const gchar *expected_node,
+    const gchar *expected_domain,
+    const gchar *expected_resource)
 {
   gchar *node = NULL;
   gchar *domain = NULL;
   gchar *resource = NULL;
 
-  g_assert (!gabble_decode_jid ("", &node, &domain, &resource));
-  g_assert (node == NULL);
-  g_assert (domain == NULL);
-  g_assert (resource == NULL);
-  g_free (domain);
-}
-
-static void
-test2 (void)
-{
-  gchar *node = NULL;
-  gchar *domain = NULL;
-  gchar *resource = NULL;
-
-  g_assert (gabble_decode_jid ("bar", &node, &domain, &resource));
-  g_assert (node == NULL);
-  g_assert (0 == strcmp (domain, "bar"));
-  g_assert (resource == NULL);
-  g_free (domain);
-}
-
-static void
-test3 (void)
-{
-  gchar *node = NULL;
-  gchar *domain = NULL;
-  gchar *resource = NULL;
-
-  g_assert (gabble_decode_jid ("foo@bar", &node, &domain, &resource));
-  g_assert (0 == strcmp (node, "foo"));
-  g_assert (0 == strcmp (domain, "bar"));
-  g_assert (resource == NULL);
-  g_free (node);
-  g_free (domain);
-}
-
-static void
-test4 (void)
-{
-  gchar *node = NULL;
-  gchar *domain = NULL;
-  gchar *resource = NULL;
-
-  g_assert (gabble_decode_jid ("foo@bar/baz", &node, &domain, &resource));
-  g_assert (0 == strcmp (node, "foo"));
-  g_assert (0 == strcmp (domain, "bar"));
-  g_assert (0 == strcmp (resource, "baz"));
+  g_assert (gabble_decode_jid (jid, &node, &domain, &resource));
+  g_assert (!tp_strdiff (expected_node, node));
+  g_assert (!tp_strdiff (expected_domain, domain));
+  g_assert (!tp_strdiff (expected_resource, resource));
   g_free (node);
   g_free (domain);
   g_free (resource);
 }
 
 static void
-test5 (void)
+test_fail (const gchar *jid)
 {
   gchar *node = NULL;
   gchar *domain = NULL;
   gchar *resource = NULL;
 
-  g_assert (!gabble_decode_jid ("@bar", &node, &domain, &resource));
+  g_assert (!gabble_decode_jid (jid, &node, &domain, &resource));
   g_assert (node == NULL);
   g_assert (domain == NULL);
   g_assert (resource == NULL);
-  g_free (node);
-  g_free (domain);
-  g_free (resource);
-}
-
-static void
-test6 (void)
-{
-  gchar *node = NULL;
-  gchar *domain = NULL;
-  gchar *resource = NULL;
-
-  g_assert (!gabble_decode_jid ("foo@bar/", &node, &domain, &resource));
-  g_assert (node == NULL);
-  g_assert (domain == NULL);
-  g_assert (resource == NULL);
-  g_free (node);
-  g_free (domain);
-  g_free (resource);
 }
 
 int
 main (void)
 {
-  test1 ();
-  test2 ();
-  test3 ();
-  test4 ();
-  test5 ();
-  test6 ();
+  test_fail ("");
+  test_pass ("bar", NULL, "bar", NULL);
+  test_pass ("foo@bar", "foo", "bar", NULL);
+  test_pass ("foo@bar/baz", "foo", "bar", "baz");
+  test_fail ("@bar");
+  test_fail ("foo@bar/");
 
   return 0;
 }

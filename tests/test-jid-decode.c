a90bd4da5ba0b959eb6feb31cca096218fa93dc5
Dafydd Harries <dafydd.harries@collabora.co.uk>
2009-08-25 18:34:04 +0100

    test-jid-decode: rename server -> domain for consistency

--- tests/test-jid-decode.c
+++ tests/test-jid-decode.c
@@ -9,58 +9,58 @@ static void
 test1 (void)
 {
   gchar *node = NULL;
-  gchar *server = NULL;
+  gchar *domain = NULL;
   gchar *resource = NULL;
 
-  g_assert (!gabble_decode_jid ("", &node, &server, &resource));
+  g_assert (!gabble_decode_jid ("", &node, &domain, &resource));
   g_assert (node == NULL);
-  g_assert (server == NULL);
+  g_assert (domain == NULL);
   g_assert (resource == NULL);
-  g_free (server);
+  g_free (domain);
 }
 
 static void
 test2 (void)
 {
   gchar *node = NULL;
-  gchar *server = NULL;
+  gchar *domain = NULL;
   gchar *resource = NULL;
 
-  g_assert (gabble_decode_jid ("bar", &node, &server, &resource));
+  g_assert (gabble_decode_jid ("bar", &node, &domain, &resource));
   g_assert (node == NULL);
-  g_assert (0 == strcmp (server, "bar"));
+  g_assert (0 == strcmp (domain, "bar"));
   g_assert (resource == NULL);
-  g_free (server);
+  g_free (domain);
 }
 
 static void
 test3 (void)
 {
   gchar *node = NULL;
-  gchar *server = NULL;
+  gchar *domain = NULL;
   gchar *resource = NULL;
 
-  g_assert (gabble_decode_jid ("foo@bar", &node, &server, &resource));
+  g_assert (gabble_decode_jid ("foo@bar", &node, &domain, &resource));
   g_assert (0 == strcmp (node, "foo"));
-  g_assert (0 == strcmp (server, "bar"));
+  g_assert (0 == strcmp (domain, "bar"));
   g_assert (resource == NULL);
   g_free (node);
-  g_free (server);
+  g_free (domain);
 }
 
 static void
 test4 (void)
 {
   gchar *node = NULL;
-  gchar *server = NULL;
+  gchar *domain = NULL;
   gchar *resource = NULL;
 
-  g_assert (gabble_decode_jid ("foo@bar/baz", &node, &server, &resource));
+  g_assert (gabble_decode_jid ("foo@bar/baz", &node, &domain, &resource));
   g_assert (0 == strcmp (node, "foo"));
-  g_assert (0 == strcmp (server, "bar"));
+  g_assert (0 == strcmp (domain, "bar"));
   g_assert (0 == strcmp (resource, "baz"));
   g_free (node);
-  g_free (server);
+  g_free (domain);
   g_free (resource);
 }
 
@@ -68,15 +68,15 @@ static void
 test5 (void)
 {
   gchar *node = NULL;
-  gchar *server = NULL;
+  gchar *domain = NULL;
   gchar *resource = NULL;
 
-  g_assert (!gabble_decode_jid ("@bar", &node, &server, &resource));
+  g_assert (!gabble_decode_jid ("@bar", &node, &domain, &resource));
   g_assert (node == NULL);
-  g_assert (server == NULL);
+  g_assert (domain == NULL);
   g_assert (resource == NULL);
   g_free (node);
-  g_free (server);
+  g_free (domain);
   g_free (resource);
 }
 
@@ -84,15 +84,15 @@ static void
 test6 (void)
 {
   gchar *node = NULL;
-  gchar *server = NULL;
+  gchar *domain = NULL;
   gchar *resource = NULL;
 
-  g_assert (!gabble_decode_jid ("foo@bar/", &node, &server, &resource));
+  g_assert (!gabble_decode_jid ("foo@bar/", &node, &domain, &resource));
   g_assert (node == NULL);
-  g_assert (server == NULL);
+  g_assert (domain == NULL);
   g_assert (resource == NULL);
   g_free (node);
-  g_free (server);
+  g_free (domain);
   g_free (resource);
 }
 

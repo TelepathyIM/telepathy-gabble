37f6419427d4fc86b6036d60eaceac3a780f4adf
Alban Crequy <alban.crequy@collabora.co.uk>
2009-10-08 11:56:19 +0100

    test-avatar-async.py: Do not loop when there is no avatar

--- tests/twisted/vcard/test-avatar-async.py
+++ tests/twisted/vcard/test-avatar-async.py
@@ -49,7 +49,24 @@ def test(q, bus, conn, stream):
         EventPattern('stream-iq', to=None, query_ns='vcard-temp',
             query_name='vCard'))
 
+    # When we start, there is no avatar
     acknowledge_iq(stream, iq_event.stanza)
+    self_handle = conn.GetSelfHandle()
+
+    # Another resource confirms we have no avatar. We don't request our vCard
+    # because we already know there is no avatar
+    presence_stanza = make_presence('test@localhost/noavatar',
+                                    to='test@localhost/Resource',
+                                    show='away', status='At the pub',
+                                    photo="")
+    q.forbid_events([avatar_request_event, avatar_retrieved_event])
+    # Gabble must resist temptation to send vCard requests even with several
+    # presence stanza sent!
+    stream.send(presence_stanza)
+    stream.send(presence_stanza)
+    sync_stream(q, stream) # Twice because the vCard request is done in
+    sync_stream(q, stream) # g_idle_add
+    q.unforbid_events([avatar_request_event, avatar_retrieved_event])
 
     # Request on the first contact. Test the cache.
     handle = conn.RequestHandles(1, ['bob@foo.com'])[0]
@@ -113,7 +130,6 @@ def test(q, bus, conn, stream):
     assertEquals('image/png', event.args[3])
 
     # Test with our own avatar test@localhost/Resource2
-    self_handle = conn.GetSelfHandle()
     presence_stanza = make_presence('test@localhost/Resource2',
                                     to='test@localhost/Resource',
                                     show='away', status='At the pub',

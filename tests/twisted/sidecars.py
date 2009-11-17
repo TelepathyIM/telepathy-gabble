f4b8a85615e5d7e7819cb154fe6958648c658a81
Will Thompson <will.thompson@collabora.co.uk>
2009-11-17 11:55:59 +0000

    Test sidecar support using test plugin

---
+++ tests/twisted/sidecars.py
@@ -0,0 +1,62 @@
+"""
+Test Gabble's implementation of sidecars, using the test plugin.
+"""
+
+from servicetest import call_async, EventPattern, assertEquals, assertContains
+from gabbletest import exec_test
+import constants as cs
+
+TEST_PLUGIN_IFACE = "org.freedesktop.Telepathy.Gabble.Plugin.Test"
+
+def test(q, bus, conn, stream):
+    conn.Connect()
+    q.expect('dbus-signal', signal='StatusChanged',
+            args=[cs.CONN_STATUS_CONNECTED, cs.CSR_REQUESTED])
+
+    call_async(q, conn.Future, 'EnsureSidecar', 'not an interface')
+    q.expect('dbus-error', name=cs.INVALID_ARGUMENT)
+
+    # The test plugin makes no reference to this interface.
+    call_async(q, conn.Future, 'EnsureSidecar', 'unsupported.sidecar')
+    q.expect('dbus-error', name=cs.NOT_IMPLEMENTED)
+
+    # This sidecar doesn't even implement get_immutable_properties; it should
+    # just get the empty dict filled in for it.
+    path, props = conn.Future.EnsureSidecar(TEST_PLUGIN_IFACE)
+    assertEquals({}, props)
+
+    # We should get the same sidecar if we request it again
+    path2, props2 = conn.Future.EnsureSidecar(TEST_PLUGIN_IFACE)
+    assertEquals((path, props), (path2, props2))
+
+    # This sidecar does have some properties:
+    path, props = conn.Future.EnsureSidecar(TEST_PLUGIN_IFACE + ".Props")
+    assertContains(TEST_PLUGIN_IFACE + ".Props.Greeting", props)
+
+    # The plugin claims it implements this sidecar, but actually doesn't. Check
+    # that we don't blow up (although this is no different from Gabble's
+    # perspective to creating a sidecar failing because a network service
+    # wasn't there, for instance).
+    call_async(q, conn.Future, 'EnsureSidecar', TEST_PLUGIN_IFACE + ".Buggy")
+    q.expect('dbus-error', name=cs.NOT_IMPLEMENTED)
+
+    # TODO: test ensuring a sidecar that waits for something from the network,
+    # disconnecting while it's waiting, and ensuring that nothing breaks
+    # regardless of whether the network replies before </stream:stream> or not.
+
+    call_async(q, conn, 'Disconnect')
+
+    q.expect_many(
+        EventPattern('dbus-signal', signal='StatusChanged',
+            args=[cs.CONN_STATUS_DISCONNECTED, cs.CSR_REQUESTED]),
+        EventPattern('stream-closed'),
+        )
+
+    call_async(q, conn.Future, 'EnsureSidecar', 'zomg.what')
+    q.expect('dbus-error', name=cs.DISCONNECTED)
+
+    stream.sendFooter()
+    q.expect('dbus-return', method='Disconnect')
+
+if __name__ == '__main__':
+    exec_test(test)

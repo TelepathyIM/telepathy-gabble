aa533f5806fbc847cdd2a44f489cc093b31c3be2
Will Thompson <will.thompson@collabora.co.uk>
2009-12-07 15:40:12 +0000

    Fix coding style checks for out-of-tree builds

--- tools/check-coding-style.mk
+++ tools/check-coding-style.mk
@@ -3,12 +3,12 @@ check-coding-style:
 	if test -n "$(check_misc_sources)"; then \
 		tools_dir=$(top_srcdir)/tools \
 		sh $(top_srcdir)/tools/check-misc.sh \
-			$(check_misc_sources) || fail=1; \
+			$(addprefix $(srcdir)/,$(check_misc_sources)) || fail=1; \
 	fi; \
 	if test -n "$(check_c_sources)"; then \
 		tools_dir=$(top_srcdir)/tools \
 		sh $(top_srcdir)/tools/check-c-style.sh \
-			$(check_c_sources) || fail=1; \
+			$(addprefix $(srcdir)/,$(check_c_sources)) || fail=1; \
 	fi;\
 	if test yes = "$(ENABLE_CODING_STYLE_CHECKS)"; then \
 		exit "$$fail";\

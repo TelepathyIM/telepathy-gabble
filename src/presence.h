96af7bbe896abf2869467d087416d6596022ea98
Sjoerd Simons <sjoerd.simons@collabora.co.uk>
2010-01-04 11:34:13 +0100

    Factor out code to add the status, caps and vcard to a presence

--- src/presence.h
+++ src/presence.h
@@ -96,6 +96,9 @@ gboolean gabble_presence_resource_has_caps (GabblePresence *presence,
 
 LmMessage *gabble_presence_as_message (GabblePresence *presence,
     const gchar *to);
+void gabble_presence_add_status_and_vcard (GabblePresence *presence,
+  WockyXmppStanza *stanza);
+
 gchar *gabble_presence_dump (GabblePresence *presence);
 
 gboolean gabble_presence_added_to_view (GabblePresence *presence);

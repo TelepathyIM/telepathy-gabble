1e56a445a34d428c0feefd7423983608bd04919c
Will Thompson <will.thompson@collabora.co.uk>
2010-01-11 13:42:39 +0000

    Treat <item-not-found/> as the empty vCard
    When we want to update our vCard, we first retrieve our vCard from the
    server. XEP-0054 says that the server MUST return <item-not-found/> if
    you have no vCard set, but we previously assumed that the server would
    return success, with an empty <vCard/> node. Prosody respects the XEP.
    So, let's treat the former case identically to the latter.
    
    Fixes: fd.o#25987
    Reviewed-by: Simon McVittie <simon.mcvittie@collabora.co.uk>

---
+++ tests/twisted/vcard/item-not-found.py
@@ -0,0 +1,42 @@
+"""
+Regression test for <http://bugs.freedesktop.org/show_bug.cgi?id=25987> where
+Gabble didn't understand that <item-not-found/> just means the user doesn't
+have a vCard yet, and so it can go right ahead and set one.
+"""
+
+from twisted.words.xish import domish
+from servicetest import call_async
+from gabbletest import (
+    exec_test, sync_stream, send_error_reply, expect_and_handle_set_vcard,
+    )
+import ns
+
+def expect_get_and_send_item_not_found(q, stream):
+    get_vcard_event = q.expect('stream-iq', query_ns=ns.VCARD_TEMP,
+        query_name='vCard', iq_type='get')
+
+    error = domish.Element((None, 'error'))
+    error['type'] = 'cancel'
+    error.addElement((ns.STANZA, 'item-not-found'))
+    send_error_reply(stream, get_vcard_event.stanza, error)
+
+def test(q, bus, conn, stream):
+    conn.Connect()
+
+    expect_get_and_send_item_not_found(q, stream)
+
+    sync_stream(q, stream)
+
+    call_async(
+        q, conn.Avatars, 'SetAvatar', 'Guy.brush', 'image/x-mighty-pirate')
+
+    # Gabble checks again, but we still don't have a vCard
+    expect_get_and_send_item_not_found(q, stream)
+
+    # Never mind! It creates a new one.
+    expect_and_handle_set_vcard(q, stream)
+
+    q.expect('dbus-return', method='SetAvatar')
+
+if __name__ == '__main__':
+    exec_test(test)

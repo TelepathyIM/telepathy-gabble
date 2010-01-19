5a7757ca53b83ee6904354f02a4caa0ade0a11eb
Mike Ruprecht <mike.ruprecht@collabora.co.uk>
2010-01-18 21:14:13 -0600

    Don't send a content-modify message if the content hasn't been sent yet.
    This fixes a crash if you SetSending before session-initiate is sent.

--- src/jingle-content.c
+++ src/jingle-content.c
@@ -978,10 +978,13 @@ gabble_jingle_content_change_direction (GabbleJingleContent *c,
       return FALSE;
     }
 
-  msg = gabble_jingle_session_new_message (c->session,
-      JINGLE_ACTION_CONTENT_MODIFY, &sess_node);
-  gabble_jingle_content_produce_node (c, sess_node, FALSE, FALSE, NULL);
-  gabble_jingle_session_send (c->session, msg, NULL, NULL);
+  if (priv->state >= JINGLE_CONTENT_STATE_SENT)
+    {
+      msg = gabble_jingle_session_new_message (c->session,
+          JINGLE_ACTION_CONTENT_MODIFY, &sess_node);
+      gabble_jingle_content_produce_node (c, sess_node, FALSE, FALSE, NULL);
+      gabble_jingle_session_send (c->session, msg, NULL, NULL);
+    }
 
   /* FIXME: actually check whether remote end accepts our content-modify */
   return TRUE;

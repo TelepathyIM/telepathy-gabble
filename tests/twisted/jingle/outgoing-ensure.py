afbbb05d6be544074fabbdad6e25641c16cb74f4
Mike Ruprecht <mike.ruprecht@collabora.co.uk>
2010-01-21 20:51:19 -0600

    Test Ensuring channels that are most likely still being created.

--- tests/twisted/jingle/outgoing-ensure.py
+++ tests/twisted/jingle/outgoing-ensure.py
@@ -89,6 +89,63 @@ def test(q, bus, conn, stream, channel_type):
     chan.Close()
 
 
+    # Ensure a channel that doesn't exist yet.
+    call_async(q, conn.Requests, 'EnsureChannel', request)
+
+    # Re-ensure a channel that is hopefully still pending creation.
+    call_async(q, conn.Requests, 'EnsureChannel', request)
+
+    ret, ret2, old_sig, new_sig = q.expect_many(
+        EventPattern('dbus-return', method='EnsureChannel'),
+        EventPattern('dbus-return', method='EnsureChannel'),
+        EventPattern('dbus-signal', signal='NewChannel',
+            predicate=lambda e: cs.CHANNEL_TYPE_CONTACT_LIST not in e.args),
+        EventPattern('dbus-signal', signal='NewChannels',
+            predicate=lambda e:
+                cs.CHANNEL_TYPE_CONTACT_LIST not in e.args[0][0][1].values()),
+        )
+
+    yours, path, props = ret.value
+
+    # this channel was created in response to our EnsureChannel call, so it
+    # should be ours.
+    assert yours, ret.value
+
+    sig_path, sig_ct, sig_ht, sig_h, sig_sh = old_sig.args
+
+    assertEquals(sig_path, path)
+    assertEquals(channel_type, sig_ct)
+    assertEquals(cs.HT_CONTACT, sig_ht)
+    assertEquals(handle, sig_h)
+    assert sig_sh # suppress handler
+
+    assertLength(1, new_sig.args)
+    assertLength(1, new_sig.args[0])        # one channel
+    assertLength(2, new_sig.args[0][0])     # two struct members
+    assertEquals(path, new_sig.args[0][0][0])
+    emitted_props = new_sig.args[0][0][1]
+
+    assertEquals(channel_type, emitted_props[cs.CHANNEL_TYPE])
+    assertEquals(cs.HT_CONTACT, emitted_props[cs.TARGET_HANDLE_TYPE])
+    assertEquals(handle, emitted_props[cs.TARGET_HANDLE])
+    assertEquals(jt.peer_bare_jid, emitted_props[cs.TARGET_ID])
+    assert emitted_props[cs.REQUESTED]
+    assertEquals(self_handle, emitted_props[cs.INITIATOR_HANDLE])
+    assertEquals('test@localhost', emitted_props[cs.INITIATOR_ID])
+
+    yours2, path2, props2 = ret2.value
+
+    # We should have got back the same channel we created a page or so ago.
+    assertEquals(path2, path)
+    # It's not been created for this call, so Yours should be False.
+    assert not yours2
+
+    # Time passes ... afterwards we close the chan
+
+    chan = wrap_channel(bus.get_object(conn.bus_name, path), 'StreamedMedia')
+    chan.Close()
+
+
     # The remaining checks don't apply to calls
     if channel_type == cs.CHANNEL_TYPE_CALL:
         return

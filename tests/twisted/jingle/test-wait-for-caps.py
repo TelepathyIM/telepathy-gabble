364363ac8b57393659f5e6aa22de7f53771746fa
Mike Ruprecht <mike.ruprecht@collabora.co.uk>
2010-01-25 21:50:36 -0600

    Update test-wait-for-caps to check Calls as well.

--- tests/twisted/jingle/test-wait-for-caps.py
+++ tests/twisted/jingle/test-wait-for-caps.py
@@ -13,7 +13,7 @@ import dbus
 import constants as cs
 import ns
 
-def test(q, bus, conn, stream):
+def test(q, bus, conn, stream, channel_type):
     jt = jingletest.JingleTest(stream, 'test@localhost', 'foo@bar.com/Foo')
     jt2 = jingletest.JingleTest(stream, 'test@localhost', 'foo2@bar.com/Foo')
     # Make gabble think this is a different client
@@ -30,10 +30,10 @@ def test(q, bus, conn, stream):
     q.expect('dbus-signal', signal='StatusChanged',
             args=[cs.CONN_STATUS_CONNECTED, cs.CSR_REQUESTED])
 
-    run_test(q, bus, conn, stream, jt, True)
-    run_test(q, bus, conn, stream, jt2, False)
+    run_test(q, bus, conn, stream, jt, True, channel_type)
+    run_test(q, bus, conn, stream, jt2, False, channel_type)
 
-def run_test(q, bus, conn, stream, jt, request_before_presence):
+def run_test(q, bus, conn, stream, jt, request_before_presence, channel_type):
     """
     Requests streams on a media channel to jt.remote_jid, either before their
     presence is received (if request_before_presence is True) or after their
@@ -44,20 +44,30 @@ def run_test(q, bus, conn, stream, jt, request_before_presence):
     # We intentionally DON'T set remote presence yet. Since Gabble is still
     # unsure whether to treat contact as offline for this purpose, it
     # will tentatively allow channel creation and contact handle addition
-
-    request = dbus.Dictionary({ cs.CHANNEL_TYPE: cs.CHANNEL_TYPE_STREAMED_MEDIA,
+    request = dbus.Dictionary({ cs.CHANNEL_TYPE: channel_type,
                                 cs.TARGET_HANDLE_TYPE: cs.HT_CONTACT,
                                 cs.TARGET_ID: jt.remote_jid
                               }, signature='sv')
-    path, props = conn.CreateChannel(request, dbus_interface=cs.CONN_IFACE_REQUESTS)
-    media_iface = make_channel_proxy(conn, path, 'Channel.Type.StreamedMedia')
-    handle = props[cs.TARGET_HANDLE]
+
+    if channel_type == cs.CHANNEL_TYPE_CALL:
+        request[cs.CALL_INITIAL_AUDIO] = True
+
+    if channel_type == cs.CHANNEL_TYPE_STREAMED_MEDIA:
+        path, props = conn.CreateChannel(request,
+            dbus_interface=cs.CONN_IFACE_REQUESTS)
+        media_iface = make_channel_proxy(conn, path,
+            'Channel.Type.StreamedMedia')
+
+        handle = props[cs.TARGET_HANDLE]
 
     sync_dbus(bus, q, conn)
 
     def call_request_streams():
-        call_async(q, media_iface, 'RequestStreams', handle,
-            [cs.MEDIA_STREAM_TYPE_AUDIO])
+        if channel_type == cs.CHANNEL_TYPE_STREAMED_MEDIA:
+            call_async(q, media_iface, 'RequestStreams', handle,
+                [cs.MEDIA_STREAM_TYPE_AUDIO])
+        else:
+            call_async(q, conn.Requests, 'CreateChannel', request)
 
     def send_presence():
         jt.send_remote_presence()
@@ -82,8 +92,14 @@ def run_test(q, bus, conn, stream, jt, request_before_presence):
     jt.send_remote_disco_reply(info_event.stanza)
 
     # RequestStreams should now happily complete
-    q.expect('dbus-return', method='RequestStreams')
+    if channel_type == cs.CHANNEL_TYPE_STREAMED_MEDIA:
+        q.expect('dbus-return', method='RequestStreams')
+    else:
+        q.expect('dbus-return', method='CreateChannel')
 
 if __name__ == '__main__':
-    exec_test(test, timeout=10)
+    exec_test(lambda q, bus, conn, stream:
+        test(q, bus, conn, stream, cs.CHANNEL_TYPE_STREAMED_MEDIA), timeout=10)
+    exec_test(lambda q, bus, conn, stream:
+        test(q, bus, conn, stream, cs.CHANNEL_TYPE_CALL), timeout=10)
 

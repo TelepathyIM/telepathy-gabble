1bca8a2938e4acbf8c14a510ed7afa335a18d955
Sjoerd Simons <sjoerd.simons@collabora.co.uk>
2010-01-29 19:14:34 +0000

    Test the channel is as we expected it to be

--- tests/twisted/jingle/call-muc.py
+++ tests/twisted/jingle/call-muc.py
@@ -35,7 +35,13 @@ def run_incoming_test(jp, q, bus, conn, stream):
 
     stream.send(presence)
 
-    q.expect_many(EventPattern('dbus-signal', signal='NewChannels'))
+    e = q.expect ('dbus-signal',
+        signal='NewChannels',
+        predicate=lambda e: \
+            e.args[0][0][1][cs.CHANNEL_TYPE] == cs.CHANNEL_TYPE_CALL )
+
+    (path, props) = e.args[0][0]
+    general_tests (jp, q, bus, conn, stream, path, props)
 
 def run_outgoing_test(jp, q, bus, conn, stream):
     conn.Connect()
@@ -50,10 +56,21 @@ def run_outgoing_test(jp, q, bus, conn, stream):
           cs.CALL_INITIAL_AUDIO: True,
          }, byte_arrays = True)
 
-
     stream.send(make_muc_presence('none', 'participant', muc, 'test'))
 
-    q.expect_many (EventPattern ('dbus-return', method='CreateChannel'))
+    e = q.expect ('dbus-return', method='CreateChannel')
+
+    (path, props) = e.value
+
+    presence = make_muc_presence('owner', 'moderator', muc, 'bob')
+    muji = presence.addElement((ns.MUJI, 'muji'))
+    stream.send(presence)
+
+    general_tests (jp, q, bus, conn, stream, path, props)
+
+def general_tests (jp, q, bus, conn, stream, path, props):
+
+    assertEquals (cs.HT_ROOM, props[cs.TARGET_HANDLE_TYPE])
 
 if __name__ == '__main__':
     test_all_dialects(run_outgoing_test)

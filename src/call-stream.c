6e8d9d3042a9ab9b1898035c79820d46d2ae80b4
Sjoerd Simons <sjoerd.simons@collabora.co.uk>
2010-02-18 18:39:31 +0000

    Add some extra debug and fix identation

--- src/call-stream.c
+++ src/call-stream.c
@@ -375,7 +375,7 @@ call_stream_sender_update_state (GabbleCallStream *self,
     GabbleSendingState state)
 {
   GabbleCallStreamPrivate *priv = self->priv;
-  gpointer state_p;
+  gpointer state_p = 0;
   gboolean exists;
 
   exists = g_hash_table_lookup_extended (priv->senders,
@@ -385,6 +385,10 @@ call_stream_sender_update_state (GabbleCallStream *self,
 
   if (exists && GPOINTER_TO_UINT (state_p) == state)
     return FALSE;
+
+  DEBUG ("Updating sender %d state: %d => %d\n", contact,
+    GPOINTER_TO_UINT (state_p), state);
+
 
   g_hash_table_insert (priv->senders,
     GUINT_TO_POINTER (contact),
@@ -411,10 +415,14 @@ call_stream_update_sender_states (GabbleCallStream *self)
   created_by_us = gabble_jingle_content_is_created_by_us (priv->content);
   updates = g_hash_table_new (g_direct_hash, g_direct_equal);
 
+  DEBUG ("Created by us?: %d", created_by_us);
+
   if (gabble_jingle_content_sending (priv->content))
     {
       if (state == JINGLE_CONTENT_STATE_EMPTY && created_by_us)
-        local_state = GABBLE_SENDING_STATE_SENDING;
+        {
+          local_state = GABBLE_SENDING_STATE_SENDING;
+        }
       else if (created_by_us || state == JINGLE_CONTENT_STATE_ACKNOWLEDGED)
         {
           gpointer state_p;

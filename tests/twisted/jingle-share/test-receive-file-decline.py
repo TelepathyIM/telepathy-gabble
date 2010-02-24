7759db81a6617725055bd268c264e3d1a820f1ad
Youness Alaoui <youness.alaoui@collabora.co.uk>
2010-02-24 15:15:59 -0500

    Adding 4 new tests to the jingle share test suite. Allows testing of use cases from the receiver side

---
+++ tests/twisted/jingle-share/test-receive-file-decline.py
@@ -0,0 +1,66 @@
+import constants as cs
+
+from servicetest import EventPattern
+from file_transfer_helper import SendFileTest, ReceiveFileTest, \
+    FileTransferTest, exec_file_transfer_test
+
+class ReceiveFileDecline(ReceiveFileTest):
+
+    def __init__(self, file, address_type, access_control, access_control_param):
+        FileTransferTest.__init__(self, file, address_type, access_control,
+                                  access_control_param)
+        self._actions = [self.connect, self.set_ft_caps, None,
+
+                         self.wait_for_ft_caps, None,
+
+                         self.check_new_channel, self.create_ft_channel,
+                         self.close_and_check, self.done]
+
+    def close_and_check(self):
+        self.channel.Close()
+
+        _, event, _ = self.q.expect_many(
+            EventPattern('dbus-signal', signal='FileTransferStateChanged',
+                         args=[cs.FT_STATE_CANCELLED, \
+                                   cs.FT_STATE_CHANGE_REASON_LOCAL_STOPPED],
+                         path=self.channel.__dbus_object_path__),
+            EventPattern('stream-iq', stream=self.stream,
+                         iq_type='set', query_name='session'),
+            EventPattern('dbus-signal', signal='Closed',
+                         path=self.channel.__dbus_object_path__))
+
+
+        while event.query.getAttribute('type') != 'terminate':
+            event = self.q.expect('stream-iq', stream=self.stream,
+                                  iq_type='set', query_name='session')
+
+
+
+class SendFileDeclined (SendFileTest):
+    def __init__(self, file, address_type,
+                 access_control, acces_control_param):
+        FileTransferTest.__init__(self, file, address_type,
+                                  access_control, acces_control_param)
+
+        self._actions = [self.connect, self.set_ft_caps,
+                         self.check_ft_available, None,
+
+                         self.wait_for_ft_caps, None,
+
+                         self.request_ft_channel, self.create_ft_channel,
+                         self.provide_file, None,
+
+                         self.check_declined, self.close_channel, self.done]
+
+    def check_declined(self):
+        state_event = self.q.expect('dbus-signal',
+                                    signal='FileTransferStateChanged',
+                                    path=self.channel.__dbus_object_path__)
+
+        state, reason = state_event.args
+        assert state == cs.FT_STATE_CANCELLED
+        assert reason == cs.FT_STATE_CHANGE_REASON_REMOTE_STOPPED
+
+
+if __name__ == '__main__':
+    exec_file_transfer_test(SendFileDeclined, ReceiveFileDecline)

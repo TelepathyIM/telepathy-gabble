40dbeb2a20ab107c1d0b03d1ef81c5d253c900dc
Simon McVittie <simon.mcvittie@collabora.co.uk>
2010-02-25 14:35:18 +0000

    vcard/test-contact-info.py: assert that <ORG> can be unserialized

--- tests/twisted/vcard/test-contact-info.py
+++ tests/twisted/vcard/test-contact-info.py
@@ -3,7 +3,7 @@
 Test ContactInfo support.
 """
 
-from servicetest import call_async, EventPattern
+from servicetest import call_async, EventPattern, assertEquals
 from gabbletest import exec_test, acknowledge_iq, make_result_iq
 import constants as cs
 import dbus
@@ -34,12 +34,19 @@ def test(q, bus, conn, stream):
     label.addElement('LINE', content='42 West Wallaby Street')
     label.addElement('LINE', content="Bishop's Stortford\n")
     label.addElement('LINE', content='Huntingdon')
+    org = result.firstChildElement().addElement('ORG')
+    # ORG is a sequence of decreasingly large org.units, starting
+    # with the organisation name itself (but here we've moved the org name
+    # to the end, to make sure that works.)
+    org.addElement('ORGUNIT', content='Dept. of Examples')
+    org.addElement('ORGUNIT', content='Exemplary Team')
+    org.addElement('ORGNAME', content='Collabora Ltd.')
     stream.send(result)
 
     q.expect('dbus-signal', signal='ContactInfoChanged')
 
     # A second request should be satisfied from the cache.
-    assert conn.ContactInfo.GetContactInfo([handle]) == \
+    assertEquals(
         {handle: [(u'fn', [], [u'Bob']),
                   (u'n', [], [u'', u'Bob', u'', u'', u'']),
                   (u'nickname', [], [r'bob,bob1\,,bob2,bob3\,bob4']),
@@ -47,7 +54,11 @@ def test(q, bus, conn, stream):
                   (u'label', [], ['42 West Wallaby Street\n'
                       "Bishop's Stortford\n"
                       'Huntingdon\n']),
-                  ]}
+                  # ORG is a sequence of decreasingly large org.units, starting
+                  # with the organisation
+                  (u'org', [], [u'Collabora Ltd.', u'Dept. of Examples',
+                      u'Exemplary Team']),
+                  ]}, conn.ContactInfo.GetContactInfo([handle]))
 
 
 if __name__ == '__main__':

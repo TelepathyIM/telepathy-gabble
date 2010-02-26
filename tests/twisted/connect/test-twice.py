f04db7cb85f866bd0d9d6a8f65fb1131811beb59
Youness Alaoui <youness.alaoui@collabora.co.uk>
2010-02-26 15:26:50 -0500

    fix unit test by waiting for NewChannels signals This unit test stopped working after the fake server started echoing our presence. This actually caused a race condition that made the order of events to become inconsistent. The 'stream-authenticated' event is being sent locally by the fake server while the other events we are expecting are dbus-signals. Since the fake server now provides us with a roster, and echoes our presence there are a few more dbus signals that get sent (NewChannels and PresenceUpdated) which makes the StatusChanged dbus signal to arrive a bit 'late'. By waiting for those bottleneck-ing signals before connecting the second account, it makes the race condition disappear

--- tests/twisted/connect/test-twice.py
+++ tests/twisted/connect/test-twice.py
@@ -20,23 +20,45 @@ def test(q, bus, conns, streams):
     # Connection 1
     conn1.Connect()
     q.expect('dbus-signal', signal='StatusChanged',
-            args=[cs.CONN_STATUS_CONNECTING, cs.CSR_REQUESTED])
+             args=[cs.CONN_STATUS_CONNECTING, cs.CSR_REQUESTED],
+             path=conn1.object.__dbus_object_path__)
     q.expect('stream-authenticated')
     q.expect('dbus-signal', signal='PresenceUpdate',
-        args=[{1L: (0L, {u'available': {}})}])
+             args=[{1L: (0L, {u'available': {}})}],
+             path=conn1.object.__dbus_object_path__)
     q.expect('dbus-signal', signal='StatusChanged',
-            args=[cs.CONN_STATUS_CONNECTED, cs.CSR_REQUESTED])
+             args=[cs.CONN_STATUS_CONNECTED, cs.CSR_REQUESTED],
+             path=conn1.object.__dbus_object_path__)
+    for target_id in ('publish', 'subscribe', 'stored'):
+        event = q.expect('dbus-signal', signal='NewChannels',
+                         path=conn1.object.__dbus_object_path__)
+        channels = event.args[0]
+        assert len(channels) == 1
+        path, props = channels[0]
+        assert props[cs.CHANNEL_TYPE] == cs.CHANNEL_TYPE_CONTACT_LIST, props
+        assert props[cs.TARGET_ID] == target_id, props
 
     # Connection 2
     conn2.Connect()
     q.expect('dbus-signal', signal='StatusChanged',
-            args=[cs.CONN_STATUS_CONNECTING, cs.CSR_REQUESTED])
+             args=[cs.CONN_STATUS_CONNECTING, cs.CSR_REQUESTED],
+             path=conn2.object.__dbus_object_path__)
     q.expect('stream-authenticated')
     q.expect('dbus-signal', signal='PresenceUpdate',
-        args=[{1L: (0L, {u'available': {}})}])
+             args=[{1L: (0L, {u'available': {}})}],
+             path=conn2.object.__dbus_object_path__)
     q.expect('dbus-signal', signal='StatusChanged',
-            args=[cs.CONN_STATUS_CONNECTED, cs.CSR_REQUESTED])
+             args=[cs.CONN_STATUS_CONNECTED, cs.CSR_REQUESTED],
+             path=conn2.object.__dbus_object_path__)
 
+    for target_id in ('publish', 'subscribe', 'stored'):
+        event = q.expect('dbus-signal', signal='NewChannels',
+                         path=conn2.object.__dbus_object_path__)
+        channels = event.args[0]
+        assert len(channels) == 1
+        path, props = channels[0]
+        assert props[cs.CHANNEL_TYPE] == cs.CHANNEL_TYPE_CONTACT_LIST, props
+        assert props[cs.TARGET_ID] == target_id, props
 
 if __name__ == '__main__':
     exec_test(test, num_instances=2)

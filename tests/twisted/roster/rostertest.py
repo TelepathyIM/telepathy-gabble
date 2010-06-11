f39fdbf16756a68485f29c4a3fdda63b384bc493
Simon McVittie <simon.mcvittie@collabora.co.uk>
2010-06-11 12:17:55 +0100

    rostertest: add a more correct way to expect roster signals
    Many tests expect channels to appear in a particular order, which is a
    problem while porting to TpBaseContactList.

--- tests/twisted/roster/rostertest.py
+++ tests/twisted/roster/rostertest.py
@@ -14,6 +14,74 @@ def expect_group_channel(q, bus, conn, name, contacts, lp_contacts=[],
     return expect_contact_list_channel(q, bus, conn, cs.HT_GROUP, name,
         contacts, lp_contacts=lp_contacts, rp_contacts=rp_contacts)
 
+def get_contact_list_event_patterns(q, bus, conn, expected_handle_type, name):
+    expected_handle = conn.RequestHandles(expected_handle_type, [name])[0]
+
+    def new_channel_predicate(e):
+        path, type, handle_type, handle, suppress_handler = e.args
+        if type != cs.CHANNEL_TYPE_CONTACT_LIST:
+            return False
+        if handle_type != expected_handle_type:
+            return False
+        if handle != expected_handle:
+            return False
+        return True
+    new_channel_repr = ('NewChannel(., ContactList, %u, "%s", .)'
+            % (expected_handle_type, name))
+    new_channel_predicate.__repr__ = lambda: new_channel_repr
+
+    def new_channels_predicate(e):
+        info, = e.args
+        if len(info) != 1:
+            return False
+        path, props = info[0]
+        if props.get(cs.CHANNEL_TYPE) != cs.CHANNEL_TYPE_CONTACT_LIST:
+            return False
+        if props.get(cs.TARGET_HANDLE_TYPE) != expected_handle_type:
+            return False
+        if props.get(cs.TARGET_HANDLE) != expected_handle:
+            return False
+        return True
+    new_channels_repr = ('NewChannels(... ct=ContactList, ht=%u, name="%s"... )'
+            % (expected_handle_type, name))
+    new_channels_predicate.__repr__ = lambda: new_channels_repr
+
+    return (
+            EventPattern('dbus-signal', signal='NewChannel',
+                predicate=new_channel_predicate),
+            EventPattern('dbus-signal', signal='NewChannels',
+                predicate=new_channels_predicate)
+            )
+
+def expect_contact_list_signals(q, bus, conn, lists, groups=[]):
+    assert lists or groups
+
+    eps = []
+
+    for name in lists:
+        eps.extend(get_contact_list_event_patterns(q, bus, conn,
+            cs.HT_LIST, name))
+
+    for name in groups:
+        eps.extend(get_contact_list_event_patterns(q, bus, conn,
+            cs.HT_GROUP, name))
+
+    events = q.expect_many(*eps)
+    ret = []
+
+    for name in lists:
+        old_signal = events.pop(0)
+        new_signal = events.pop(0)
+        ret.append((old_signal, new_signal))
+
+    for name in groups:
+        old_signal = events.pop(0)
+        new_signal = events.pop(0)
+        ret.append((old_signal, new_signal))
+
+    assert len(events) == 0
+    return ret
+
 def expect_contact_list_channel(q, bus, conn, ht, name, contacts,
                                 lp_contacts=[], rp_contacts=[]):
     """
@@ -24,10 +92,28 @@ def expect_contact_list_channel(q, bus, conn, ht, name, contacts,
     Returns a proxy for the channel.
     """
 
-    old_signal, new_signal = q.expect_many(
-            EventPattern('dbus-signal', signal='NewChannel'),
-            EventPattern('dbus-signal', signal='NewChannels'),
-            )
+    # trailing comma to unpack a 1-tuple
+    if ht == cs.HT_LIST:
+        pair, = expect_contact_list_signals(q, bus, conn, [name])
+    elif ht == cs.HT_GROUP:
+        pair, = expect_contact_list_signals(q, bus, conn, [], [name])
+    else:
+        raise AssertionError("That's not a contact list handle type")
+
+    old_signal, new_signal = pair
+
+    return check_contact_list_signals(q, bus, conn, pair,
+            ht, name, contacts, lp_contacts, rp_contacts)
+
+def check_contact_list_signals(q, bus, conn, signals,
+        ht, name, contacts, lp_contacts=[], rp_contacts=[]):
+    """
+    Looks at NewChannel and NewChannels signals for the contact list with ID
+    'name' and checks that its members, lp members and rp members are exactly
+    'contacts', 'lp_contacts' and 'rp_contacts'.
+    Returns a proxy for the channel.
+    """
+    old_signal, new_signal = signals
 
     path, type, handle_type, handle, suppress_handler = old_signal.args
 
@@ -77,4 +163,3 @@ def expect_contact_list_channel(q, bus, conn, ht, name, contacts,
     assertContains('GroupFlags', group_props)
 
     return chan
-

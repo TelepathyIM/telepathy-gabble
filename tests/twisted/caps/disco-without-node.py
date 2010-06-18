f22cee75964a9ed3f76a386e2e645e2678f14b84
Will Thompson <will.thompson@collabora.co.uk>
2010-06-18 13:39:43 +0100

    Move test-disco.py into caps/, and modernize

---
+++ tests/twisted/caps/disco-without-node.py
@@ -0,0 +1,24 @@
+"""
+Test that Gabble responds to disco#info queries without a node='' attribute.
+"""
+
+from servicetest import assertEquals
+from gabbletest import exec_test, elem_iq, elem
+import constants as cs
+import ns
+
+def test(q, bus, conn, stream):
+    conn.Connect()
+    q.expect('dbus-signal', signal='StatusChanged',
+            args=[cs.CONN_STATUS_CONNECTED, cs.CSR_REQUESTED])
+
+    jid = 'foo@bar.com'
+
+    iq = elem_iq(stream, 'get', from_=jid)(elem(ns.DISCO_INFO, 'query'))
+    stream.send(iq)
+
+    event = q.expect('stream-iq', iq_type='result', to='foo@bar.com')
+    assertEquals(iq['id'], event.stanza['id'])
+
+if __name__ == '__main__':
+    exec_test(test)

aee17ce6a9748ce1ee2a86fed13afa598797a992
Simon McVittie <simon.mcvittie@collabora.co.uk>
2010-06-21 15:08:30 +0100

    roster/test-roster-item-deletion: test the new API too
    Also stop assuming that quux's handle will be 2 just because it's the
    second handle ever seen.

--- tests/twisted/roster/test-roster-item-deletion.py
+++ tests/twisted/roster/test-roster-item-deletion.py
@@ -5,14 +5,21 @@ Regression test for http://bugs.freedesktop.org/show_bug.cgi?id=19524
 import dbus
 
 from twisted.words.protocols.jabber.client import IQ
+from twisted.words.xish import domish
 
 from gabbletest import exec_test, acknowledge_iq
 from rostertest import expect_contact_list_signals, check_contact_list_signals
-from servicetest import assertLength
+from servicetest import (assertLength, assertEquals, EventPattern, call_async)
 import constants as cs
 import ns
 
-def test(q, bus, conn, stream):
+def test_ancient(q, bus, conn, stream):
+    test(q, bus, conn, stream, False)
+
+def test_modern(q, bus, conn, stream):
+    test(q, bus, conn, stream, True)
+
+def test(q, bus, conn, stream, modern=True):
     conn.Connect()
 
     def send_roster_iq(stream, jid, subscription):
@@ -32,7 +39,16 @@ def test(q, bus, conn, stream):
     item['jid'] = 'quux@foo.com'
     item['subscription'] = 'none'
 
+    quux_handle = conn.RequestHandles(cs.HT_CONTACT, ['quux@foo.com'])[0]
+
     stream.send(event.stanza)
+
+    # slight implementation detail: TpBaseContactList emits ContactsChanged
+    # before it announces its channels
+    q.expect('dbus-signal', signal='ContactsChanged',
+            interface=cs.CONN_IFACE_CONTACT_LIST, path=conn.object_path,
+            args=[{quux_handle:
+                (cs.SUBSCRIPTION_STATE_NO, cs.SUBSCRIPTION_STATE_NO, '')}, []])
 
     pairs = expect_contact_list_signals(q, bus, conn,
             ['publish', 'subscribe', 'stored'])
@@ -46,10 +62,40 @@ def test(q, bus, conn, stream):
 
     assertLength(0, pairs)      # i.e. we've checked all of them
 
-    stored.Group.RemoveMembers([dbus.UInt32(2)], '')
+    expectations = [
+            EventPattern('stream-iq',
+            iq_type='set', query_ns=ns.ROSTER),
+            ]
+
+    if modern:
+        call_async(q, conn.ContactList, 'RemoveContacts', [quux_handle])
+    else:
+        call_async(q, stored.Group, 'RemoveMembers', [quux_handle], '')
+        expectations.append(EventPattern('dbus-return',
+            method='RemoveMembers'))
+
+    event = q.expect_many(*expectations)[0]
+    item = event.query.firstChildElement()
+    assertEquals('quux@foo.com', item['jid'])
+    assertEquals('remove', item['subscription'])
+
     send_roster_iq(stream, 'quux@foo.com', 'remove')
 
-    acknowledge_iq(stream, q.expect('stream-iq').stanza)
+    q.expect_many(
+            EventPattern('dbus-signal', interface=cs.CHANNEL_IFACE_GROUP,
+                path=stored.object_path, signal='MembersChanged',
+                args=['', [], [quux_handle], [], [], 0, 0]),
+            EventPattern('dbus-signal', interface=cs.CONN_IFACE_CONTACT_LIST,
+                path=conn.object_path, signal='ContactsChanged',
+                args=[{}, [quux_handle]]),
+            EventPattern('stream-iq', iq_id='push', iq_type='result'),
+            )
+
+    acknowledge_iq(stream, event.stanza)
+
+    if modern:
+        q.expect('dbus-return', method='RemoveContacts')
 
 if __name__ == '__main__':
-    exec_test(test)
+    exec_test(test_ancient)
+    exec_test(test_modern)

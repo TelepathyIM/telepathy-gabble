49b006aa3614bc539f414ec5274604e8e0fdc793
Will Thompson <will.thompson@collabora.co.uk>
2010-09-10 16:29:51 +0100

    Turn privacy list test pseudo-methods into methods.
    Since we have a specific subclass of XmppXmlStream for a server
    supporting privacy lists, functions which take such a stream as the
    first argument should really be methods of that subclass. :)

--- tests/twisted/presence/invisible_helper.py
+++ tests/twisted/presence/invisible_helper.py
@@ -8,20 +8,28 @@ class Xep0186XmlStream(XmppXmlStream):
 class Xep0126XmlStream(XmppXmlStream):
     handle_privacy_lists = False
 
-def send_privacy_list_push_iq(stream, list_name):
-    iq = elem_iq(stream, 'set')(
-        elem(ns.PRIVACY, 'query')(
-            elem('list', name=list_name)
-            )
-        )
-    stream.send(iq)
-    return iq["id"]
+    def handle_get_all_privacy_lists(self, q, bus, conn, lists=[]):
+        list_elements = [elem('list', name=l) for l in lists]
 
-def send_privacy_list(stream, req_iq, list_items):
-    req_list = xpath.queryForNodes('//list', req_iq)[0]
-    iq = elem_iq(stream, "result", id=req_iq["id"])(
-        elem(ns.PRIVACY, 'query')(
-            elem('list', name=req_list["name"])(*list_items)
+        e = q.expect('stream-iq', query_ns=ns.PRIVACY, iq_type='get')
+        iq = elem_iq(self, "result", id=e.stanza["id"])(
+            elem(ns.PRIVACY, 'query')(*list_elements))
+        self.send(iq)
+
+    def send_privacy_list_push_iq(self, list_name):
+        iq = elem_iq(self, 'set')(
+            elem(ns.PRIVACY, 'query')(
+                elem('list', name=list_name)
+                )
             )
-        )
-    stream.send(iq)
+        self.send(iq)
+        return iq["id"]
+
+    def send_privacy_list(self, req_iq, list_items):
+        req_list = xpath.queryForNodes('//list', req_iq)[0]
+        iq = elem_iq(self, "result", id=req_iq["id"])(
+            elem(ns.PRIVACY, 'query')(
+                elem('list', name=req_list["name"])(*list_items)
+                )
+            )
+        self.send(iq)

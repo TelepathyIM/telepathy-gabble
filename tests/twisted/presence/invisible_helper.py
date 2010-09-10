13d656cbc63711d78190e9e73dd44d8e8631d6d1
Will Thompson <will.thompson@collabora.co.uk>
2010-09-10 17:40:25 +0100

    Add a XmlStream with privacy list invisibility
    This class allows you to run a test against a server which Gabble
    believes supports privacy list-based invisibilty without any further
    modifications to the test. This will allow us to check that invisibility
    support doesn't break other functionality.
    
    We already have a Xep0186Stream which claims to support another dialect
    of invisibility that Gabble understands. It doesn't actually let you
    become invisible, but this shouldn't matter for non-invisibility-related
    tests.

--- tests/twisted/presence/invisible_helper.py
+++ tests/twisted/presence/invisible_helper.py
@@ -1,4 +1,6 @@
-from gabbletest import XmppXmlStream, IQ, elem, elem_iq
+from gabbletest import (
+    XmppXmlStream, elem, elem_iq, send_error_reply, acknowledge_iq
+)
 import ns
 from twisted.words.xish import xpath
 
@@ -8,13 +10,17 @@ class Xep0186XmlStream(XmppXmlStream):
 class Xep0126XmlStream(XmppXmlStream):
     handle_privacy_lists = False
 
-    def handle_get_all_privacy_lists(self, q, bus, conn, lists=[]):
+    def send_privacy_list_list(self, iq_id, lists=[]):
         list_elements = [elem('list', name=l) for l in lists]
 
-        e = q.expect('stream-iq', query_ns=ns.PRIVACY, iq_type='get')
-        iq = elem_iq(self, "result", id=e.stanza["id"])(
+        iq = elem_iq(self, "result", id=iq_id)(
             elem(ns.PRIVACY, 'query')(*list_elements))
         self.send(iq)
+
+    def handle_get_all_privacy_lists(self, q, bus, conn, lists=[]):
+        e = q.expect('stream-iq', query_ns=ns.PRIVACY, iq_type='get')
+
+        self.send_privacy_list_list(e.iq_id, lists)
 
     def send_privacy_list_push_iq(self, list_name):
         iq = elem_iq(self, 'set')(
@@ -33,3 +39,43 @@ class Xep0126XmlStream(XmppXmlStream):
                 )
             )
         self.send(iq)
+
+class ValidInvisibleListStream(Xep0126XmlStream):
+    """This stream class pretends to be a server which supports privacy lists.
+    It has exactly one stored list, named 'invisible', which satisfies Gabble's
+    idea of what an invisible list should look like. Activating that list, or the  Any attempts to modify the
+    stored lists, or activate one, will fail.
+
+    The intention is that this class could be used to run presence tests
+    unrelated to invisibility against a server which supports invisibility."""
+
+    def __init__(self, event_func, authenticator):
+        Xep0126XmlStream.__init__(self, event_func, authenticator)
+
+        self.addObserver("/iq/query[@xmlns='%s']" % ns.PRIVACY,
+            self.privacy_list_iq_cb)
+
+    def privacy_list_iq_cb(self, iq):
+        if iq.getAttribute("type") == 'set':
+            active = xpath.queryForNodes("//active", iq)
+
+            if active and ('name' not in active[0].attributes or
+                           active[0]['name'] == 'invisible'):
+                acknowledge_iq(self, iq)
+            else:
+                # Don't allow other lists to be activated; and don't allow
+                # modifications.
+                send_error_reply(self, iq)
+        else:
+            requested_lists = xpath.queryForNodes('//list', iq)
+
+            if not requested_lists:
+                self.send_privacy_list_list(iq['id'], ['invisible'])
+            elif requested_lists[0]['name'] == 'invisible':
+                self.send_privacy_list(iq,
+                    [elem('item', action='deny', order='1')(
+                        elem('presence-out')
+                      )
+                    ])
+            else:
+                send_error_reply(self, iq, elem(ns.STANZA, 'item-not-found'))

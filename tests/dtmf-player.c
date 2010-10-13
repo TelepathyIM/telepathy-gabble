621ee607a8890a2082f186e1c0d134baf801fbfc
Simon McVittie <simon.mcvittie@collabora.co.uk>
2010-10-13 16:54:28 +0100

    dtmf-player test: test deferring tones

--- tests/dtmf-player.c
+++ tests/dtmf-player.c
@@ -82,6 +82,14 @@ finished_cb (GabbleDTMFPlayer *dtmf_player G_GNUC_UNUSED,
 }
 
 static void
+deferred_cb (GabbleDTMFPlayer *dtmf_player G_GNUC_UNUSED,
+    const gchar *tones,
+    Fixture *f)
+{
+  fixture_log (f, "deferred '%s'", tones);
+}
+
+static void
 setup (Fixture *f,
     gconstpointer nil G_GNUC_UNUSED)
 {
@@ -98,6 +106,8 @@ setup (Fixture *f,
       G_CALLBACK (stopped_tone_cb), f);
   g_signal_connect (f->dtmf_player, "finished",
       G_CALLBACK (finished_cb), f);
+  g_signal_connect (f->dtmf_player, "tones-deferred",
+      G_CALLBACK (deferred_cb), f);
 }
 
 static void
@@ -174,6 +184,31 @@ test_cancel_in_gap (Fixture *f,
 }
 
 static void
+test_cancel_in_pause (Fixture *f,
+    gconstpointer nil G_GNUC_UNUSED)
+{
+  gboolean ok;
+
+  ok = gabble_dtmf_player_play (f->dtmf_player, "#p*", 1, 1, 10000, &f->error);
+  g_assert_no_error (f->error);
+  g_assert (ok);
+  g_assert (gabble_dtmf_player_is_active (f->dtmf_player));
+  fixture_assert_log (f, "started '#'\n");
+
+  /* wait 1ms for it to reach the pause */
+  while (tp_strdiff (f->log->str, "started '#'\nstopped\n"))
+    g_main_context_iteration (NULL, TRUE);
+
+  gabble_dtmf_player_cancel (f->dtmf_player);
+  g_assert (!gabble_dtmf_player_is_active (f->dtmf_player));
+
+  fixture_assert_log (f,
+      "started '#'\n"
+      "stopped\n"
+      "cancelled\n");
+}
+
+static void
 test_sequence (Fixture *f,
     gconstpointer nil G_GNUC_UNUSED)
 {
@@ -206,6 +241,57 @@ test_sequence (Fixture *f,
       "finished\n");
 }
 
+static void
+test_wait (Fixture *f,
+    gconstpointer nil G_GNUC_UNUSED)
+{
+  gboolean ok;
+
+  ok = gabble_dtmf_player_play (f->dtmf_player, "*w#w", 1, 1, 1,
+      &f->error);
+  g_assert_no_error (f->error);
+  g_assert (ok);
+  g_assert (gabble_dtmf_player_is_active (f->dtmf_player));
+  fixture_assert_log (f, "started '*'\n");
+
+  while (gabble_dtmf_player_is_active (f->dtmf_player))
+    g_main_context_iteration (NULL, TRUE);
+
+  fixture_assert_log (f,
+      "started '*'\n"
+      "stopped\n"
+      "deferred '#w'\n"
+      "finished\n");
+
+  /* Now the user's ready to continue. */
+
+  ok = gabble_dtmf_player_play (f->dtmf_player, "#w", 1, 1, 1,
+      &f->error);
+  g_assert_no_error (f->error);
+  g_assert (ok);
+  g_assert (gabble_dtmf_player_is_active (f->dtmf_player));
+
+  fixture_assert_log (f,
+      "started '*'\n"
+      "stopped\n"
+      "deferred '#w'\n"
+      "finished\n"
+      "started '#'\n");
+
+  while (gabble_dtmf_player_is_active (f->dtmf_player))
+    g_main_context_iteration (NULL, TRUE);
+
+  /* It doesn't really make sense to defer "", so we just stop there */
+  fixture_assert_log (f,
+      "started '*'\n"
+      "stopped\n"
+      "deferred '#w'\n"
+      "finished\n"
+      "started '#'\n"
+      "stopped\n"
+      "finished\n");
+}
+
 static gboolean
 timeout_cb (gpointer nil G_GNUC_UNUSED)
 {
@@ -232,7 +318,9 @@ main (int argc,
   FIXTURE_TEST (empty);
   FIXTURE_TEST (cancel);
   FIXTURE_TEST (cancel_in_gap);
+  FIXTURE_TEST (cancel_in_pause);
   FIXTURE_TEST (sequence);
+  FIXTURE_TEST (wait);
 
   return g_test_run ();
 }

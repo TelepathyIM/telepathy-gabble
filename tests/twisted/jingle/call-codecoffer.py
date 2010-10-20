6886f08831e8a16c22a54e7a72c6834926280171
Jonny Lamb <jonny.lamb@collabora.co.uk>
2010-10-20 13:33:55 +0100

    tests: add more tests to call-codecoffer.py to test popping up codec offers
    Signed-off-by: Jonny Lamb <jonny.lamb@collabora.co.uk>

--- tests/twisted/jingle/call-codecoffer.py
+++ tests/twisted/jingle/call-codecoffer.py
@@ -3,6 +3,7 @@ Testing different methods related to the CodecOffer interface.
 """
 
 import dbus
+from dbus.exceptions import DBusException
 
 from servicetest import (EventPattern,
     assertEquals, assertContains, assertLength, assertNotEquals
@@ -66,7 +67,7 @@ def update_codecs(jt2):
         ])
     jt2.stream.send(jt2.jp.xml(node))
 
-def test(jp, q, bus, conn, stream):
+def prepare_test(jp, q, bus, conn, stream):
     remote_jid = 'foo@bar.com/Foo'
     jt2 = JingleTest2(jp, conn, q, stream, 'test@localhost', remote_jid)
 
@@ -88,6 +89,21 @@ def test(jp, q, bus, conn, stream):
                 cs.CHANNEL_TYPE_CALL + '/video/h264',
             ]),
         ])
+
+    return remote_jid, jt2, self_handle, remote_handle
+
+def try_to_access_old_offer(conn, path):
+    try:
+        offer = bus.get_object (conn.bus_name, path)
+        ret = offer.GetAll (cs.CALL_CONTENT_CODECOFFER,
+            dbus_interface=dbus.PROPERTIES_IFACE)
+    except Exception, e:
+        pass
+    else:
+        assert False, 'Offer still exists'
+
+def test_incoming(jp, q, bus, conn, stream):
+    remote_jid, jt2, self_handle, remote_handle = prepare_test(jp, q, bus, conn, stream)
 
     jt2.incoming_call()
 
@@ -127,14 +143,7 @@ def test(jp, q, bus, conn, stream):
     chan.Close(dbus_interface=cs.CHANNEL)
     signal = q.expect('dbus-signal', signal='ChannelClosed')
 
-    try:
-        offer = bus.get_object (conn.bus_name, path)
-        ret = offer.GetAll (cs.CALL_CONTENT_CODECOFFER,
-            dbus_interface=dbus.PROPERTIES_IFACE)
-    except Exception, e:
-        pass
-    else:
-        assert False, 'Offer still exists'
+    try_to_access_old_offer(conn, path)
 
     try:
         ret = conn.GetAll (cs.CONN, dbus_interface=dbus.PROPERTIES_IFACE)
@@ -146,5 +155,117 @@ def test(jp, q, bus, conn, stream):
         # SelfHandle, and might also have Interfaces and Status
         assert len(ret) > 0
 
+def test_outgoing(jp, q, bus, conn, stream):
+    remote_jid, jt2, self_handle, remote_handle = prepare_test(jp, q, bus, conn, stream)
+
+    # make a new outgoing call
+    conn.CreateChannel({
+            cs.TARGET_HANDLE_TYPE: cs.HT_CONTACT,
+            cs.CHANNEL_TYPE: cs.CHANNEL_TYPE_CALL,
+            cs.TARGET_HANDLE: remote_handle,
+            cs.CALL_INITIAL_AUDIO: True,
+            cs.CALL_INITIAL_VIDEO: False
+            }, dbus_interface=cs.CONN_IFACE_REQUESTS)
+
+    ret = q.expect_many(EventPattern('dbus-signal', signal='NewChannels',
+            predicate=lambda e:
+                cs.CHANNEL_TYPE_CALL in e.args[0][0][1].values()),
+        # a codec offer appears already!
+        EventPattern('dbus-signal', signal='NewCodecOffer'))
+
+    # all the basic stuff is already tested in call-basics.py
+    chan = bus.get_object(conn.bus_name, ret[0].args[0][0][0])
+
+    # there is no remote codec information, so this should be empty
+    assertEquals(ret[1].args[1], {})
+
+    # get a list of audio codecs we can support
+    codecs = jt2.get_call_audio_codecs_dbus()
+
+    # make sure UpdateCodecs fails
+    props = chan.GetAll(cs.CHANNEL_TYPE_CALL,
+            dbus_interface=dbus.PROPERTIES_IFACE)
+
+    content = bus.get_object(conn.bus_name, props["Contents"][0])
+
+    try:
+        content.UpdateCodecs(codecs, dbus_interface=cs.CALL_CONTENT_IFACE_MEDIA)
+    except DBusException, e:
+        # this should fail now that there is a codec offer around
+        if e.get_dbus_name() != cs.NOT_AVAILABLE:
+            raise e
+    else:
+        assert false
+
+    # we'll need these later
+    content_props = content.GetAll(cs.CALL_CONTENT,
+            dbus_interface=dbus.PROPERTIES_IFACE)
+
+
+    # make an offer they can't refuse
+    offer = bus.get_object(conn.bus_name, ret[1].args[0])
+    props = offer.GetAll(cs.CALL_CONTENT_CODECOFFER,
+            dbus_interface=dbus.PROPERTIES_IFACE)
+
+    # this also needs to be empty
+    assertEquals(props["RemoteContactCodecMap"], {})
+
+    offer.Accept(codecs, dbus_interface=cs.CALL_CONTENT_CODECOFFER)
+
+    q.expect('dbus-signal', signal='CodecsChanged',
+             args=[{self_handle: codecs}, []])
+
+    chan.Accept(dbus_interface=cs.CHANNEL_TYPE_CALL)
+
+    # add candidates
+    cstream = bus.get_object(conn.bus_name, content_props["Streams"][0])
+    candidates = jt2.get_call_remote_transports_dbus ()
+    cstream.AddCandidates (candidates,
+        dbus_interface=cs.CALL_STREAM_IFACE_MEDIA)
+
+    ret = q.expect_many(EventPattern('dbus-signal', signal='LocalCandidatesAdded'),
+                        EventPattern('stream-iq',
+            predicate=jp.action_predicate('session-initiate')))
+    assertEquals (candidates, ret[0].args[0])
+
+    jt2.parse_session_initiate(ret[1].query)
+
+    cstream.CandidatesPrepared (dbus_interface=cs.CALL_STREAM_IFACE_MEDIA)
+
+    local_candidates = cstream.Get(cs.CALL_STREAM_IFACE_MEDIA,
+        "LocalCandidates", dbus_interface=dbus.PROPERTIES_IFACE)
+    assertEquals (candidates,  local_candidates)
+
+    endpoints = cstream.Get(cs.CALL_STREAM_IFACE_MEDIA,
+        "Endpoints", dbus_interface=dbus.PROPERTIES_IFACE)
+    assertLength (1, endpoints)
+
+    # accept with a subset of the codecs
+    old_codecs = jt2.audio_codecs
+    jt2.audio_codecs = jt2.audio_codecs[:-1] # all but the last one
+
+    # session-accept
+    jt2.accept()
+
+    ret = q.expect('dbus-signal', signal='NewCodecOffer')
+
+    # make sure the codec offer has the updated codecs
+    assertEquals(ret.args[1], {remote_handle: codecs[:-1]})
+
+    # accept new offer
+    offer = bus.get_object(conn.bus_name, ret.args[0])
+    offer.Accept(codecs[:-1], dbus_interface=cs.CALL_CONTENT_CODECOFFER)
+
+    # now we should both have the smaller set of codecs, easy
+    o = q.expect ('dbus-signal', signal='CodecsChanged')
+    assertEquals ([{ self_handle: codecs[:-1], remote_handle: codecs[:-1]}, []],
+        o.args)
+
+    chan.Close(dbus_interface=cs.CHANNEL)
+    signal = q.expect('dbus-signal', signal='ChannelClosed')
+
+    try_to_access_old_offer(conn, ret.args[0])
+
 if __name__ == '__main__':
-    test_dialects(test, [JingleProtocol031])
+    test_dialects(test_incoming, [JingleProtocol031])
+    test_dialects(test_outgoing, [JingleProtocol031])

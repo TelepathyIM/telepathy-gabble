454ab32c41737990065a9cd2176fa43871a5dc49
Simon McVittie <simon.mcvittie@collabora.co.uk>
2010-11-24 13:09:31 +0000

    sasl/plain test: convert to SaslComplexAuthenticator so we can test other reasons for failure

--- tests/twisted/sasl/plain.py
+++ tests/twisted/sasl/plain.py
@@ -12,11 +12,12 @@ import dbus
 from servicetest import EventPattern, assertEquals, assertContains, call_async
 from gabbletest import exec_test
 import constants as cs
-from saslutil import SaslPlainAuthenticator, connect_and_get_sasl_channel, \
+from saslutil import SaslComplexAuthenticator, connect_and_get_sasl_channel, \
     abort_auth
 
 JID = "test@example.org"
 PASSWORD = "pass"
+INITIAL_RESPONSE = '\0' + JID.split('@')[0] + '\0' + PASSWORD
 
 def test_plain_success(q, bus, conn, stream):
     chan, props = connect_and_get_sasl_channel(q, bus, conn)
@@ -28,9 +29,16 @@ def test_plain_success(q, bus, conn, stream):
     # We can't necessarily do PLAIN auth without this information.
     assertEquals('test', props.get(cs.SASL_DEFAULT_USERNAME))
 
-    chan.SASLAuthentication.StartMechanismWithData(
-        'PLAIN', '\0' + JID.split('@')[0] + '\0' + PASSWORD)
+    chan.SASLAuthentication.StartMechanismWithData('PLAIN', INITIAL_RESPONSE)
+    e, _ = q.expect_many(
+            EventPattern('sasl-auth', initial_response=INITIAL_RESPONSE),
+            EventPattern('dbus-signal', signal='SASLStatusChanged',
+                interface=cs.CHANNEL_IFACE_SASL_AUTH,
+                args=[cs.SASL_STATUS_IN_PROGRESS, '', {}]),
+            )
+    authenticator = e.authenticator
 
+    authenticator.success(None)
     q.expect('dbus-signal', signal='SASLStatusChanged',
              interface=cs.CHANNEL_IFACE_SASL_AUTH,
              args=[cs.SASL_STATUS_SERVER_SUCCEEDED, '', {}])
@@ -47,9 +55,19 @@ def test_plain_success(q, bus, conn, stream):
 def test_plain_no_account(q, bus, conn, stream):
     chan, props = connect_and_get_sasl_channel(q, bus, conn)
 
-    chan.SASLAuthentication.StartMechanismWithData(
-        'PLAIN', '\0' + JID.split('@')[0] + '\0' + PASSWORD)
+    assertEquals('example.com', props.get(cs.SASL_DEFAULT_REALM))
+    assertEquals('', props.get(cs.SASL_DEFAULT_USERNAME))
 
+    chan.SASLAuthentication.StartMechanismWithData('PLAIN', INITIAL_RESPONSE)
+    e, _ = q.expect_many(
+            EventPattern('sasl-auth', initial_response=INITIAL_RESPONSE),
+            EventPattern('dbus-signal', signal='SASLStatusChanged',
+                interface=cs.CHANNEL_IFACE_SASL_AUTH,
+                args=[cs.SASL_STATUS_IN_PROGRESS, '', {}]),
+            )
+    authenticator = e.authenticator
+
+    authenticator.success(None)
     q.expect('dbus-signal', signal='SASLStatusChanged',
              interface=cs.CHANNEL_IFACE_SASL_AUTH,
              args=[cs.SASL_STATUS_SERVER_SUCCEEDED, '', {}])
@@ -66,13 +84,16 @@ def test_plain_no_account(q, bus, conn, stream):
 def test_plain_fail(q, bus, conn, stream):
     chan, props = connect_and_get_sasl_channel(q, bus, conn)
 
-    chan.SASLAuthentication.StartMechanismWithData(
-        'PLAIN', '\0' + JID.split('@')[0] + '\0' + 'wrong')
+    chan.SASLAuthentication.StartMechanismWithData('PLAIN', INITIAL_RESPONSE)
+    e, _ = q.expect_many(
+            EventPattern('sasl-auth', initial_response=INITIAL_RESPONSE),
+            EventPattern('dbus-signal', signal='SASLStatusChanged',
+                interface=cs.CHANNEL_IFACE_SASL_AUTH,
+                args=[cs.SASL_STATUS_IN_PROGRESS, '', {}]),
+            )
+    authenticator = e.authenticator
 
-    e = q.expect('dbus-signal', signal='SASLStatusChanged',
-                 interface=cs.CHANNEL_IFACE_SASL_AUTH,
-                 args=[cs.SASL_STATUS_IN_PROGRESS, '', {}])
-
+    authenticator.failure('not-authorized')
     e = q.expect('dbus-signal', signal='SASLStatusChanged',
                  interface=cs.CHANNEL_IFACE_SASL_AUTH)
     assertEquals(e.args[:2],
@@ -88,9 +109,16 @@ def test_plain_fail(q, bus, conn, stream):
 def test_plain_abort(q, bus, conn, stream):
     chan, props = connect_and_get_sasl_channel(q, bus, conn)
 
-    chan.SASLAuthentication.StartMechanismWithData(
-        'PLAIN', '\0' + JID.split('@')[0] + '\0' + PASSWORD)
+    chan.SASLAuthentication.StartMechanismWithData('PLAIN', INITIAL_RESPONSE)
+    e, _ = q.expect_many(
+            EventPattern('sasl-auth', initial_response=INITIAL_RESPONSE),
+            EventPattern('dbus-signal', signal='SASLStatusChanged',
+                interface=cs.CHANNEL_IFACE_SASL_AUTH,
+                args=[cs.SASL_STATUS_IN_PROGRESS, '', {}]),
+            )
+    authenticator = e.authenticator
 
+    authenticator.success(None)
     q.expect('dbus-signal', signal='SASLStatusChanged',
              interface=cs.CHANNEL_IFACE_SASL_AUTH,
              args=[cs.SASL_STATUS_SERVER_SUCCEEDED, '', {}])
@@ -105,14 +133,21 @@ def test_bad_usage(q, bus, conn, stream):
             'This is uncalled for')
     q.expect('dbus-error', method='Respond', name=cs.NOT_AVAILABLE)
 
-    chan.SASLAuthentication.StartMechanismWithData(
-        'PLAIN', '\0' + JID.split('@')[0] + '\0' + PASSWORD)
+    chan.SASLAuthentication.StartMechanismWithData('PLAIN', INITIAL_RESPONSE)
+    e, _ = q.expect_many(
+            EventPattern('sasl-auth', initial_response=INITIAL_RESPONSE),
+            EventPattern('dbus-signal', signal='SASLStatusChanged',
+                interface=cs.CHANNEL_IFACE_SASL_AUTH,
+                args=[cs.SASL_STATUS_IN_PROGRESS, '', {}]),
+            )
+    authenticator = e.authenticator
 
     call_async(q, chan.SASLAuthentication, 'StartMechanismWithData',
             'PLAIN', 'foo')
     q.expect('dbus-error', method='StartMechanismWithData',
             name=cs.NOT_AVAILABLE)
 
+    authenticator.success(None)
     q.expect('dbus-signal', signal='SASLStatusChanged',
              interface=cs.CHANNEL_IFACE_SASL_AUTH,
              args=[cs.SASL_STATUS_SERVER_SUCCEEDED, '', {}])
@@ -124,21 +159,21 @@ def test_bad_usage(q, bus, conn, stream):
 if __name__ == '__main__':
     exec_test(
         test_plain_success, {'password': None, 'account' : JID},
-        authenticator=SaslPlainAuthenticator(JID.split('@')[0], PASSWORD))
+        authenticator=SaslComplexAuthenticator('test', ['PLAIN']))
 
     exec_test(
         test_plain_no_account,
         {'password': None, 'account' : 'example.com'},
-        authenticator=SaslPlainAuthenticator(JID.split('@')[0], PASSWORD))
+        authenticator=SaslComplexAuthenticator('test', ['PLAIN']))
 
     exec_test(
         test_plain_fail, {'password': None, 'account' : JID},
-        authenticator=SaslPlainAuthenticator(JID.split('@')[0], PASSWORD))
+        authenticator=SaslComplexAuthenticator('test', ['PLAIN']))
 
     exec_test(
         test_plain_abort, {'password': None, 'account' : JID},
-        authenticator=SaslPlainAuthenticator(JID.split('@')[0], PASSWORD))
+        authenticator=SaslComplexAuthenticator('test', ['PLAIN']))
 
     exec_test(
         test_bad_usage, {'password': None, 'account' : JID},
-        authenticator=SaslPlainAuthenticator(JID.split('@')[0], PASSWORD))
+        authenticator=SaslComplexAuthenticator('test', ['PLAIN']))

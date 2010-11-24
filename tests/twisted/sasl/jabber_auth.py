769d107867503ef24bf6882c1c65d80def3df8f6
Simon McVittie <simon.mcvittie@collabora.co.uk>
2010-11-24 13:53:32 +0000

    jabber_auth test: drive the SASL authenticator interactively

--- tests/twisted/sasl/jabber_auth.py
+++ tests/twisted/sasl/jabber_auth.py
@@ -12,19 +12,31 @@ import dbus
 from servicetest import EventPattern, assertEquals, assertSameSets
 from gabbletest import exec_test, JabberXmlStream, JabberAuthenticator
 import constants as cs
-from saslutil import connect_and_get_sasl_channel, abort_auth
+from saslutil import expect_sasl_channel, abort_auth
 
 JID = "test@localhost"
 PASSWORD = "pass"
 
 def test_jabber_pass_success(q, bus, conn, stream):
-    chan, props = connect_and_get_sasl_channel(q, bus, conn)
+    conn.Connect()
+
+    q.expect('dbus-signal', signal='StatusChanged',
+             args=[cs.CONN_STATUS_CONNECTING, cs.CSR_REQUESTED])
+
+    e = q.expect('auth-initial-iq')
+    authenticator = e.authenticator
+    authenticator.respondToInitialIq(e.iq)
+
+    chan, props = expect_sasl_channel(q, bus, conn)
 
     assertSameSets(['X-TELEPATHY-PASSWORD'],
             props.get(cs.SASL_AVAILABLE_MECHANISMS))
 
     chan.SASLAuthentication.StartMechanismWithData('X-TELEPATHY-PASSWORD',
             PASSWORD)
+
+    e = q.expect('auth-second-iq')
+    authenticator.respondToSecondIq(e.iq)
 
     q.expect('dbus-signal', signal='SASLStatusChanged',
              interface=cs.CHANNEL_IFACE_SASL_AUTH,
@@ -43,4 +55,5 @@ if __name__ == '__main__':
     exec_test(
         test_jabber_pass_success, {'password': None, 'account' : JID},
         protocol=JabberXmlStream,
-        authenticator=JabberAuthenticator(JID.split('@')[0], PASSWORD))
+        authenticator=JabberAuthenticator(JID.split('@')[0], PASSWORD,
+            emit_events=True))

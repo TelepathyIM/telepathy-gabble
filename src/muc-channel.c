72b0a0a4dce061c12cbf406a54059c31beb1a6f7
Jonny Lamb <jonny.lamb@collabora.co.uk>
2011-01-03 12:33:19 +0000

    muc-channel: emit SubjectChanged where appropriate
    Signed-off-by: Jonny Lamb <jonny.lamb@collabora.co.uk>

--- src/muc-channel.c
+++ src/muc-channel.c
@@ -502,10 +502,11 @@ gabble_muc_channel_constructed (GObject *obj)
    * checked so we know it's valid. */
   g_assert (ok);
 
-  priv->subject = tp_value_array_build (3,
+  priv->subject = tp_value_array_build (4,
       G_TYPE_STRING, "",
       G_TYPE_STRING, "",
       G_TYPE_INT64, (gint64) 0,
+      G_TYPE_UINT, 0,
       G_TYPE_INVALID);
 
   if (priv->invited)
@@ -1729,6 +1730,7 @@ update_permissions (GabbleMucChannel *chan)
   TpChannelGroupFlags grp_flags_add, grp_flags_rem;
   TpPropertyFlags prop_flags_add, prop_flags_rem;
   TpIntSet *changed_props_val, *changed_props_flags;
+  GabbleRoomSubjectFlags room_subject_flags = 0;
 
   /*
    * Update group flags.
@@ -1770,16 +1772,28 @@ update_permissions (GabbleMucChannel *chan)
     {
       prop_flags_add = TP_PROPERTY_FLAG_WRITE;
       prop_flags_rem = 0;
+      room_subject_flags = GABBLE_ROOM_SUBJECT_FLAG_CAN_SET |
+        GABBLE_ROOM_SUBJECT_FLAG_PRESENT;
     }
   else
     {
       prop_flags_add = 0;
       prop_flags_rem = TP_PROPERTY_FLAG_WRITE;
+      room_subject_flags = GABBLE_ROOM_SUBJECT_FLAG_PRESENT;
     }
 
   tp_properties_mixin_change_flags (G_OBJECT (chan),
       ROOM_PROP_SUBJECT, prop_flags_add, prop_flags_rem,
       changed_props_flags);
+
+  g_value_set_uint (g_value_array_get_nth (priv->subject, 3),
+      room_subject_flags);
+
+  gabble_svc_channel_interface_room_emit_subject_changed (chan,
+      g_value_get_string (g_value_array_get_nth (priv->subject, 0)),
+      g_value_get_string (g_value_array_get_nth (priv->subject, 1)),
+      g_value_get_int64 (g_value_array_get_nth (priv->subject, 2)),
+      room_subject_flags);
 
   /* The room properties below are part of the "room definition", so are
    * defined by the XEP to be editable only by owners. */
@@ -2691,6 +2705,7 @@ _gabble_muc_channel_handle_subject (GabbleMucChannel *chan,
   TpIntSet *changed_values, *changed_flags;
   GValue val = { 0, };
   const gchar *actor;
+  GabbleRoomSubjectFlags room_subject_flags = 0;
 
   g_assert (GABBLE_IS_MUC_CHANNEL (chan));
 
@@ -2788,6 +2803,8 @@ _gabble_muc_channel_handle_subject (GabbleMucChannel *chan,
   g_value_unset (&val);
 
   /* Room properties */
+  room_subject_flags = g_value_get_uint (
+      g_value_array_get_nth (priv->subject, 3));
   g_value_array_free (priv->subject);
 
   if (handle_type == TP_HANDLE_TYPE_CONTACT)
@@ -2803,13 +2820,20 @@ _gabble_muc_channel_handle_subject (GabbleMucChannel *chan,
       actor = "";
     }
 
-  priv->subject = tp_value_array_build (3,
+  priv->subject = tp_value_array_build (4,
       G_TYPE_STRING, subject,
       G_TYPE_STRING, actor,
       G_TYPE_INT64, (gint64) timestamp,
+      G_TYPE_UINT, room_subject_flags,
       G_TYPE_INVALID);
 
   /* Emit signals */
+  gabble_svc_channel_interface_room_emit_subject_changed (chan,
+      g_value_get_string (g_value_array_get_nth (priv->subject, 0)),
+      g_value_get_string (g_value_array_get_nth (priv->subject, 1)),
+      g_value_get_int64 (g_value_array_get_nth (priv->subject, 2)),
+      g_value_get_uint (g_value_array_get_nth (priv->subject, 3)));
+
   tp_properties_mixin_emit_changed (G_OBJECT (chan), changed_values);
   tp_properties_mixin_emit_flags (G_OBJECT (chan), changed_flags);
   tp_intset_destroy (changed_values);

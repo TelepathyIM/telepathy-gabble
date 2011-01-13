2986f9cb03d68e3726ea129cd84d9e773f4f1b3d
Jonny Lamb <jonny.lamb@collabora.co.uk>
2011-01-13 15:06:38 +0000

    presence-before-closing.py: test Disconnect() right after Close()
    Signed-off-by: Jonny Lamb <jonny.lamb@collabora.co.uk>

--- tests/twisted/muc/presence-before-closing.py
+++ tests/twisted/muc/presence-before-closing.py
@@ -58,7 +58,58 @@ def test(q, bus, conn, stream):
 
     # now that the channel has finally closed, let's try and request
     # it again which should succeed!
-    join_muc(q, bus, conn, stream, room)
+    _, chan, _, _ = join_muc(q, bus, conn, stream, room)
+
+    # let's clear up though.
+    chan.Close()
+    event = q.expect('stream-presence', to=room + '/test')
+    echo_muc_presence(q, stream, event.stanza, 'none', 'participant')
+    q.expect_many(EventPattern('dbus-signal', signal='Closed'),
+                  EventPattern('dbus-signal', signal='ChannelClosed'))
+
+def test_then_disconnect(q, bus, conn, stream):
+    conn.Connect()
+    q.expect('dbus-signal', signal='StatusChanged',
+            args=[cs.CONN_STATUS_CONNECTED, cs.CSR_REQUESTED])
+
+    room = 'test@conf.localhost'
+
+    room_handle, chan, path, props, disco = join_muc(q, bus, conn, stream,
+            room,
+            also_capture=[EventPattern('stream-iq', iq_type='get',
+                query_name='query', query_ns=ns.DISCO_INFO, to=room)])
+
+    sync_dbus(bus, q, conn)
+
+    # we call Close...
+    call_async(q, chan, 'Close')
+    q.expect('dbus-return', method='Close')
+
+    # ...so gabble announces our unavailable presence to the MUC.
+    event = q.expect('stream-presence', to=room + '/test')
+    elem = event.stanza
+    assertEquals('unavailable', elem['type'])
+
+    # oh no, but now we want to disconnect.
+    call_async(q, conn, 'Disconnect')
+
+    # the muc factory is told to close everything, so it does so
+    # without announcing it to the channel because it does it
+    # forcibly, so the channels disappear.
+    q.expect_many(EventPattern('dbus-signal', signal='Closed'),
+                  EventPattern('dbus-signal', signal='ChannelClosed'))
+
+    # now echo the unavailable presence; this shouldn't be handled
+    # because the channel has already closed.
+    echo_muc_presence(q, stream, elem, 'none', 'participant')
+
+    # send the stream footer so that the connection thinks it's
+    # property disconnected now.
+    stream.sendFooter()
+
+    # finally, Disconnect returns
+    q.expect('dbus-return', method='Disconnect')
 
 if __name__ == '__main__':
     exec_test(test)
+    exec_test(test_then_disconnect)

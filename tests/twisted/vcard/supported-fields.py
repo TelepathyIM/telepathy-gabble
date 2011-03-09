3968462da687029e83f5e3e18b50c81c0232d2bf
Jonny Lamb <jonny.lamb@collabora.co.uk>
2011-03-09 16:51:55 +0000

    vcard/supported-fields.py: test for Overwritten_By_Nickname flag
    Also, fix the name of the other contact info field flag which was
    clearly changed at some point before it became undrafted.
    
    Signed-off-by: Jonny Lamb <jonny.lamb@collabora.co.uk>

--- tests/twisted/vcard/supported-fields.py
+++ tests/twisted/vcard/supported-fields.py
@@ -23,7 +23,8 @@ from servicetest import (EventPattern, assertEquals, call_async)
 from gabbletest import (exec_test, GoogleXmlStream)
 import constants as cs
 
-PARAMS_MANDATORY = cs.CONTACT_INFO_FIELD_FLAG_PARAMETERS_MANDATORY
+PARAMS_EXACT = cs.CONTACT_INFO_FIELD_FLAG_PARAMETERS_EXACT
+OVERWRITTEN_BY_NICKNAME = cs.CONTACT_INFO_FIELD_FLAG_OVERWRITTEN_BY_NICKNAME
 UNLIMITED = 0xffffffffL
 
 def types(s):
@@ -38,8 +39,8 @@ def check_google_props(props):
     for f in sf:
         f[1].sort()     # type-parameters
     assertEquals([
-        ('fn', [], PARAMS_MANDATORY, 1),
-        ('n', [], PARAMS_MANDATORY, 1),
+        ('fn', [], PARAMS_EXACT | OVERWRITTEN_BY_NICKNAME, 1),
+        ('n', [], PARAMS_EXACT, 1),
         ], sf)
 
 def check_normal_props(props):
@@ -50,29 +51,29 @@ def check_normal_props(props):
         f[1].sort()     # type-parameters
     assertEquals([
         ('adr', types('home work postal parcel dom intl pref'), 0, UNLIMITED),
-        ('bday', [], PARAMS_MANDATORY, UNLIMITED),
+        ('bday', [], PARAMS_EXACT, UNLIMITED),
         ('email', types('home work internet pref x400'), 0, UNLIMITED),
-        ('fn', [], PARAMS_MANDATORY, 1),
-        ('geo', [], PARAMS_MANDATORY, 1),
+        ('fn', [], PARAMS_EXACT, 1),
+        ('geo', [], PARAMS_EXACT, 1),
         ('label', types('home work postal parcel dom intl pref'), 0,
             UNLIMITED),
-        ('mailer', [], PARAMS_MANDATORY, UNLIMITED),
-        ('n', [], PARAMS_MANDATORY, 1),
-        ('nickname', [], PARAMS_MANDATORY, UNLIMITED),
-        ('note', [], PARAMS_MANDATORY, UNLIMITED),
-        ('org', [], PARAMS_MANDATORY, UNLIMITED),
-        ('prodid', [], PARAMS_MANDATORY, UNLIMITED),
-        ('rev', [], PARAMS_MANDATORY, UNLIMITED),
-        ('role', [], PARAMS_MANDATORY, UNLIMITED),
-        ('sort-string', [], PARAMS_MANDATORY, UNLIMITED),
+        ('mailer', [], PARAMS_EXACT, UNLIMITED),
+        ('n', [], PARAMS_EXACT, 1),
+        ('nickname', [], PARAMS_EXACT | OVERWRITTEN_BY_NICKNAME, UNLIMITED),
+        ('note', [], PARAMS_EXACT, UNLIMITED),
+        ('org', [], PARAMS_EXACT, UNLIMITED),
+        ('prodid', [], PARAMS_EXACT, UNLIMITED),
+        ('rev', [], PARAMS_EXACT, UNLIMITED),
+        ('role', [], PARAMS_EXACT, UNLIMITED),
+        ('sort-string', [], PARAMS_EXACT, UNLIMITED),
         ('tel', types('home work voice fax pager msg cell video bbs '
             'modem isdn pcs pref'), 0, UNLIMITED),
-        ('title', [], PARAMS_MANDATORY, UNLIMITED),
-        ('tz', [], PARAMS_MANDATORY, UNLIMITED),
-        ('uid', [], PARAMS_MANDATORY, UNLIMITED),
-        ('url', [], PARAMS_MANDATORY, UNLIMITED),
-        ('x-desc', [], PARAMS_MANDATORY, UNLIMITED),
-        ('x-jabber', [], PARAMS_MANDATORY, UNLIMITED),
+        ('title', [], PARAMS_EXACT, UNLIMITED),
+        ('tz', [], PARAMS_EXACT, UNLIMITED),
+        ('uid', [], PARAMS_EXACT, UNLIMITED),
+        ('url', [], PARAMS_EXACT, UNLIMITED),
+        ('x-desc', [], PARAMS_EXACT, UNLIMITED),
+        ('x-jabber', [], PARAMS_EXACT, UNLIMITED),
         ], sf)
 
 def test_google(q, bus, conn, stream):

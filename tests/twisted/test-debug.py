38a6178a7529e3b1bf1ab8439185541e01c18dd5
David Laban <david.laban@collabora.co.uk>
2011-05-31 16:10:16 -0400

    test-debug.py: actually check that no debugging happens
    Reviewed-by: Will Thompson <will.thompson@collabora.co.uk>

--- tests/twisted/test-debug.py
+++ tests/twisted/test-debug.py
@@ -5,14 +5,10 @@ Test the debug message interface.
 
 import dbus
 
+from servicetest import assertEquals, sync_dbus
 from gabbletest import exec_test
 import constants as cs
 from config import DEBUGGING
-
-if not DEBUGGING:
-    print " --   Not testing debugger, built with --disable-debug"
-    raise SystemExit(77)
-
 path = '/org/freedesktop/Telepathy/debug'
 iface = 'org.freedesktop.Telepathy.Debug'
 
@@ -35,17 +31,30 @@ def test(q, bus, conn, stream):
     assert props_iface.Get(iface, 'Enabled') == False
     props_iface.Set(iface, 'Enabled', True)
 
+    channel_path = conn.RequestChannel(
+        cs.CHANNEL_TYPE_TEXT, cs.HT_CONTACT, conn.GetSelfHandle(), True)
+    q.expect('dbus-signal', signal='NewChannel')
+
+    if DEBUGGING:
+        assert len(messages) > 0
+    else:
+        assertEquals([], messages)
+
+    # Turn signalling off and check we don't get any more messages.
+
+    props_iface.Set(iface, 'Enabled', False)
+    sync_dbus(bus, q, conn)
+    snapshot = list(messages)
+
+    channel = bus.get_object(conn.bus_name, channel_path)
+    channel.Close(dbus_interface=cs.CHANNEL)
+    q.expect('dbus-signal', signal='Closed')
+
     conn.RequestChannel(
         cs.CHANNEL_TYPE_TEXT, cs.HT_CONTACT, conn.GetSelfHandle(), True)
     q.expect('dbus-signal', signal='NewChannel')
-    assert len(messages) > 0
 
-    # Turn signalling off and check we have no new messages.
-
-    props_iface.Set(iface, 'Enabled', False)
-    snapshot = list(messages)
-
-    assert snapshot == messages
+    assertEquals (snapshot, messages)
 
 if __name__ == '__main__':
     exec_test(test)

a6c0404419c7c84924e1b98783860f8ad99aa18f
Will Thompson <will.thompson@collabora.co.uk>
2011-06-23 12:51:58 +0100

    initial-contact-presence: split expect from assertion
    I think this makes things clearer: we're expecting some presences to
    change in response to the roster arriving, and we're expecting the
    changes to be these.
    
    This means that the test failing manifests itself as an assertion
    failure, not a timeout.

--- tests/twisted/presence/initial-contact-presence.py
+++ tests/twisted/presence/initial-contact-presence.py
@@ -33,9 +33,16 @@ def test(q, bus, conn, stream):
     event.query.addChild(make_roster_item('che@foo.com', 'to'))
     stream.send(event.stanza)
 
-    q.expect('dbus-signal', signal='PresencesChanged',
-        args=[{amy: (cs.PRESENCE_OFFLINE, u'offline', u''),
-               che: (cs.PRESENCE_OFFLINE, u'offline', u'')}])
+    # The presence for contacts on the roster whose subscription is 'to' or
+    # 'both' should change from 'unknown' (as checked above) to 'offline'.
+    e = q.expect('dbus-signal', signal='PresencesChanged')
+    changed_presences, = e.args
+    assertEquals(
+        {amy: (cs.PRESENCE_OFFLINE, u'offline', u''),
+         che: (cs.PRESENCE_OFFLINE, u'offline', u''),
+        },
+        changed_presences)
+
     assertEquals({amy: (cs.PRESENCE_OFFLINE, u'offline', u''),
                   bob: (cs.PRESENCE_UNKNOWN, u'unknown', u''),
                   che: (cs.PRESENCE_OFFLINE, u'offline', u'')},

276abd5ff84d635eb5f8cd3eee56978f75631722
Colin Walters <walters@verbum.org>
2011-06-24 18:42:23 +0100

    Honor NOCONFIGURE for compatibility with gnome-autogen.sh
    See also:
    http://people.gnome.org/~walters/docs/build-api.txt

--- autogen.sh
+++ autogen.sh
@@ -34,16 +34,21 @@ cd lib/ext/telepathy-yell
 sh autogen.sh --no-configure
 cd ../../..
 
-run_configure=true
-for arg in $*; do
-    case $arg in
-        --no-configure)
-            run_configure=false
-            ;;
-        *)
-            ;;
-    esac
-done
+# Honor NOCONFIGURE for compatibility with gnome-autogen.sh
+if test x"$NOCONFIGURE" = x; then
+    run_configure=true
+    for arg in $*; do
+        case $arg in
+            --no-configure)
+                run_configure=false
+                ;;
+            *)
+                ;;
+        esac
+    done
+else
+    run_configure=false
+fi
 
 if test $run_configure = true; then
     ./configure "$@"

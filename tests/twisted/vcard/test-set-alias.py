4080addd6c2fa879b69e1e9b4567ccac0cf15697
Will Thompson <will.thompson@collabora.co.uk>
2011-06-28 10:30:24 +0100

    test-set-alias: clean up vCard check.
    This makes the test more thorough: it actually checks the structure of
    the vCard…

--- tests/twisted/vcard/test-set-alias.py
+++ tests/twisted/vcard/test-set-alias.py
@@ -3,8 +3,10 @@
 Test alias setting support.
 """
 
-from servicetest import EventPattern, assertEquals
-from gabbletest import exec_test, acknowledge_iq
+from servicetest import EventPattern, assertEquals, assertEquals
+from gabbletest import (
+    exec_test, acknowledge_iq, expect_and_handle_get_vcard, expect_and_handle_set_vcard,
+)
 import constants as cs
 import ns
 
@@ -16,23 +18,21 @@ def validate_pep_update(pep_update, expected_nickname):
     assertEquals(expected_nickname, nick.children[0])
 
 def test(q, bus, conn, stream):
-    iq_event = q.expect('stream-iq', to=None, query_ns='vcard-temp',
-            query_name='vCard')
-
-    acknowledge_iq(stream, iq_event.stanza)
-
-    conn.Aliasing.SetAliases({1: 'lala'})
+    self_handle = conn.GetSelfHandle()
+    conn.Aliasing.SetAliases({self_handle: 'lala'})
+    expect_and_handle_get_vcard(q, stream)
 
     pep_update = q.expect('stream-iq', iq_type='set', query_ns=ns.PUBSUB, query_name='pubsub')
     validate_pep_update(pep_update, 'lala')
     acknowledge_iq(stream, pep_update.stanza)
 
-    iq_event = q.expect('stream-iq', iq_type='set', query_ns='vcard-temp',
-        query_name='vCard')
-    acknowledge_iq(stream, iq_event.stanza)
+    def check(vCard):
+        nickname = vCard.elements(uri=ns.VCARD_TEMP, name='NICKNAME').next()
+        assertEquals('lala', nickname.children[0])
+    expect_and_handle_set_vcard(q, stream, check=check)
 
     event = q.expect('dbus-signal', signal='AliasesChanged',
-        args=[[(1, u'lala')]])
+        args=[[(self_handle, u'lala')]])
 
 if __name__ == '__main__':
     exec_test(test)

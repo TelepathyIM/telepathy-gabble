22382935be99e027da46303107926a15cd8f3017
Will Thompson <will.thompson@collabora.co.uk>
2011-06-28 10:19:12 +0100

    Test setting our own alias via PEP
    Astonishingly, this was untested...

--- tests/twisted/vcard/test-set-alias.py
+++ tests/twisted/vcard/test-set-alias.py
@@ -3,9 +3,17 @@
 Test alias setting support.
 """
 
-from servicetest import EventPattern
+from servicetest import EventPattern, assertEquals
 from gabbletest import exec_test, acknowledge_iq
 import constants as cs
+import ns
+
+def validate_pep_update(pep_update, expected_nickname):
+    publish = pep_update.query.elements(uri=ns.PUBSUB, name='publish').next()
+    assertEquals(ns.NICK, publish['node'])
+    item = publish.elements(uri=ns.PUBSUB, name='item').next()
+    nick = item.elements(uri=ns.NICK, name='nick').next()
+    assertEquals(expected_nickname, nick.children[0])
 
 def test(q, bus, conn, stream):
     iq_event = q.expect('stream-iq', to=None, query_ns='vcard-temp',
@@ -14,6 +22,10 @@ def test(q, bus, conn, stream):
     acknowledge_iq(stream, iq_event.stanza)
 
     conn.Aliasing.SetAliases({1: 'lala'})
+
+    pep_update = q.expect('stream-iq', iq_type='set', query_ns=ns.PUBSUB, query_name='pubsub')
+    validate_pep_update(pep_update, 'lala')
+    acknowledge_iq(stream, pep_update.stanza)
 
     iq_event = q.expect('stream-iq', iq_type='set', query_ns='vcard-temp',
         query_name='vCard')

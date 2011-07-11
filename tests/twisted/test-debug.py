4080a244d604207bac505073295b7ed1f1556538
Will Thompson <will.thompson@collabora.co.uk>
2011-07-11 17:30:04 +0100

    test-debug: use ProxyWrapper
    Obsessive? Moi?

--- tests/twisted/test-debug.py
+++ tests/twisted/test-debug.py
@@ -5,7 +5,7 @@ Test the debug message interface.
 
 import dbus
 
-from servicetest import assertEquals, sync_dbus, call_async
+from servicetest import assertEquals, sync_dbus, call_async, ProxyWrapper
 from gabbletest import exec_test
 import constants as cs
 from config import DEBUGGING
@@ -18,25 +18,23 @@ def test(q, bus, conn, stream):
     def new_message(timestamp, domain, level, string):
         messages.append((timestamp, domain, level, string))
 
-    debug = bus.get_object(conn.bus_name, path)
-    debug_iface = dbus.Interface(debug, iface)
-    debug_iface.connect_to_signal('NewDebugMessage', new_message)
-    props_iface = dbus.Interface(debug, cs.PROPERTIES_IFACE)
+    debug = ProxyWrapper(bus.get_object(conn.bus_name, path), iface)
+    debug.connect_to_signal('NewDebugMessage', new_message)
 
     if not DEBUGGING:
         # If we're built with --disable-debug, check that the Debug object
         # isn't present.
-        call_async(q, debug_iface, 'GetMessages')
+        call_async(q, debug, 'GetMessages')
         q.expect('dbus-error', method='GetMessages')
         return
 
-    assert len(debug_iface.GetMessages()) > 0
+    assert len(debug.GetMessages()) > 0
 
     # Turn signalling on and generate some messages.
 
     assert len(messages) == 0
-    assert props_iface.Get(iface, 'Enabled') == False
-    props_iface.Set(iface, 'Enabled', True)
+    assert debug.Properties.Get(iface, 'Enabled') == False
+    debug.Properties.Set(iface, 'Enabled', True)
 
     channel_path = conn.RequestChannel(
         cs.CHANNEL_TYPE_TEXT, cs.HT_CONTACT, conn.GetSelfHandle(), True)
@@ -46,7 +44,7 @@ def test(q, bus, conn, stream):
 
     # Turn signalling off and check we don't get any more messages.
 
-    props_iface.Set(iface, 'Enabled', False)
+    debug.Properties.Set(iface, 'Enabled', False)
     sync_dbus(bus, q, conn)
     snapshot = list(messages)
 

396e49c5492c8142cfc3fc45054eee0bbba36e23
Jonny Lamb <jonny.lamb@collabora.co.uk>
2011-10-26 08:48:31 +0100

    file-transfer tests: add simple Metadata test
    Signed-off-by: Jonny Lamb <jonny.lamb@collabora.co.uk>

---
+++ tests/twisted/file-transfer/metadata.py
@@ -0,0 +1,14 @@
+# The 'normal' cases are tested with test-receive-file.py and test-send-file-provide-immediately.py
+# This file tests some corner cases
+import dbus
+
+from file_transfer_helper import exec_file_transfer_test, SendFileTest
+
+class SendFileNoMetadata(SendFileTest):
+    # this is basically the equivalent of calling CreateChannel
+    # without these two properties
+    service_name = ''
+    metadata = {}
+
+if __name__ == '__main__':
+    exec_file_transfer_test(SendFileNoMetadata, True)

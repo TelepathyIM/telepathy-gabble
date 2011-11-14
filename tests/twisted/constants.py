a9608bc48432be0e18441b931c60d6438acc40e9
Andre Moreira Magalhaes (andrunko) <andre.magalhaes@collabora.co.uk>
2011-11-14 15:05:14 -0200

    twisted/constants.py: Re-use PROTOCOL definition when defining PROTOCOL_IFACE_ADDRESSING.

--- tests/twisted/constants.py
+++ tests/twisted/constants.py
@@ -416,8 +416,7 @@ MT_DELIVERY_REPORT = 4
 
 PROTOCOL = 'org.freedesktop.Telepathy.Protocol'
 PROTOCOL_IFACE_PRESENCES = PROTOCOL + '.Interface.Presence'
-PROTOCOL_IFACE_ADDRESSING = \
-    'org.freedesktop.Telepathy.Protocol.Interface.Addressing'
+PROTOCOL_IFACE_ADDRESSING = PROTOCOL + '.Interface.Addressing'
 
 PARAM_REQUIRED = 1
 PARAM_REGISTER = 2

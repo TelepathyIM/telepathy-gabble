TelepathyIM/telepathy-gabble

commit c1be75d93784a9b1b37b0dd717b1494029a675b8

Remove GabbleXmppError.
There is a FIXME buried in here … but the tests pass.

src/jingle-factory.c
@@ -27,6 +27,7 @@
 
 #include <loudmouth/loudmouth.h>
 #include <libsoup/soup.h>
+#include <wocky/wocky-utils.h>
 
 #define DEBUG_FLAG GABBLE_DEBUG_MEDIA
 
@@ -260,6 +261,7 @@ got_jingle_info_stanza (GabbleJingleFactory *fac,
   LmMessageSubType sub_type;
   WockyNode *query_node, *node;
   const gchar *from = wocky_stanza_get_from (message);
+  GError *error = NULL;
 
   if (from != NULL)
     {
@@ -284,19 +286,11 @@ got_jingle_info_stanza (GabbleJingleFactory *fac,
 
   sub_type = lm_message_get_sub_type (message);
 
-  if (sub_type == LM_MESSAGE_SUB_TYPE_ERROR)
+  if (wocky_stanza_extract_errors (message, NULL, &error, NULL, NULL))
     {
-      GabbleXmppError xmpp_error = XMPP_ERROR_UNDEFINED_CONDITION;
-
-      node = wocky_node_get_child (wocky_stanza_get_top_node (message),
-          "error");
-      if (node != NULL)
-        {
-          xmpp_error = gabble_xmpp_error_from_node (node, NULL);
-        }
-
-      DEBUG ("jingle info error: %s", gabble_xmpp_error_string (xmpp_error));
-
+      DEBUG ("jingle info error: %s",
+          wocky_xmpp_stanza_error_to_string (error));
+      g_error_free (error);
       return LM_HANDLER_RESULT_REMOVE_MESSAGE;
     }
 
@@ -772,8 +766,9 @@ ensure_session (GabbleJingleFactory *self,
         }
       else
         {
-          g_set_error (error, GABBLE_XMPP_ERROR,
-              XMPP_ERROR_JINGLE_UNKNOWN_SESSION, "session %s is unknown", sid);
+          g_set_error (error, WOCKY_XMPP_ERROR,
+              WOCKY_JINGLE_ERROR_UNKNOWN_SESSION,
+              "session %s is unknown", sid);
           return NULL;
         }
     }
@@ -828,11 +823,9 @@ jingle_cb (LmMessageHandler *handler,
 
 REQUEST_ERROR:
   g_assert (error != NULL);
-
   DEBUG ("NAKing with error: %s", error->message);
-  _gabble_connection_send_iq_error (priv->conn, msg, error->code,
-      error->message);
-
+  wocky_porter_send_iq_gerror (wocky_session_get_porter (priv->conn->session),
+      msg, error);
   g_error_free (error);
 
   if (sess != NULL && new_session)

src/bytestream-factory.c
@@ -1069,7 +1069,7 @@ si_tube_received (GabbleBytestreamFactory *self,
 
       if (room_handle == 0)
         {
-          GError e = { GABBLE_XMPP_ERROR, XMPP_ERROR_BAD_REQUEST,
+          GError e = { WOCKY_XMPP_ERROR, WOCKY_XMPP_ERROR_BAD_REQUEST,
               "<muc-stream> is only valid in a MUC context" };
 
           gabble_bytestream_iface_close (bytestream, &e);
@@ -1082,7 +1082,7 @@ si_tube_received (GabbleBytestreamFactory *self,
     }
   else
     {
-      GError e = { GABBLE_XMPP_ERROR, XMPP_ERROR_BAD_REQUEST,
+      GError e = { WOCKY_XMPP_ERROR, WOCKY_XMPP_ERROR_BAD_REQUEST,
           "Invalid tube SI request: expected <tube>, <stream> or "
           "<muc-stream>" };
 
@@ -1112,6 +1112,7 @@ bytestream_factory_iq_si_cb (LmMessageHandler *handler,
       (TpBaseConnection *) priv->conn, TP_HANDLE_TYPE_CONTACT);
   TpHandleRepoIface *room_repo = tp_base_connection_get_handles (
       (TpBaseConnection *) priv->conn, TP_HANDLE_TYPE_ROOM);
+  WockyPorter *porter = wocky_session_get_porter (priv->conn->session);
   WockyNode *si;
   TpHandle peer_handle = 0, room_handle;
   GabbleBytestreamIface *bytestream = NULL;
@@ -1136,8 +1137,8 @@ bytestream_factory_iq_si_cb (LmMessageHandler *handler,
   if (!streaminit_parse_request (msg, si, &profile, &from, &stream_id,
         &stream_init_id, &mime_type, &stream_methods, &multiple))
     {
-      _gabble_connection_send_iq_error (priv->conn, msg,
-          XMPP_ERROR_BAD_REQUEST, "failed to parse SI request");
+      wocky_porter_send_iq_error (porter, msg,
+          WOCKY_XMPP_ERROR_BAD_REQUEST, "failed to parse SI request");
       goto out;
     }
 
@@ -1190,8 +1191,8 @@ bytestream_factory_iq_si_cb (LmMessageHandler *handler,
 
   if (peer_handle == 0)
     {
-      _gabble_connection_send_iq_error (priv->conn, msg,
-          XMPP_ERROR_JID_MALFORMED, NULL);
+      wocky_porter_send_iq_error (porter, msg,
+          WOCKY_XMPP_ERROR_JID_MALFORMED, NULL);
       goto out;
     }
 
@@ -1241,10 +1242,10 @@ bytestream_factory_iq_si_cb (LmMessageHandler *handler,
 
   if (bytestream == NULL)
     {
-      DEBUG ("SI request doesn't contain any supported stream methods.");
+      GError error = { WOCKY_SI_ERROR, WOCKY_SI_ERROR_NO_VALID_STREAMS, "" };
 
-      _gabble_connection_send_iq_error (priv->conn, msg,
-          XMPP_ERROR_SI_NO_VALID_STREAMS, NULL);
+      DEBUG ("SI request doesn't contain any supported stream methods.");
+      wocky_porter_send_iq_gerror (porter, msg, &error);
       goto out;
     }
 
@@ -1254,7 +1255,7 @@ bytestream_factory_iq_si_cb (LmMessageHandler *handler,
       if (!gabble_bytestream_multiple_has_stream_method (
             GABBLE_BYTESTREAM_MULTIPLE (bytestream)))
         {
-          GError e = { GABBLE_XMPP_ERROR, XMPP_ERROR_SI_NO_VALID_STREAMS, "" };
+          GError e = { WOCKY_SI_ERROR, WOCKY_SI_ERROR_NO_VALID_STREAMS, "" };
           DEBUG ("No valid stream method in the multi bytestream. Closing");
 
           gabble_bytestream_iface_close (bytestream, &e);
@@ -1278,7 +1279,7 @@ bytestream_factory_iq_si_cb (LmMessageHandler *handler,
     }
   else
     {
-      GError e = { GABBLE_XMPP_ERROR, XMPP_ERROR_SI_BAD_PROFILE, "" };
+      GError e = { WOCKY_SI_ERROR, WOCKY_SI_ERROR_BAD_PROFILE, "" };
       DEBUG ("SI profile unsupported: %s", profile);
 
       gabble_bytestream_iface_close (bytestream, &e);
@@ -1300,6 +1301,7 @@ handle_ibb_open_iq (GabbleBytestreamFactory *self,
 {
   GabbleBytestreamFactoryPrivate *priv =
     GABBLE_BYTESTREAM_FACTORY_GET_PRIVATE (self);
+  WockyPorter *porter = wocky_session_get_porter (priv->conn->session);
   GabbleBytestreamIBB *bytestream;
   WockyNode *open_node;
   ConstBytestreamIdentifier bsid = { NULL, NULL };
@@ -1319,8 +1321,8 @@ handle_ibb_open_iq (GabbleBytestreamFactory *self,
   if (bsid.jid == NULL)
     {
       DEBUG ("got a message without a from field");
-      _gabble_connection_send_iq_error (priv->conn, msg,
-          XMPP_ERROR_BAD_REQUEST, NULL);
+      wocky_porter_send_iq_error (porter, msg,
+          WOCKY_XMPP_ERROR_BAD_REQUEST, NULL);
       return TRUE;
     }
 
@@ -1328,8 +1330,8 @@ handle_ibb_open_iq (GabbleBytestreamFactory *self,
   if (bsid.stream == NULL)
     {
       DEBUG ("IBB open stanza doesn't contain stream id");
-      _gabble_connection_send_iq_error (priv->conn, msg,
-          XMPP_ERROR_BAD_REQUEST, NULL);
+      wocky_porter_send_iq_error (porter, msg,
+          WOCKY_XMPP_ERROR_BAD_REQUEST, NULL);
       return TRUE;
     }
 
@@ -1338,8 +1340,8 @@ handle_ibb_open_iq (GabbleBytestreamFactory *self,
     {
       /* We don't accept streams not previously announced using SI */
       DEBUG ("unknown stream: <%s> from <%s>", bsid.stream, bsid.jid);
-      _gabble_connection_send_iq_error (priv->conn, msg,
-          XMPP_ERROR_BAD_REQUEST, NULL);
+      wocky_porter_send_iq_error (porter, msg,
+          WOCKY_XMPP_ERROR_BAD_REQUEST, NULL);
       return TRUE;
     }
 
@@ -1349,8 +1351,8 @@ handle_ibb_open_iq (GabbleBytestreamFactory *self,
     {
       /* We don't accept streams not previously accepted using SI */
       DEBUG ("unaccepted stream: <%s> from <%s>", bsid.stream, bsid.jid);
-      _gabble_connection_send_iq_error (priv->conn, msg,
-          XMPP_ERROR_BAD_REQUEST, NULL);
+      wocky_porter_send_iq_error (porter, msg,
+          WOCKY_XMPP_ERROR_BAD_REQUEST, NULL);
       return TRUE;
     }
 
@@ -1366,7 +1368,7 @@ handle_ibb_open_iq (GabbleBytestreamFactory *self,
   g_object_set (bytestream, "state", GABBLE_BYTESTREAM_STATE_OPEN,
       NULL);
 
-  _gabble_connection_acknowledge_set_iq (priv->conn, msg);
+  wocky_porter_acknowledge_iq (porter, msg, NULL);
 
   return TRUE;
 }
@@ -1377,6 +1379,7 @@ handle_ibb_close_iq (GabbleBytestreamFactory *self,
 {
   GabbleBytestreamFactoryPrivate *priv =
     GABBLE_BYTESTREAM_FACTORY_GET_PRIVATE (self);
+  WockyPorter *porter = wocky_session_get_porter (priv->conn->session);
   ConstBytestreamIdentifier bsid = { NULL, NULL };
   GabbleBytestreamIBB *bytestream;
   WockyNode *close_node;
@@ -1394,8 +1397,8 @@ handle_ibb_close_iq (GabbleBytestreamFactory *self,
   if (bsid.jid == NULL)
     {
       DEBUG ("got a message without a from field");
-      _gabble_connection_send_iq_error (priv->conn, msg,
-          XMPP_ERROR_BAD_REQUEST, "IBB <close> has no 'from' attribute");
+      wocky_porter_send_iq_error (porter, msg,
+          WOCKY_XMPP_ERROR_BAD_REQUEST, "IBB <close> has no 'from' attribute");
       return TRUE;
     }
 
@@ -1403,8 +1406,8 @@ handle_ibb_close_iq (GabbleBytestreamFactory *self,
   if (bsid.stream == NULL)
     {
       DEBUG ("IBB close stanza doesn't contain stream id");
-      _gabble_connection_send_iq_error (priv->conn, msg,
-          XMPP_ERROR_BAD_REQUEST, "IBB <close> has no stream ID");
+      wocky_porter_send_iq_error (porter, msg,
+          WOCKY_XMPP_ERROR_BAD_REQUEST, "IBB <close> has no stream ID");
       return TRUE;
     }
 
@@ -1412,8 +1415,8 @@ handle_ibb_close_iq (GabbleBytestreamFactory *self,
   if (bytestream == NULL)
     {
       DEBUG ("unknown stream: <%s> from <%s>", bsid.stream, bsid.jid);
-      _gabble_connection_send_iq_error (priv->conn, msg,
-          XMPP_ERROR_ITEM_NOT_FOUND, NULL);
+      wocky_porter_send_iq_error (porter, msg,
+          WOCKY_XMPP_ERROR_ITEM_NOT_FOUND, NULL);
     }
   else
     {
@@ -1434,6 +1437,7 @@ handle_ibb_data (GabbleBytestreamFactory *self,
 {
   GabbleBytestreamFactoryPrivate *priv =
     GABBLE_BYTESTREAM_FACTORY_GET_PRIVATE (self);
+  WockyPorter *porter = wocky_session_get_porter (priv->conn->session);
   GabbleBytestreamIBB *bytestream = NULL;
   WockyNode *data;
   ConstBytestreamIdentifier bsid = { NULL, NULL };
@@ -1454,8 +1458,8 @@ handle_ibb_data (GabbleBytestreamFactory *self,
     {
       DEBUG ("got a message without a from field");
       if (is_iq)
-        _gabble_connection_send_iq_error (priv->conn, msg,
-            XMPP_ERROR_BAD_REQUEST, "IBB <close> has no 'from' attribute");
+        wocky_porter_send_iq_error (porter, msg, WOCKY_XMPP_ERROR_BAD_REQUEST,
+            "IBB <close> has no 'from' attribute");
       return TRUE;
     }
 
@@ -1464,8 +1468,8 @@ handle_ibb_data (GabbleBytestreamFactory *self,
     {
       DEBUG ("got a IBB message data without a stream id field");
       if (is_iq)
-        _gabble_connection_send_iq_error (priv->conn, msg,
-            XMPP_ERROR_BAD_REQUEST, "IBB <data> needs a stream ID");
+        wocky_porter_send_iq_error (porter, msg, WOCKY_XMPP_ERROR_BAD_REQUEST,
+            "IBB <data> needs a stream ID");
       return TRUE;
     }
 
@@ -1475,8 +1479,8 @@ handle_ibb_data (GabbleBytestreamFactory *self,
     {
       DEBUG ("unknown stream: <%s> from <%s>", bsid.stream, bsid.jid);
       if (is_iq)
-        _gabble_connection_send_iq_error (priv->conn, msg,
-            XMPP_ERROR_BAD_REQUEST, "IBB <data> has unknown stream ID");
+        wocky_porter_send_iq_error (porter, msg, WOCKY_XMPP_ERROR_BAD_REQUEST,
+            "IBB <data> has unknown stream ID");
 
       return TRUE;
     }
@@ -1594,6 +1598,7 @@ handle_socks5_query_iq (GabbleBytestreamFactory *self,
 {
   GabbleBytestreamFactoryPrivate *priv =
     GABBLE_BYTESTREAM_FACTORY_GET_PRIVATE (self);
+  WockyPorter *porter = wocky_session_get_porter (priv->conn->session);
   GabbleBytestreamSocks5 *bytestream;
   WockyNode *query_node;
   ConstBytestreamIdentifier bsid = { NULL, NULL };
@@ -1613,8 +1618,8 @@ handle_socks5_query_iq (GabbleBytestreamFactory *self,
   if (bsid.jid == NULL)
     {
       DEBUG ("got a message without a from field");
-      _gabble_connection_send_iq_error (priv->conn, msg,
-          XMPP_ERROR_BAD_REQUEST, "SOCKS5 <query> has no 'from' attribute");
+      wocky_porter_send_iq_error (porter, msg, WOCKY_XMPP_ERROR_BAD_REQUEST,
+          "SOCKS5 <query> has no 'from' attribute");
       return TRUE;
     }
 
@@ -1622,8 +1627,8 @@ handle_socks5_query_iq (GabbleBytestreamFactory *self,
   if (bsid.stream == NULL)
     {
       DEBUG ("SOCKS5 query stanza doesn't contain stream id");
-      _gabble_connection_send_iq_error (priv->conn, msg,
-          XMPP_ERROR_BAD_REQUEST, "SOCKS5 <query> has no stream ID");
+      wocky_porter_send_iq_error (porter, msg, WOCKY_XMPP_ERROR_BAD_REQUEST,
+          "SOCKS5 <query> has no stream ID");
       return TRUE;
     }
 
@@ -1632,8 +1637,7 @@ handle_socks5_query_iq (GabbleBytestreamFactory *self,
     {
       /* We don't accept streams not previously announced using SI */
       DEBUG ("unknown stream: <%s> from <%s>", bsid.stream, bsid.jid);
-      _gabble_connection_send_iq_error (priv->conn, msg,
-          XMPP_ERROR_ITEM_NOT_FOUND,
+      wocky_porter_send_iq_error (porter, msg, WOCKY_XMPP_ERROR_ITEM_NOT_FOUND,
           "SOCKS5 <query> has an unknown stream ID");
       return TRUE;
     }
@@ -1643,8 +1647,7 @@ handle_socks5_query_iq (GabbleBytestreamFactory *self,
   if (tmp != NULL && tp_strdiff (tmp, "tcp"))
     {
       DEBUG ("non-TCP SOCKS5 bytestreams are not supported");
-      _gabble_connection_send_iq_error (priv->conn, msg,
-          XMPP_ERROR_BAD_REQUEST,
+      wocky_porter_send_iq_error (porter, msg, WOCKY_XMPP_ERROR_BAD_REQUEST,
           "SOCKS5 non-TCP bytestreams are not supported");
       return TRUE;
     }

src/bytestream-ibb.c
@@ -628,8 +628,9 @@ gabble_bytestream_ibb_receive (GabbleBytestreamIBB *self,
       DEBUG ("can't receive data through a not open bytestream (state: %d)",
           priv->state);
       if (is_iq)
-        _gabble_connection_send_iq_error (priv->conn, msg,
-            XMPP_ERROR_BAD_REQUEST, "IBB bytestream isn't open");
+        wocky_porter_send_iq_error (
+            wocky_session_get_porter (priv->conn->session), msg,
+            WOCKY_XMPP_ERROR_BAD_REQUEST, "IBB bytestream isn't open");
       return;
     }
 
@@ -644,8 +645,9 @@ gabble_bytestream_ibb_receive (GabbleBytestreamIBB *self,
     {
       DEBUG ("base64 decoding failed");
       if (is_iq)
-        _gabble_connection_send_iq_error (priv->conn, msg,
-            XMPP_ERROR_BAD_REQUEST, "base64 decoding failed");
+        wocky_porter_send_iq_error (
+            wocky_session_get_porter (priv->conn->session), msg,
+            WOCKY_XMPP_ERROR_BAD_REQUEST, "base64 decoding failed");
       return;
     }
 
@@ -662,8 +664,9 @@ gabble_bytestream_ibb_receive (GabbleBytestreamIBB *self,
           DEBUG ("Buffer is full. Closing the bytestream");
 
           if (is_iq)
-            _gabble_connection_send_iq_error (priv->conn, msg,
-                XMPP_ERROR_NOT_ACCEPTABLE, "buffer is full");
+            wocky_porter_send_iq_error (
+                wocky_session_get_porter (priv->conn->session), msg,
+                WOCKY_XMPP_ERROR_NOT_ACCEPTABLE, "buffer is full");
 
           gabble_bytestream_iface_close (GABBLE_BYTESTREAM_IFACE (self), NULL);
           g_string_free (str, TRUE);
@@ -755,16 +758,15 @@ gabble_bytestream_ibb_decline (GabbleBytestreamIBB *self,
       '@', "id", priv->stream_init_id,
       NULL);
 
-  if (error != NULL && error->domain == GABBLE_XMPP_ERROR)
+  if (error != NULL)
     {
-      gabble_xmpp_error_to_node (error->code,
-        wocky_stanza_get_top_node (msg), error->message);
+      wocky_stanza_error_to_node (error, wocky_stanza_get_top_node (msg));
     }
   else
     {
-      gabble_xmpp_error_to_node (XMPP_ERROR_FORBIDDEN,
-          wocky_stanza_get_top_node (msg),
-          "Offer Declined");
+      GError fallback = { WOCKY_XMPP_ERROR, WOCKY_XMPP_ERROR_FORBIDDEN,
+          "Offer Declined" };
+      wocky_stanza_error_to_node (&fallback, wocky_stanza_get_top_node (msg));
     }
 
   _gabble_connection_send (priv->conn, msg, NULL);
@@ -785,6 +787,7 @@ gabble_bytestream_ibb_close (GabbleBytestreamIface *iface,
 {
   GabbleBytestreamIBB *self = GABBLE_BYTESTREAM_IBB (iface);
   GabbleBytestreamIBBPrivate *priv = GABBLE_BYTESTREAM_IBB_GET_PRIVATE (self);
+  WockyPorter *porter = wocky_session_get_porter (priv->conn->session);
   GSList *l;
 
   if (priv->state == GABBLE_BYTESTREAM_STATE_CLOSED)
@@ -795,16 +798,9 @@ gabble_bytestream_ibb_close (GabbleBytestreamIface *iface,
   priv->received_stanzas_not_acked = g_slist_reverse (
       priv->received_stanzas_not_acked);
 
-  for (l = priv->received_stanzas_not_acked; l != NULL;
-      l = g_slist_next (l))
-    {
-      LmMessage *iq = (LmMessage *) l->data;
-
-      _gabble_connection_send_iq_error (priv->conn, iq,
-          XMPP_ERROR_ITEM_NOT_FOUND, NULL);
-
-      lm_message_unref (iq);
-    }
+  for (l = priv->received_stanzas_not_acked; l != NULL; l = g_slist_next (l))
+    wocky_porter_send_iq_error (porter, l->data,
+        WOCKY_XMPP_ERROR_ITEM_NOT_FOUND, NULL);
 
   g_slist_free (priv->received_stanzas_not_acked);
   priv->received_stanzas_not_acked = NULL;

src/bytestream-ibb.h
@@ -25,8 +25,6 @@
 
 #include <telepathy-glib/base-connection.h>
 
-#include "error.h"
-
 G_BEGIN_DECLS
 
 typedef struct _GabbleBytestreamIBB GabbleBytestreamIBB;

src/bytestream-multiple.h
@@ -28,7 +28,6 @@
 #include <telepathy-glib/base-connection.h>
 
 #include "bytestream-iface.h"
-#include "error.h"
 
 G_BEGIN_DECLS
 

src/bytestream-socks5.c
@@ -621,6 +621,7 @@ socks5_error (GabbleBytestreamSocks5 *self)
 {
   GabbleBytestreamSocks5Private *priv =
       GABBLE_BYTESTREAM_SOCKS5_GET_PRIVATE (self);
+  WockyPorter *porter = wocky_session_get_porter (priv->conn->session);
   Socks5State previous_state;
 
   stop_timer (self);
@@ -657,8 +658,9 @@ socks5_error (GabbleBytestreamSocks5 *self)
         g_signal_emit_by_name (self, "connection-error");
 
         g_assert (priv->msg_for_acknowledge_connection != NULL);
-        _gabble_connection_send_iq_error (priv->conn,
-            priv->msg_for_acknowledge_connection, XMPP_ERROR_ITEM_NOT_FOUND,
+        wocky_porter_send_iq_error (porter,
+            priv->msg_for_acknowledge_connection,
+            WOCKY_XMPP_ERROR_ITEM_NOT_FOUND,
             "impossible to connect to any streamhost");
 
         lm_message_unref (priv->msg_for_acknowledge_connection);
@@ -1444,16 +1446,15 @@ gabble_bytestream_socks5_decline (GabbleBytestreamSocks5 *self,
       '@', "id", priv->stream_init_id,
       NULL);
 
-  if (error != NULL && error->domain == GABBLE_XMPP_ERROR)
+  if (error != NULL)
     {
-      gabble_xmpp_error_to_node (error->code,
-        wocky_stanza_get_top_node (msg), error->message);
+      wocky_stanza_error_to_node (error, wocky_stanza_get_top_node (msg));
     }
   else
     {
-      gabble_xmpp_error_to_node (XMPP_ERROR_FORBIDDEN,
-          wocky_stanza_get_top_node (msg),
-          "Offer Declined");
+      GError fallback = { WOCKY_XMPP_ERROR, WOCKY_XMPP_ERROR_FORBIDDEN,
+          "Offer Declined" };
+      wocky_stanza_error_to_node (&fallback, wocky_stanza_get_top_node (msg));
     }
 
   _gabble_connection_send (priv->conn, msg, NULL);

src/bytestream-socks5.h
@@ -27,8 +27,6 @@
 
 #include <telepathy-glib/base-connection.h>
 
-#include "error.h"
-
 G_BEGIN_DECLS
 
 typedef struct _GabbleBytestreamSocks5 GabbleBytestreamSocks5;

src/conn-aliasing.c
@@ -25,6 +25,7 @@
 #include <telepathy-glib/gtypes.h>
 #include <telepathy-glib/interfaces.h>
 #include <telepathy-glib/svc-connection.h>
+#include <wocky/wocky-utils.h>
 
 #define DEBUG_FLAG GABBLE_DEBUG_CONNECTION
 
@@ -488,22 +489,14 @@ nick_publish_msg_reply_cb (GabbleConnection *conn,
                            gpointer user_data)
 {
 #ifdef ENABLE_DEBUG
-  if (lm_message_get_sub_type (reply_msg) == LM_MESSAGE_SUB_TYPE_ERROR)
+  GError *error = NULL;
+
+  if (wocky_stanza_extract_errors (reply_msg, NULL, &error, NULL, NULL))
     {
-      WockyNode *error_node;
+      DEBUG ("can't publish nick using PEP: %s: %s",
+          wocky_xmpp_stanza_error_to_string (error), error->message);
 
-      error_node = wocky_node_get_child (
-          wocky_stanza_get_top_node (reply_msg), "error");
-
-      if (error_node != NULL)
-        {
-          GabbleXmppError error = gabble_xmpp_error_from_node (error_node,
-              NULL);
-
-          DEBUG ("can't publish nick using PEP: %s: %s",
-              gabble_xmpp_error_string (error),
-              gabble_xmpp_error_description (error));
-        }
+      g_clear_error (&error);
     }
 #endif
 

commit ecee60dfc33f05e56eb2d2b34faf188943ff8931

jingle: correctly send back unknown-session errors
I broke this in c1be75d9 but it wasn't tested. Luckily
WOCKY_JINGLE_ERROR_UNKNOWN_SESSION (2) is a valid WockyXmppError too so
we didn't crash before, we just sent <gone> instead.

https://bugs.freedesktop.org/show_bug.cgi?id=33789 is relevant.

src/jingle-factory.c
@@ -367,7 +367,7 @@ ensure_session (GabbleJingleFactory *self,
         }
       else
         {
-          g_set_error (error, WOCKY_XMPP_ERROR,
+          g_set_error (error, WOCKY_JINGLE_ERROR,
               WOCKY_JINGLE_ERROR_UNKNOWN_SESSION,
               "session %s is unknown", sid);
           return NULL;

tests/twisted/Makefile.am
@@ -215,6 +215,7 @@ TWISTED_JINGLE_TESTS = \
 	jingle/test-wait-for-caps-incomplete.py \
 	jingle/test-wait-for-caps.py \
 	jingle/transport-info-parsing.py \
+	jingle/unknown-session.py \
 	$(NULL)
 
 TWISTED_FT_TESTS = \

tests/twisted/jingle/unknown-session.py
@@ -1,0 +1,38 @@
+"""
+Tests that Gabble doesn't explode if it gets Jingle stanzas for unknown
+sessions.
+"""
+
+from gabbletest import exec_test
+from servicetest import assertEquals
+from jingletest2 import JingleProtocol031
+import ns
+
+def assertHasChild(node, uri, name):
+    try:
+        node.elements(uri=uri, name=name).next()
+    except StopIteration:
+        raise AssertionError(
+            "Expected <%s xmlns='%s'> to be a child of\n    %s" % (
+            name, uri, node.toXml()))
+
+def test_send_action_for_unknown_session(q, bus, conn, stream):
+    jp = JingleProtocol031()
+    peer = 'guybrush@threepwo.od'
+
+    iq = jp.SetIq(peer, 'test@localhost',
+        [ jp.Jingle('fine-leather-jackets', peer, 'session-info', [])
+        ])
+    stream.send(jp.xml(iq))
+
+    e = q.expect('stream-iq', iq_type='error', iq_id=iq[2]['id'])
+    stanza = e.stanza
+    error_node = stanza.children[-1]
+    assertEquals('error', error_node.name)
+
+    # http://xmpp.org/extensions/xep-0166.html#example-29
+    assertHasChild(error_node, ns.STANZA, 'item-not-found')
+    assertHasChild(error_node, ns.JINGLE_ERRORS, 'unknown-session')
+
+if __name__ == '__main__':
+    exec_test(test_send_action_for_unknown_session)

tests/twisted/ns.py
@@ -26,6 +26,7 @@ JINGLE_015 = "http://jabber.org/protocol/jingle"
 JINGLE_015_AUDIO = "http://jabber.org/protocol/jingle/description/audio"
 JINGLE_015_VIDEO = "http://jabber.org/protocol/jingle/description/video"
 JINGLE = "urn:xmpp:jingle:1"
+JINGLE_ERRORS = "urn:xmpp:jingle:errors:1"
 JINGLE_RTP = "urn:xmpp:jingle:apps:rtp:1"
 JINGLE_RTP_AUDIO = "urn:xmpp:jingle:apps:rtp:audio"
 JINGLE_RTP_VIDEO = "urn:xmpp:jingle:apps:rtp:video"
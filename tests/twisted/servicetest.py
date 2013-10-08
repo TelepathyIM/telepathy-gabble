0089204ffe2245c2bf8682dc3325107dc7dde956
Guillaume Desmottes <guillaume.desmottes@collabora.co.uk>
2013-10-08 12:48:22 -0400

    servicetest: add ContactBlocking wrapper

--- tests/twisted/servicetest.py
+++ tests/twisted/servicetest.py
@@ -630,6 +630,7 @@ def wrap_connection(conn):
          ('MailNotification', cs.CONN_IFACE_MAIL_NOTIFICATION),
          ('ContactList', cs.CONN_IFACE_CONTACT_LIST),
          ('ContactGroups', cs.CONN_IFACE_CONTACT_GROUPS),
+         ('ContactBlocking', cs.CONN_IFACE_CONTACT_BLOCKING),
          ('PowerSaving', cs.CONN_IFACE_POWER_SAVING),
          ('Addressing', cs.CONN_IFACE_ADDRESSING),
          ('ClientTypes', cs.CONN_IFACE_CLIENT_TYPES),

db960b774b142270cb8cdddfd801c442f0931fff
Guillaume Desmottes <guillaume.desmottes@collabora.co.uk>
2013-10-08 13:50:43 -0400

    push-from-contact: stop using the old roster API

--- tests/twisted/roster/push-from-contact.py
+++ tests/twisted/roster/push-from-contact.py
@@ -4,9 +4,7 @@ Ensure that Gabble correctly ignores roster pushes from contacts.
 
 from servicetest import EventPattern
 from gabbletest import exec_test, acknowledge_iq
-from rostertest import (
-    make_roster_push, expect_contact_list_signals, check_contact_list_signals,
-)
+from rostertest import make_roster_push
 import ns
 import constants as cs
 
@@ -17,10 +15,7 @@ def test(q, bus, conn, stream):
     event = q.expect('stream-iq', query_ns=ns.ROSTER)
     acknowledge_iq(stream, event.stanza)
 
-    pairs = expect_contact_list_signals(q, bus, conn,
-            ['stored'])
-    stored = check_contact_list_signals(q, bus, conn, pairs.pop(0),
-            cs.HT_LIST, 'stored', [])
+    q.expect('dbus-signal', signal='ContactListStateChanged', args=[cs.CONTACT_LIST_STATE_SUCCESS])
 
     # Some malicious peer sends us a roster push to try to trick us into
     # showing them on our roster. Gabble should know better than to trust it.
@@ -29,8 +24,7 @@ def test(q, bus, conn, stream):
     stream.send(iq)
 
     q.forbid_events(
-        [ EventPattern('dbus-signal', signal='MembersChanged',
-              path=stored.object_path),
+        [
           EventPattern('dbus-signal', signal='ContactsChanged'),
         ])
 

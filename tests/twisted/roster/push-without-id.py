f4f623e1a42f97b1496422c9c4c3d4b2a63287a8
Guillaume Desmottes <guillaume.desmottes@collabora.co.uk>
2013-10-08 13:50:43 -0400

    push-without-id: stop using the old roster API

--- tests/twisted/roster/push-without-id.py
+++ tests/twisted/roster/push-without-id.py
@@ -6,9 +6,7 @@ we should interop with them. Think of it of like No_Reply in D-Bus...
 
 from servicetest import EventPattern, sync_dbus
 from gabbletest import exec_test, acknowledge_iq, sync_stream
-from rostertest import (
-    make_roster_push, expect_contact_list_signals, check_contact_list_signals,
-)
+from rostertest import make_roster_push
 import ns
 import constants as cs
 
@@ -19,10 +17,7 @@ def test(q, bus, conn, stream):
     event = q.expect('stream-iq', query_ns=ns.ROSTER)
     acknowledge_iq(stream, event.stanza)
 
-    pairs = expect_contact_list_signals(q, bus, conn,
-            ['stored'])
-    stored = check_contact_list_signals(q, bus, conn, pairs.pop(0),
-            cs.HT_LIST, 'stored', [])
+    q.expect('dbus-signal', signal='ContactListStateChanged', args=[cs.CONTACT_LIST_STATE_SUCCESS])
 
     # The server sends us a roster push without an id=''. WTF!
     iq = make_roster_push(stream, jid, 'both')
@@ -31,8 +26,6 @@ def test(q, bus, conn, stream):
 
     h = conn.get_contact_handle_sync(jid)
     q.expect_many(
-        EventPattern('dbus-signal', signal='MembersChanged',
-            args=['', [h], [], [], [], 0, 0], path=stored.object_path),
         EventPattern('dbus-signal', signal='ContactsChanged',
             args=[{ h: (cs.SUBSCRIPTION_STATE_YES,
                     cs.SUBSCRIPTION_STATE_YES, ''), },
@@ -52,9 +45,7 @@ def test(q, bus, conn, stream):
     stream.send(iq)
 
     q.forbid_events(
-        [ EventPattern('dbus-signal', signal='MembersChanged',
-              path=stored.object_path),
-          EventPattern('dbus-signal', signal='ContactsChanged'),
+        [ EventPattern('dbus-signal', signal='ContactsChanged'),
         ])
     # Make sure Gabble's got the evil push...
     sync_stream(q, stream)

7bbbb3f241c92a9acfe924a9e02a09b86927a4d0
Simon McVittie <simon.mcvittie@collabora.co.uk>
2014-03-26 13:35:58 +0000

    Stop trying to use automake-1.11

--- autogen.sh
+++ autogen.sh
@@ -1,19 +1,6 @@
 #!/bin/sh
 set -e
 
-if test -n "$AUTOMAKE"; then
-    : # don't override an explicit user request
-elif automake-1.11 --version >/dev/null 2>/dev/null && \
-     aclocal-1.11 --version >/dev/null 2>/dev/null; then
-    # If we have automake-1.11, use it. This is the oldest version (=> least
-    # likely to introduce undeclared dependencies) that will give us
-    # --enable-silent-rules support.
-    AUTOMAKE=automake-1.11
-    export AUTOMAKE
-    ACLOCAL=aclocal-1.11
-    export ACLOCAL
-fi
-
 autoreconf -i -f
 
 #Check if submodules should be enabled

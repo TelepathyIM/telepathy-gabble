3e607035afaba2637d0873dbda04199580b024aa
Guillaume Desmottes <guillaume.desmottes@collabora.co.uk>
2014-05-13 11:58:11 +0200

    search-manager: use tp_asv_new()

--- src/search-manager.c
+++ src/search-manager.c
@@ -332,14 +332,12 @@ gabble_search_manager_type_foreach_channel_class (GType type,
     TpChannelManagerTypeChannelClassFunc func,
     gpointer user_data)
 {
-  GHashTable *table = g_hash_table_new_full (g_str_hash, g_str_equal,
-      NULL, (GDestroyNotify) tp_g_value_slice_free);
-  GValue *value;
+  GHashTable *table;
 
-  value = tp_g_value_slice_new_string (
-      TP_IFACE_CHANNEL_TYPE_CONTACT_SEARCH1);
-  g_hash_table_insert (table, (gchar *) search_channel_fixed_properties[0],
-      value);
+  table = tp_asv_new (
+      TP_PROP_CHANNEL_CHANNEL_TYPE, G_TYPE_STRING,
+        TP_IFACE_CHANNEL_TYPE_CONTACT_SEARCH1,
+      NULL);
 
   func (type, table, search_channel_allowed_properties, user_data);
 
